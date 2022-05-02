d5259ffe20e1d79e3e34aebb8342a962e0101d7c
Nichols, Kieran <kieran.nichols@tylertech.com>
2022-05-02 11:17:34 -0400

    chore(time-picker): add new `TimePickerComponentDelegate`

--- src/lib/time-picker/index.ts
+++ src/lib/time-picker/index.ts
@@ -3,6 +3,7 @@ import { defineCustomElement } from '@tylertech/forge-core';
 import { TimePickerComponent } from './time-picker';
 
 export * from './time-picker-adapter';
+export * from './time-picker-component-delegate';
 export * from './time-picker-constants';
 export * from './time-picker-foundation';
 export * from './time-picker-utils';
