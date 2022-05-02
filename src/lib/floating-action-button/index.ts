56a8f77dfe315a3c422816c0b2cfba221f1d2a74
Nichols, Kieran <kieran.nichols@tylertech.com>
2022-05-02 11:41:16 -0400

    chore(floating-action-button): added `FloatingActionButtonComponentDelegate` class

--- src/lib/floating-action-button/index.ts
+++ src/lib/floating-action-button/index.ts
@@ -4,6 +4,7 @@ import { FloatingActionButton } from './floating-action-button';
 
 export * from './floating-action-button-constants';
 export * from './floating-action-button';
+export * from './floating-action-button-component-delegate';
 
 export function defineFloatingActionButtonComponent(): void {
   defineCustomElement(FloatingActionButton);
