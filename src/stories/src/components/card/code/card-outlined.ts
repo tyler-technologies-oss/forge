043be3ede073337f50fb1c9f0241813241b06d9a
Nichols, Kieran <kieran.nichols@tylertech.com>
2022-06-01 10:31:46 -0400

    chore(storybook): update card and bottom-sheet docs

--- src/stories/src/components/card/code/card-outlined.ts
+++ src/stories/src/components/card/code/card-outlined.ts
@@ -1,11 +1,9 @@
 export const CardOutlinedCodeHtml = () => {
-  return `<forge-card outlined>
-  Content
-</forge-card>`;
-};
-
-export const CardOutlinedCodeBlazor = () => {
-  return `<ForgeCard Outlined>
-  Content
-</ForgeCard>`;
+  return `
+<forge-card outlined>
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias quas sed
+  aliquid cumque sunt iste ad, alias quod adipisci? Nulla, libero necessitatibus
+  enim sint nesciunt provident excepturi dolorum pariatur illum?  
+</forge-card>
+  `;
 };
---
+++ src/stories/src/components/card/code/card-scaffold.ts
@@ -0,0 +1,48 @@
+export const CardScaffoldCodeHtml = () => {
+  return `
+<forge-card class="scaffold-card">
+  <forge-scaffold>
+    <forge-toolbar slot="header">
+      <h1 slot="start" class="forge-typography--title">Lorem ipsum</h1>
+    </forge-toolbar>
+
+    <p slot="body" class="forge-typography--body2" tabindex="0" class="card-content">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias quas sed
+      aliquid cumque sunt iste ad, alias quod adipisci? Nulla, libero necessitatibus
+      enim sint nesciunt provident excepturi dolorum pariatur illum?
+
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias quas sed
+      aliquid cumque sunt iste ad, alias quod adipisci? Nulla, libero necessitatibus
+      enim sint nesciunt provident excepturi dolorum pariatur illum?
+
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias quas sed
+      aliquid cumque sunt iste ad, alias quod adipisci? Nulla, libero necessitatibus
+      enim sint nesciunt provident excepturi dolorum pariatur illum?
+    </p>
+
+    <forge-toolbar slot="footer" inverted>
+      <forge-button type="outlined" slot="end">
+        <button type="button">Cancel</button>
+      </forge-button>
+      <forge-button type="unelevated" slot="end" style="margin-left: 8px;">
+        <button type="button">Ok</button>
+      </forge-button>
+    </forge-toolbar>
+  </forge-scaffold>
+</forge-card>
+  `;
+};
+
+export const CardScaffoldCodeScss = () =>
+  `
+.scaffold-card {
+  width: 400px;
+  --forge-card-padding: 0;
+  --forge-card-height: 300px
+}
+
+.card-content {
+  padding: 16px;
+  margin: 0;
+}
+  `;
