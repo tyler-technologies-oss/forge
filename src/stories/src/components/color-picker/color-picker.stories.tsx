1e2a799f76fe06f59dedc3cde8f57fe4ba8fa211
Nichols, Kieran <kieran.nichols@tylertech.com>
2022-06-02 16:21:39 -0400

    chore(storybook): update color-picker docs

--- src/stories/src/components/color-picker/color-picker.stories.tsx
+++ src/stories/src/components/color-picker/color-picker.stories.tsx
@@ -1,7 +1,9 @@
-import { Meta } from '@storybook/react';
-const MDX = require('./color-picker.mdx').default;
+import React from 'react';
+import { Meta, Story } from '@storybook/react';
 import { IColorPickerProps, argTypes } from './color-picker-args';
-import { DefaultTemplate } from './templates/default';
+import { ForgeCard, ForgeColorPicker } from '@tylertech/forge-react';
+
+const MDX = require('./color-picker.mdx').default;
 
 export default {
   title: 'Components/Color Picker',
@@ -9,14 +11,26 @@ export default {
   parameters: { 
     docs: { 
       page: MDX
-    },
-  },
+    }
+  }
 } as Meta;
 
-export const Default = DefaultTemplate.bind({});
+export const Default: Story<IColorPickerProps> = ({
+  value = '#990000',
+  allowOpacity = true,
+  opacity = 1,
+}) => {
+  value = value.length === 7 ? value : '';
+
+  return (
+    <ForgeCard outlined style={{'--forge-card-padding': '0', display: 'inline-block'}}>
+      <ForgeColorPicker value={value} allowOpacity={allowOpacity} opacity={opacity} />
+    </ForgeCard>
+  );
+};
 Default.args = {
   value: '#990000',
   allowOpacity: true,
-  opacity: 1,
+  opacity: 1
 } as IColorPickerProps;
 
