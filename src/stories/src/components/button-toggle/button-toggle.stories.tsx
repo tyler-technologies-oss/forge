168c3c39e39f0b37f01fab022f4fd4d8c3c11192
Nichols, Kieran <kieran.nichols@tylertech.com>
2022-06-02 10:56:23 -0400

    chore(storybook): update button-toggle docs

--- src/stories/src/components/button-toggle/button-toggle.stories.tsx
+++ src/stories/src/components/button-toggle/button-toggle.stories.tsx
@@ -1,7 +1,11 @@
-import { Meta } from '@storybook/react';
-const MDX = require('./button-toggle.mdx').default;
+import React, { useEffect } from 'react';
+import { Meta, Story } from '@storybook/react';
 import { IButtonToggleGroupProps, argTypes } from './button-toggle-args';
-import { DefaultTemplate } from './templates/default';
+import { ForgeButtonToggle, ForgeButtonToggleGroup, ForgeIcon } from '@tylertech/forge-react';
+import { tylIconEmail, tylIconPhone } from '@tylertech/tyler-icons/standard';
+import { IconRegistry } from '@tylertech/forge';
+
+const MDX = require('./button-toggle.mdx').default;
 
 export default {
   title: 'Components/Button Toggle',
@@ -9,11 +13,47 @@ export default {
   parameters: { 
     docs: { 
       page: MDX
-    },
-  },
+    }
+  }
 } as Meta;
 
-export const Default = DefaultTemplate.bind({});
+export const Default: Story<IButtonToggleGroupProps> = ({
+  multiple = false, 
+  stretch = false, 
+  mandatory = false, 
+  vertical = false, 
+  dense = false, 
+  disabled = false,
+  hasLeading = false,
+  hasTrailing = false,
+}) => {
+  useEffect(() => {
+    IconRegistry.define([tylIconEmail, tylIconPhone])
+  }, []);
+  return (
+    <ForgeButtonToggleGroup
+      multiple={multiple} 
+      stretch={stretch} 
+      mandatory={mandatory} 
+      vertical={vertical} 
+      dense={dense} 
+      disabled={disabled}>
+      <ForgeButtonToggle value="email" button-aria-label="By email">
+        {hasLeading && <ForgeIcon slot="leading" name="email" />}
+        By email
+        {hasTrailing && <ForgeIcon slot="trailing" name="email" />}
+      </ForgeButtonToggle>
+      <ForgeButtonToggle value="mail" button-aria-label="By mail">
+        By mail
+      </ForgeButtonToggle>
+      <ForgeButtonToggle value="phone" button-aria-label="By phone">
+        {hasLeading && <ForgeIcon slot="leading" name="email" />}
+        By phone
+        {hasTrailing && <ForgeIcon slot="trailing" name="phone" />}
+      </ForgeButtonToggle>
+    </ForgeButtonToggleGroup>
+  );
+};
 Default.args = {
   multiple: false, 
   stretch: false, 
--- src/stories/src/components/button-toggle/code/button-toggle-icons.ts
+++ src/stories/src/components/button-toggle/code/button-toggle-icons.ts
@@ -1,21 +1,6 @@
-export const ButtonToggleLeadingCodeHtml = () => {
-  return `<forge-button-toggle-group>
-  <forge-button-toggle value="email" aria-label="By email">
-    <forge-icon slot="leading" name="email"></forge-icon>
-    By email
-  </forge-button-toggle>
-  <forge-button-toggle value="mail" aria-label="By mail">
-    By mail
-  </forge-button-toggle>
-  <forge-button-toggle value="phone" aria-label="By phone">
-    <forge-icon slot="leading" name="phone"></forge-icon>
-    By phone
-  </forge-button-toggle>
-</forge-button-toggle-group>`;
-};
-
 export const ButtonToggleTrailingCodeHtml = () => {
-  return `<forge-button-toggle-group>
+  return `
+<forge-button-toggle-group>
   <forge-button-toggle value="email" aria-label="By email">
     By email
     <forge-icon slot="trailing" name="email"></forge-icon>
@@ -27,5 +12,6 @@ export const ButtonToggleTrailingCodeHtml = () => {
     By phone
     <forge-icon slot="trailing" name="phone"></forge-icon>
   </forge-button-toggle>
-</forge-button-toggle-group>`;
+</forge-button-toggle-group>
+  `;
 };
