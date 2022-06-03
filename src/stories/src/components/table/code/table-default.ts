15f14bd2b975ce42875f8e3f9234771a713d2043
Nichols, Kieran <kieran.nichols@tylertech.com>
2022-06-03 12:39:39 -0400

    chore(storybook): update table docs

--- src/stories/src/components/table/code/table-default.ts
+++ src/stories/src/components/table/code/table-default.ts
@@ -5,13 +5,40 @@ export const TableDefaultHtml = () => `
 export const TableDefaultTs = () => `
 const table = document.querySelector('forge-table');
 table.data = [
-  { Id: 1, Name: 'First1 Last1', Description: 'Test description 1', Age: '24' },
-  { Id: 2, Name: 'First2 Last2', Description: 'Test description 2', Age: '25' },
-  { Id: 3, Name: 'First3 Last3', Description: 'Test description 3', Age: '26' }
+  { id: 1, name: 'First1 Last1', description: 'Test description 1', age: '25' },
+  { id: 2, name: 'First2 Last2', description: 'Test description 2', age: '26' },
+  { id: 3, name: 'First3 Last3', description: 'Test description 3', age: '27' },
+  { id: 4, name: 'First4 Last4', description: 'Test description 4', age: '28' },
+  { id: 5, name: 'First5 Last5', description: 'Test description 5', age: '29' },
+  { id: 6, name: 'First6 Last6', description: 'Test description 6', age: '30' },
+  { id: 7, name: 'First7 Last7', description: 'Test description 7', age: '31' },
+  { id: 8, name: 'First8 Last8', description: 'Test description 8', age: '32' },
+  { id: 9, name: 'First9 Last9', description: 'Test description 9', age: '33' },
+  { id: 10, name: 'First10 Last10', description: 'Test description 10', age: '34' }
 ];
 table.columnConfigurations = [
-  { property: 'Name', header: 'Name', sortable: true, initialSort: true },
-  { property: 'Description', header: 'Description' },
-  { property: 'Age', header: 'Age', sortable: true, align: 'right' }
+  {
+    property: 'name',
+    header: 'Name',
+    sortable: true,
+    initialSort: true,
+    filter: true,
+    filterDelegate: new TextFieldComponentDelegate({ options: { placeholder: 'Filter name...' } })
+  },
+  {
+    property: 'description',
+    header: 'Description',
+    sortable: true,
+    filter: true,
+    filterDelegate: new TextFieldComponentDelegate({ options: { placeholder: 'Filter description...' } })
+  },
+  {
+    property: 'age',
+    header: 'Age',
+    sortable: true,
+    align: CellAlign.Right,
+    filter: true,
+    filterDelegate: new TextFieldComponentDelegate({ options: { placeholder: 'Filter age...', type: 'number' } })
+  }
 ];
 `;
