badb7f65b032cb104cd3535e7b66b127ad6a8a0a
Nichols, Kieran <kieran.nichols@tylertech.com>
2022-06-06 11:06:03 -0400

    chore(storybook): update select docs

--- src/stories/src/components/select/select.stories.tsx
+++ src/stories/src/components/select/select.stories.tsx
@@ -1,6 +1,10 @@
-import { Meta } from '@storybook/react';
-import { DefaultTemplate } from './templates/default';
+import React, { useEffect } from 'react';
+import { Meta, Story } from '@storybook/react';
+import { ForgeIcon, ForgeIconButton, ForgeOption, ForgeSelect } from '@tylertech/forge-react';
+import { IconRegistry } from '@tylertech/forge';
+import { tylIconFastfood } from '@tylertech/tyler-icons/standard';
 import { ISelectProps, argTypes } from './select-args';
+
 const MDX = require('./select.mdx').default;
 
 export default {
@@ -9,11 +13,54 @@ export default {
   parameters: { 
     docs: { 
       page: MDX
-    },
-  },
+    }
+  }
 } as Meta;
 
-export const Default = DefaultTemplate.bind({});
+export const Default: Story<ISelectProps> = ({
+  label = 'Food group',
+  multiple = false,
+  open = false,
+  disabled = false,
+  invalid = false,
+  required = false,
+  density = 'default',
+  floatLabelType = 'default',
+  shape = 'default',
+  placeholder = '',
+  hasLeadingIcon = false,
+  hasHelperText = false,
+  hasAddonEnd = false
+}) => {
+  useEffect(() => {
+    IconRegistry.define(tylIconFastfood)
+  }, []);
+
+  return (
+    <ForgeSelect 
+      label={label}
+      multiple={multiple}
+      open={open}
+      disabled={disabled}
+      invalid={invalid}
+      required={required}
+      density={density}
+      floatLabelType={floatLabelType}
+      shape={shape}
+      placeholder={placeholder}
+      style={{ width: '259px' }}>
+      {hasLeadingIcon && <ForgeIcon slot="leading" name="fastfood" />}
+      <ForgeOption value="grains">Bread, Cereal, Rice, and Pasta</ForgeOption>
+      <ForgeOption value="vegetables">Vegetables</ForgeOption>
+      <ForgeOption value="fruit">Fruit</ForgeOption>
+      {hasHelperText && <span slot="helper-text">Food group is required</span>}
+      {hasAddonEnd &&
+        <ForgeIconButton slot="addon-end" dense density-level={density === 'dense' ? 6 : 3}>
+          <button type="button" className="tyler-icons">more_vert</button>
+        </ForgeIconButton>}
+    </ForgeSelect>
+  );
+};
 Default.args = {
   density: 'default',
   floatLabelType: 'auto',
