82e8d736a88c3c491d946843ec8fcec08407b4f5
Nichols, Kieran <kieran.nichols@tylertech.com>
2022-06-06 13:01:41 -0400

    chore(storybook): update quantity-field docs

--- src/stories/src/components/quantity-field/quantity-field.stories.tsx
+++ src/stories/src/components/quantity-field/quantity-field.stories.tsx
@@ -1,6 +1,8 @@
-import { Meta } from '@storybook/react';
+import React from 'react';
+import { Meta, Story } from '@storybook/react';
+import { ForgeIconButton, ForgeQuantityField, ForgeTextField } from '@tylertech/forge-react';
 import { IQuantityFieldProps, argTypes } from './quantity-field-args';
-import { DefaultTemplate } from './templates/default';
+
 const MDX = require('./quantity-field.mdx').default;
 
 export default {
@@ -9,12 +11,28 @@ export default {
   parameters: { 
     docs: { 
       page: MDX
-    },
-  },
+    }
+  }
 } as Meta;
 
-export const Default = DefaultTemplate.bind({});
+export const Default: Story<IQuantityFieldProps> = ({ invalid = false, required = false }) => {
+  return (
+    <ForgeQuantityField invalid={invalid} required={required}>
+      <label slot="label" htmlFor="input-numeric">Label</label>
+      <ForgeIconButton slot="decrement-button">
+        <button type="button" className="tyler-icons" aria-label="Decrement">remove_circle_outline</button>
+      </ForgeIconButton>
+      <ForgeTextField>
+        <input type="number" id="input-numeric" style={{ textAlign: 'center' }} defaultValue="4" step="2" min="-10" max="10"/>
+      </ForgeTextField>
+      <ForgeIconButton slot="increment-button">
+        <button type="button" className="tyler-icons" aria-label="Increment">control_point</button>
+      </ForgeIconButton>
+      <div slot="helper-text">This is helpful text</div>
+    </ForgeQuantityField>
+  );
+};
 Default.args = {
   invalid: false,
-  required: false,
+  required: false
 } as IQuantityFieldProps;
