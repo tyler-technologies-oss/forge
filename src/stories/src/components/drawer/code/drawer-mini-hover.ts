d9afaf23a8f5955395dce7088c80b3dcc78ea367
Nichols, Kieran <kieran.nichols@tylertech.com>
2022-06-07 11:00:07 -0400

    chore(storybook): update drawer docs

--- src/stories/src/components/drawer/code/drawer-mini-hover.ts
+++ src/stories/src/components/drawer/code/drawer-mini-hover.ts
@@ -1,7 +1,3 @@
 export const DrawerMiniHoverCodeHtml = () => {
   return `<forge-mini-drawer hover></forge-mini-drawer>`;
 };
-
-export const DrawerMiniHoverCodeBlazor = () => {
-  return `<ForgeMiniDrawer Hover="" />`;
-};
