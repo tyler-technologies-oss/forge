71a5e44b179c6929507528adca2f0a049f311744
Nichols, Kieran <kieran.nichols@tylertech.com>
2022-08-26 14:48:56 -0400

    chore: use spaces vs tabs for code snippets

--- src/stories/src/components/split-view/code/split-view-leading.ts
+++ src/stories/src/components/split-view/code/split-view-leading.ts
@@ -1,12 +1,12 @@
 export const SplitViewLeadingCodeHtml = () => {
     return `
 <forge-split-view>
-    <forge-split-view-panel position="start">
-        <div>Panel 1</div>
-    </forge-split-view-panel>
-    <forge-split-view-panel>
-        <div>Panel 2</div>
-    </forge-split-view-panel>
+  <forge-split-view-panel position="start">
+    <div>Panel 1</div>
+  </forge-split-view-panel>
+  <forge-split-view-panel>
+    <div>Panel 2</div>
+  </forge-split-view-panel>
 </forge-split-view>
     `;
 };
--- src/stories/src/components/split-view/code/split-view-multiple-split.ts
+++ src/stories/src/components/split-view/code/split-view-multiple-split.ts
@@ -1,15 +1,15 @@
 export const SplitViewMultipleSplitCodeHtml = () => {
     return `
 <forge-split-view>
-    <forge-split-view-panel position="start">
-        <div>Panel 1</div>
-    </forge-split-view-panel>
-    <forge-split-view-panel>
-        <div>Panel 2</div>
-    </forge-split-view-panel>
-    <forge-split-view-panel position="end">
-        <div>Panel 3</div>
-    </forge-split-view-panel>
+  <forge-split-view-panel position="start">
+    <div>Panel 1</div>
+  </forge-split-view-panel>
+  <forge-split-view-panel>
+    <div>Panel 2</div>
+  </forge-split-view-panel>
+  <forge-split-view-panel position="end">
+    <div>Panel 3</div>
+  </forge-split-view-panel>
 </forge-split-view>
     `;
 };
