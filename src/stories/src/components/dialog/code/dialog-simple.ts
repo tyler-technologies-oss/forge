3d938ed44bad135ce66a6046918ad7e8d7489ef5
Kieran Nichols <kieran.nichols@tylertech.com>
2023-01-05 15:15:36 -0500

    [next] fix dialog story (#211)

--- src/stories/src/components/dialog/code/dialog-simple.ts
+++ src/stories/src/components/dialog/code/dialog-simple.ts
@@ -1,27 +1,29 @@
 export const DialogSimpleCodeHtml = () => `
-<template id="dialog-content">
+<forge-button type="raised">
+  <button type="button" id="my-button">Show dialog</button>
+</forge-button>
+
+<forge-dialog id="my-dialog" aria-labelledby="dialog-title" aria-describedby="dialog-message">
   <header class="forge-dialog__header" forge-dialog-move-target>
-    <h2 class="forge-dialog__title">Dialog header</h2>
+    <h2 id="dialog-title" class="forge-dialog__title">Dialog header</h2>
   </header>
-  <section class="forge-dialog__body">
+
+  <p id="dialog-message" class="forge-dialog__body">
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam perspiciatis ea laboriosam alias quas incidunt
     distinctio est praesentium. Sed saepe eius, voluptatibus officia dolores recusandae cum. Molestias est
     numquam odio.
-  </section>
+  </p>
+
   <footer class="forge-dialog__footer">
-    <forge-button type="outlined" style="margin-right: 16px">
-      <button type="button" id="cancel-button">Cancel</button>
-    </forge-button>
     <forge-button type="raised">
-      <button type="button" id="accept-button" forge-dialog-focus>Submit</button>
+      <button type="button" id="cancel-button">Close</button>
     </forge-button>
   </footer>
 </template>
 `;
 
 export const DialogSimpleCodeTs = () => `
-const dialog = document.createElement('forge-dialog');
-const dialogTemplate = document.getElementById('dialog-content');
-dialog.appendChild(dialogTemplate.content.cloneNode(true));
-dialog.open = true; // This will append the dialog to the body automatically and start the open animation
+const dialog = document.getElementById('my-dialog');
+const button = document.getElementById('my-button');
+button.addEventListener('click', () => dialog.open = true);
 `;
