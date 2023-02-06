af1d826aaff6a9ac4ffe9f73bfdcbebf8c4894cc
nickonometry <nick.andrews@tylertech.com>
2023-02-06 10:10:28 -0500

    feat(list): added badge demos for leading and trailing slots

---
+++ src/stories/src/components/list/code/list-leading-badge.ts
@@ -0,0 +1,27 @@
+export const ListLeadingAvatarCodeHtml = () => {
+    return `
+  <forge-list>
+    <forge-list-item two-line>
+      <div slot="leading">badge</div>
+      <span slot="title">Dog Photos</span>
+      <span slot="subtitle">9 Jan 2018</span>
+    </forge-list-item>
+    <forge-list-item two-line>
+    <div slot="leading">badge</div>
+      <span slot="title">Cat Photos</span>
+      <span slot="subtitle">22 Dec 2017</span>
+    </forge-list-item>
+    <forge-list-item two-line>
+    <div slot="leading">badge</div>
+      <span slot="title">Potatoes</span>
+      <span slot="subtitle">30 Nov 2017</span>
+    </forge-list-item>
+    <forge-list-item two-line>
+    <div slot="leading">badge</div>
+      <span slot="title">Carrots</span>
+      <span slot="subtitle">17 Oct 2017</span>
+    </forge-list-item>
+  </forge-list>
+    `;
+  };
+  
