f336d85e323029587c83f173a82c334fd234e4c5
Nichols, Kieran <kieran.nichols@tylertech.com>
2023-02-07 15:02:17 -0500

    chore: migrate icon demo

---
+++ src/dev/pages/icon/icon.ts
@@ -0,0 +1,12 @@
+import '$src/shared';
+import '@tylertech/forge/icon';
+import { IconRegistry } from '@tylertech/forge/icon';
+import { tylIconActionLauncher } from '@tylertech/tyler-icons/custom';
+import { tylIconCode, tylIconFace, tylIconPalette } from '@tylertech/tyler-icons/standard';
+
+IconRegistry.define([
+  tylIconFace,
+  tylIconCode,
+  tylIconActionLauncher,
+  tylIconPalette
+]);
