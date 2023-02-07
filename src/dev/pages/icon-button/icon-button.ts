40c1f32e0a1f5d39477fb949d05fc859f28c5110
Nichols, Kieran <kieran.nichols@tylertech.com>
2023-02-07 15:29:54 -0500

    chore: migrate fab demo

--- src/dev/pages/icon-button/icon-button.ts
+++ src/dev/pages/icon-button/icon-button.ts
@@ -1,6 +1,7 @@
 import '$src/shared';
 import '@tylertech/forge/icon';
 import '@tylertech/forge/icon-button';
+import '@tylertech/forge/icon-button/forge-icon-button.scss';
 import { IconRegistry } from '@tylertech/forge/icon';
 import { tylIconCode, tylIconFavorite, tylIconFavoriteBorder } from '@tylertech/tyler-icons/standard';
 
