59bd88909d1646d51d5962de82c4ac60fe3dfd09
nickonometry <nick.andrews@tylertech.com>
2023-04-28 13:18:25 -0400

    chore: code cleanup and unused imports cleanup

--- src/lib/stack/stack-adapter.ts
+++ src/lib/stack/stack-adapter.ts
@@ -1,7 +1,7 @@
-import { getShadowElement, toggleClass } from '@tylertech/forge-core';
+import { getShadowElement } from '@tylertech/forge-core';
+import { BaseAdapter, IBaseAdapter } from '../core/base/base-adapter';
 import { IStackComponent } from './stack';
 import { STACK_CONSTANTS } from './stack-constants';
-import { BaseAdapter, IBaseAdapter } from '../core/base/base-adapter';
 
 export interface IStackAdapter extends IBaseAdapter {
   setGap: (gap: number) => void;
