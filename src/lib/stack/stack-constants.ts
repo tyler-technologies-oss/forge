2efbc6bd1ea8c2f70cf91e3c87649de115291127
nickonometry <nick.andrews@tylertech.com>
2023-04-28 13:17:17 -0400

    chore: removed unsed classes from the constants file

--- src/lib/stack/stack-constants.ts
+++ src/lib/stack/stack-constants.ts
@@ -4,8 +4,6 @@ const elementName = `${COMPONENT_NAME_PREFIX}stack`;
 
 const classes = {
   DEFAULT: 'forge-stack',
-  INLINE: 'forge-stack--inline',
-  WRAP: 'forge-stack--inline--wrap'
 };
 
 const attributes = {
