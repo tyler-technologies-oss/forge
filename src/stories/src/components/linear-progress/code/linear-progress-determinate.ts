b4066646139115f9ebc43328b72a74c60e3dc93d
Nichols, Kieran <kieran.nichols@tylertech.com>
2023-05-01 13:11:39 -0400

    docs(linear-progress): updated docs to reflect new default buffer value state

--- src/stories/src/components/linear-progress/code/linear-progress-determinate.ts
+++ src/stories/src/components/linear-progress/code/linear-progress-determinate.ts
@@ -1,3 +1,3 @@
 export const LinearProgressDeterminateCodeHtml = () => {
-  return `<forge-linear-progress determinate progress="0.5" buffer="1"></forge-linear-progress>`;
+  return `<forge-linear-progress determinate progress="0.5"></forge-linear-progress>`;
 };
