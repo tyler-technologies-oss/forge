4ba5f5fafa75f2d96ceaff31dbc1a691ca9c2ae9
Sam Richardson <sam.richardson@tylertech.com>
2023-06-15 21:29:25 -0400

    chore(button-area): add expansion panel demo and better a11y to dev page

--- src/dev/pages/button-area/button-area.ts
+++ src/dev/pages/button-area/button-area.ts
@@ -1,4 +1,4 @@
-import { IButtonAreaComponent, ISwitchComponent, IconRegistry } from '@tylertech/forge';
+import { IButtonAreaComponent, IExpansionPanelComponent, ISwitchComponent, IconRegistry } from '@tylertech/forge';
 import { tylIconChevronRight, tylIconFavorite } from '@tylertech/tyler-icons/standard';
 
 import '$src/shared';
@@ -13,7 +13,16 @@ IconRegistry.define([
 const buttonArea = document.getElementById('button-area') as IButtonAreaComponent;
 buttonArea.addEventListener('click', () => alert('Click'));
 
+const expansionPanel = document.getElementById('expansion-panel') as IExpansionPanelComponent;
+const expansionPanelButtonArea = document.getElementById('expansion-panel-button-area') as IButtonAreaComponent;
+const expansionPanelButton = document.getElementById('expansion-panel-button');
+expansionPanel.addEventListener('forge-expansion-panel-toggle', (event: CustomEvent<boolean>) => {
+  expansionPanelButton.setAttribute('aria-expanded', event.detail.toString());
+});
+
+
 const disabledToggle = document.querySelector('#disabled-switch') as ISwitchComponent;
 disabledToggle.addEventListener('forge-switch-select', ({ detail: selected }) => {
   buttonArea.disabled = selected;
+  expansionPanelButtonArea.disabled = selected;
 });
