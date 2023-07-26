98e8b9916d41795d3eccdb179265815daddfb759
Nichols, Kieran <kieran.nichols@tylertech.com>
2023-07-26 12:51:41 -0400

    fix(field): fixed a bug where the floating label would stop responding after rerendered with the label already floating

--- src/lib/field/field-foundation.ts
+++ src/lib/field/field-foundation.ts
@@ -93,6 +93,9 @@ export class FieldFoundation {
     this._isInitialized = false;
     this._adapter.destroy();
 
+    this._adapter.removeHostAttribute(FIELD_CONSTANTS.attributes.HOST_LABEL_FLOATING);
+    this._adapter.removeRootClass(FIELD_CONSTANTS.classes.LABEL);
+
     if (this._floatingLabel) {
       this._floatingLabel.destroy();
       this._floatingLabel = undefined;
