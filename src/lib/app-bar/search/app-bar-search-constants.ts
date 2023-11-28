9f3458f121e3ccd69d9e4d81ca5cee300fff327c
Kieran Nichols <kieran.nichols@tylertech.com>
2023-11-28 09:49:42 -0500

    feat(app-bar-search): refactored to use tokens and updated patterns and API (#438)

--- src/lib/app-bar/search/app-bar-search-constants.ts
+++ src/lib/app-bar/search/app-bar-search-constants.ts
@@ -1,52 +1,29 @@
 import { COMPONENT_NAME_PREFIX } from '../../constants';
-import { IOption } from '../../select';
 
 const elementName: keyof HTMLElementTagNameMap = `${COMPONENT_NAME_PREFIX}app-bar-search`;
 
-const classes = {
-  FOCUSED: 'forge-app-bar-search--focused',
-  DISABLED: 'forge-app-bar-search--disabled'
-};
-
 const selectors = {
   ROOT: '.forge-app-bar-search',
-  INPUT: 'input',
-  GLOBAL_ICON_CONTAINER: '.forge-app-bar-search__global-icon-container',
-  CONTEXT_CONTAINER: '.forge-app-bar-search__context'
+  INPUT: 'input'
 };
 
 const attributes = {
   DISABLED: 'disabled',
   VALUE: 'value',
-  PLACEHOLDER: 'placeholder',
-  COMBINED: 'combined',
-  GLOBAL: 'global'
+  PLACEHOLDER: 'placeholder'
 };
 
 const events = {
   INPUT: `${elementName}-input`
 };
 
-const strings = {
-  DEFAULT_CONTEXT: 'global'
-};
-
 export const APP_BAR_SEARCH_CONSTANTS = {
   elementName,
-  classes,
   selectors,
   attributes,
-  events,
-  strings
+  events
 };
-
-export const DEFAULT_COMBINED_OPTIONS: IOption[] = [
-  { label: 'in this application', value: 'local' },
-  { label: 'in all products', value: strings.DEFAULT_CONTEXT }
-];
 
 export interface IAppBarSearchInputEventData {
   value: string;
-  combined: boolean;
-  context: string;
 }
--- src/lib/app-bar/search/app-bar-search.ts
+++ src/lib/app-bar/search/app-bar-search.ts
@@ -1,10 +1,9 @@
-import { CustomElement, attachShadowTemplate, ensureChildren, coerceBoolean, FoundationProperty } from '@tylertech/forge-core';
-import { tylIconArrowDropDown, tylIconLanguage, tylIconSearch } from '@tylertech/tyler-icons/standard';
+import { CustomElement, attachShadowTemplate, coerceBoolean, FoundationProperty, ensureInputElement } from '@tylertech/forge-core';
+import { tylIconSearch } from '@tylertech/tyler-icons/standard';
 import { AppBarSearchFoundation } from './app-bar-search-foundation';
 import { AppBarSearchAdapter } from './app-bar-search-adapter';
 import { IAppBarSearchInputEventData, APP_BAR_SEARCH_CONSTANTS } from './app-bar-search-constants';
-import { IOption } from '../../select';
-import { TooltipComponent } from '../../tooltip';
+import { FocusIndicatorComponent } from '../../focus-indicator';
 import { IconComponent, IconRegistry } from '../../icon';
 import { BaseComponent, IBaseComponent } from '../../core/base/base-component';
 
@@ -15,10 +14,6 @@ export interface IAppBarSearchComponent extends IBaseComponent {
   disabled: boolean;
   value: string;
   placeholder: string;
-  combined: boolean;
-  combinedOptions: IOption[];
-  selectedCombinedOption: string;
-  global: boolean;
 }
 
 declare global {
@@ -32,8 +27,6 @@ declare global {
 }
 
 /**
- * The web component class behind the `<forge-app-bar-search>` custom element.
- * 
  * @tag forge-app-bar-search
  * 
  * @slot - The default (unnamed) slot is where child `<input>` elements will be placed.
@@ -63,8 +56,8 @@ declare global {
 @CustomElement({
   name: APP_BAR_SEARCH_CONSTANTS.elementName,
   dependencies: [
-    TooltipComponent,
-    IconComponent
+    IconComponent,
+    FocusIndicatorComponent
   ]
 })
 export class AppBarSearchComponent extends BaseComponent implements IAppBarSearchComponent {
@@ -72,32 +65,25 @@ export class AppBarSearchComponent extends BaseComponent implements IAppBarSearc
     return [
       APP_BAR_SEARCH_CONSTANTS.attributes.DISABLED,
       APP_BAR_SEARCH_CONSTANTS.attributes.VALUE,
-      APP_BAR_SEARCH_CONSTANTS.attributes.PLACEHOLDER,
-      APP_BAR_SEARCH_CONSTANTS.attributes.COMBINED,
-      APP_BAR_SEARCH_CONSTANTS.attributes.GLOBAL
+      APP_BAR_SEARCH_CONSTANTS.attributes.PLACEHOLDER
     ];
   }
 
-  /** The foundation class that handles business logic for this component. */
   private _foundation: AppBarSearchFoundation;
 
   constructor() {
     super();
-    IconRegistry.define([tylIconSearch, tylIconArrowDropDown, tylIconLanguage]);
+    IconRegistry.define(tylIconSearch);
     attachShadowTemplate(this, template, styles);
     this._foundation = new AppBarSearchFoundation(new AppBarSearchAdapter(this));
   }
 
   public connectedCallback(): void {
-    if (this.children.length) {
-      this._initialize();
+    if (this.querySelector(APP_BAR_SEARCH_CONSTANTS.selectors.INPUT)) {
+      this._foundation.initialize();
     } else {
-      ensureChildren(this).then(() => this._initialize());
+      ensureInputElement(this).then(() => this._foundation.initialize());
     }
-  }
-
-  private _initialize(): void {
-    this._foundation.initialize();
   }
 
   public disconnectedCallback(): void {
@@ -115,41 +101,15 @@ export class AppBarSearchComponent extends BaseComponent implements IAppBarSearc
       case APP_BAR_SEARCH_CONSTANTS.attributes.PLACEHOLDER:
         this.placeholder = newValue;
         break;
-      case APP_BAR_SEARCH_CONSTANTS.attributes.COMBINED:
-        this.combined = coerceBoolean(newValue);
-        break;
-      case APP_BAR_SEARCH_CONSTANTS.attributes.GLOBAL:
-        this.global = coerceBoolean(newValue);
-        break;
     }
   }
 
-  /** Gets/sets the value. */
   @FoundationProperty()
   public declare value: string;
 
-  /** Gets/sets the disabled state of the search input. */
   @FoundationProperty()
   public declare disabled: boolean;
   
-  /** Gets/sets whether this is a combined search box with search options. */
   @FoundationProperty()
-  public declare combined: boolean;
-  
-  /** Gets/sets seelcted option for the combined search dropdown. */
-  @FoundationProperty()
-  public declare combinedOptions: IOption[];
-  
-  /** Gets/sets selected option for the combined search dropdown. */
-  @FoundationProperty()
-  public declare selectedCombinedOption: string;
-  
-  /** Gets/sets whether the global icon is displayed at the end of the input. */
-  @FoundationProperty()
-  public declare global: boolean;
-
-  /** Sets the input placeholder value. */
-  public set placeholder(value: string) {
-    this._foundation.placeholder = value;
-  }
+  public declare placeholder: string;
 }
