0e99419c1f1f0871af55ed1b47101610f1be9693
nickonometry <nick.andrews@tylertech.com>
2024-02-26 09:02:20 -0500

    chore(toolbar): added in props, attributes, parts, and css custom props for docs

--- src/lib/toolbar/toolbar.ts
+++ src/lib/toolbar/toolbar.ts
@@ -17,9 +17,41 @@ declare global {
 }
 
 /**
- * The web component class behind the `<forge-toolbar>` custom element.
- * 
  * @tag forge-toolbar
+ * 
+ * @summary The toolbar component allows you to place titles and actions within a container and align them to the start, center, 
+ * or end of the toolbar. This component is useful as headers and footers within pages, dialogs, sections... etc. to ensure 
+ * consistent spacing and positioning.
+ * 
+ * @property {boolean} inverted - Controls whether a border bottom (default) or top (true) is used.
+ * 
+ * @attribute {boolean} inverted - Controls whether a border bottom (default) or top (true) is used.
+ * @attribute {boolean} no-border - Disables the internal border-top or border-bottom styles.
+ * @attribute {boolean} no-padding - Sets the internal padding style to 0.
+ * @attribute {boolean} auto-height - Forces the internal container to use height: auto for dynamic content that doesn't fit the static height.
+ * @cssproperty --forge-theme-height - Controls the height.
+ * @cssproperty --forge-theme-min-height - Controls the minimum height.
+ * @cssproperty --forge-theme-surface - Controls the background-color of the toolbar.
+ * @cssproperty --forge-toolbar-divider-color - Controls the border-color of the toolbar.
+ * @cssproperty --forge-toolbar-border-block-start-width - Controls the size of the top border.
+ * @cssproperty --forge-toolbar-border-block-end-width - Controls the size of the bottom border.
+ * @cssproperty --forge-toolbar-border-block-start-style - Controls the border style of the top border.
+ * @cssproperty --forge-toolbar-border-block-end-style - Controls the border style of the bottom border.
+ * @cssproperty --forge-toolbar-border-start-start-radius - Controls the border radius of the top left corner.
+ * @cssproperty --forge-toolbar-border-start-end-radius - Controls the border radius of the top right corner.
+ * @cssproperty --forge-toolbar-border-end-start-radius - Controls the border radius of the bottom left corner.
+ * @cssproperty --forge-toolbar-border-end-end-radius - Controls the border radius of the bottom right corner.
+ * @cssproperty --forge-toolbar-padding - Controls the left and right padding using the padding-inline style.
+ * @cssproperty --forge-toolbar-padding-block - Controls the top and bottom padding using the padding-block style.
+ * @cssproperty --forge-toolbar-padding-inline - Controls the left and right padding using the padding-block style.
+ * 
+ * @csspart container - The outer container element that runs edge to edge horizontally.
+ * @csspart root - The root container element with default inline-padding.
+ * @csspart before-section-start - The container element for the before-start slot.
+ * @csspart section-start - The container element for the start slot.
+ * @csspart section-center - The container element for the center slot.
+ * @csspart section-end - The container element for the end slot.
+ * @csspart after-section-end - The container element for the after-end slot.
  */
 @CustomElement({
   name: TOOLBAR_CONSTANTS.elementName
