b37410ad918a48257607af6c9a4a9e6f1d338468
Nick Andrews <nick.andrews@tylertech.com>
2024-02-28 14:17:09 -0500

    feat(product-icon): remove product icon component (#488)
    * chore(product-icon): removed product icon from the Forge 3 library
    
    * chore(product-icon): removed product icon from the CUSTOM_ELEMENTS array

--- src/stories/src/components/product-icon/code/product-icon-custom-shadow.ts
+++
@@ -1,5 +0,0 @@
-export const ProductIconCustomShadowHtml = () => `
-<forge-product-icon iterations="8">
-  <span slot="text">FB</span>
-</forge-product-icon>
-`;
--- src/stories/src/components/product-icon/code/product-icon-default.ts
+++
@@ -1,5 +0,0 @@
-export const ProductIconDefaultHtml = () => `
-<forge-product-icon>
-  <i class="tyler-icons">accessibility</i>
-</forge-product-icon>
-`;
