f050fdcc433f2b3ce8dc34397742289e99299457
Paul Lapczynski <paul.lapczynski@tylertech.com>
2024-03-05 11:21:02 -0500

    chore: cleanup patterns

--- src/lib/page-state/page-state.test.ts
+++ src/lib/page-state/page-state.test.ts
@@ -1,24 +1,12 @@
 import { expect } from '@esm-bundle/chai';
 import { fixture, html } from '@open-wc/testing';
 import { IPageStateComponent, PageStateComponent } from './page-state';
-import { PAGE_STATE_CONSTANTS } from './page-state-constants';
 import './page-state';
 
-interface ITestContext {
-  context: ITestPageStateContext
-}
-
-interface ITestPageStateContext {
-  component: PageStateComponent;
-  destroy(): void;
-}
-
 describe('PageStateComponent', () => {
-  const { elementName: pageState } = PAGE_STATE_CONSTANTS;
-
   it('should instantiate component instance', async () => {
-    const el = await fixture<IPageStateComponent>(html`<${pageState}></${pageState}>`);
-
-    expect(el).not.to.be.accessible();
+    const el = await fixture<IPageStateComponent>(html`<forge-page-state></forge-page-state>`);
+    expect(el).to.be.instanceOf(PageStateComponent);
+    expect(el.shadowRoot).not.to.be.null;
   });
 });
