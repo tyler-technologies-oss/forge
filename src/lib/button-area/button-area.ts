9df42edb77b5a74c9eeec3d6fe799efc0ac86ceb
Derek Moss <derek.moss@tylertech.com>
2024-03-06 06:17:46 -0800

    [button-area] refactor to use tokens, state-layer, and focus-indicator (#489)

--- src/lib/button-area/button-area.ts
+++ src/lib/button-area/button-area.ts
@@ -2,7 +2,8 @@ import { CustomElement, attachShadowTemplate, ICustomElement, FoundationProperty
 import { ButtonAreaAdapter } from './button-area-adapter';
 import { ButtonAreaFoundation } from './button-area-foundation';
 import { BUTTON_AREA_CONSTANTS } from './button-area-constants';
-import { RippleComponent } from '../ripple';
+import { FocusIndicatorComponent } from '../focus-indicator';
+import { StateLayerComponent } from '../state-layer';
 
 import template from './button-area.html';
 import styles from './button-area.scss';
@@ -18,14 +19,32 @@ declare global {
 }
 
 /**
- * The custom element class behind the `<forge-button-area>` element.
- * 
  * @tag forge-button-area
+ *
+ * @summary Button areas are used to create a clickable area that group related information and actions about a single subject.
+ *
+ * @description
+ * The button area component wraps any arbitrary content with a <button> element to enable accessible clickable interfaces including nested controls and other complex content.
+ *
+ * @property {boolean} disabled - Sets whether the button area and slotted button are disabled. Setting this on one will also set it on the other.
+ *
+ * @attribute {boolean} disabled - Sets whether the button area and slotted button are disabled. Setting this on one will also set it on the other.
+ *
+ * @event {Event} click - The button area emits a native HTML click event whenever it or the slotted button is clicked. Add the listener to the <forge-button-area> element to receive all events. Note: Set data-forge-ignore on any nested buttons or other interactive elements to prevent them from activating the button area.
+ *
+ * @csspart root - The root container element.
+ * @csspart button - The visually hidden slot for the <button> element.
+ * @csspart focus-indicator - The focus-indicator indicator element.
+ * @csspart state-layer - The state-layer surface element.
+ *
+ * @slot - Places content within the default (unnamed) slot (main body of the component).
+ * @slot button - Places content within a visually hidden slot. Always place a <button> element in this slot.
  */
 @CustomElement({
   name: BUTTON_AREA_CONSTANTS.elementName,
   dependencies: [
-    RippleComponent
+    FocusIndicatorComponent,
+    StateLayerComponent
   ]
 })
 export class ButtonAreaComponent extends HTMLElement implements IButtonAreaComponent {
