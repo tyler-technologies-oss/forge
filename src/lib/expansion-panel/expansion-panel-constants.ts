bbb3d1a5b14b73e19c919664eed02254db8c8a60
Kieran Nichols <kieran.nichols@tylertech.com>
2024-03-14 10:39:45 -0400

    feat(expansion-panel): refactor to use CSS (grid) based animation and convert to use tokens and updated patterns (#495)

--- src/lib/expansion-panel/expansion-panel-constants.ts
+++ src/lib/expansion-panel/expansion-panel-constants.ts
@@ -3,50 +3,41 @@ import { OPEN_ICON_CONSTANTS } from '../open-icon';
 
 const elementName: keyof HTMLElementTagNameMap = `${COMPONENT_NAME_PREFIX}expansion-panel`;
 
-const classes = {
-  CONTAINER: 'forge-expansion-panel',
-  HEADER: 'forge-expansion-panel__header',
-  CONTENT: 'forge-expansion-panel__content'
-};
-
-const selectors = {
-  CONTAINER: `.${classes.CONTAINER}`,
-  HEADER: `.${classes.HEADER}`,
-  CONTENT: `.${classes.CONTENT}`,
-  HEADER_SLOT: `.${classes.HEADER} > slot[name=header]`,
-  OPEN_ICON: `[slot=header] ${OPEN_ICON_CONSTANTS.elementName}`
-};
-
-const events = {
-  TOGGLE: `${elementName}-toggle`
+const observedAttributes = {
+  OPEN: 'open',
+  ORIENTATION: 'orientation',
+  ANIMATION_TYPE: 'animation-type'
 };
 
 const attributes = {
-  OPEN: 'open',
-  ORIENTATION: 'orientation',
-  USE_ANIMATIONS: 'use-animations',
-  IGNORE: 'data-forge-ignore',
-  IGNORE_ALT: 'forge-ignore'
+  ...observedAttributes,
+  OPENING: 'opening'
 };
 
-const numbers = {
-  COLLAPSE_ANIMATION_DURATION: 400,
-  CLICK_DEBOUNCE_THRESHOLD: 200
+const classes = {
+  HIDDEN: 'hidden'
 };
 
-const strings = {
-  ORIENTATION_VERTICAL: 'vertical',
-  ORIENTATION_HORIZONTAL: 'horizontal',
-  EXPANSION_VERTICAL_TRANSITION: `height ${numbers.COLLAPSE_ANIMATION_DURATION}ms cubic-bezier(0.4, 0, 0.2, 1), opacity ${numbers.COLLAPSE_ANIMATION_DURATION}ms ease-in-out`,
-  EXPANSION_HORIZONTAL_TRANSITION: `width ${numbers.COLLAPSE_ANIMATION_DURATION}ms cubic-bezier(0.4, 0, 0.2, 1), opacity ${numbers.COLLAPSE_ANIMATION_DURATION}ms ease-in-out`
+const selectors = {
+  HEADER: '.header',
+  CONTENT: '.content',
+  IGNORE: ':is([data-forge-ignore],[forge-ignore])',
+  OPEN_ICON: `:is([slot=header] ${OPEN_ICON_CONSTANTS.elementName}, ${OPEN_ICON_CONSTANTS.elementName}[slot^=header])`
+};
+
+const events = {
+  TOGGLE: `${elementName}-toggle`,
+  ANIMATION_COMPLETE: `${elementName}-animation-complete`
 };
 
 export const EXPANSION_PANEL_CONSTANTS = {
   elementName,
+  observedAttributes,
+  attributes,
   classes,
   selectors,
-  events,
-  attributes,
-  numbers,
-  strings
+  events
 };
+
+export type ExpansionPanelOrientation = 'horizontal' | 'vertical';
+export type ExpansionPanelAnimationType = 'default' | 'none';
