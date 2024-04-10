b8213fbb20777c8f9c63d59fb6344f849e6b6c43
Nichols, Kieran <kieran.nichols@tylertech.com>
2024-04-10 17:51:04 -0400

    chore: restore storybook demo

---
+++ src/stories/src/components/toast/code/toast-default.ts
@@ -0,0 +1,7 @@
+export const ToastDefaultHtml = () => `
+<forge-button>Show toast</forge-button>
+`;
+
+export const ToastDefaultTs = () => `
+ToastComponent.present({ message: 'Save successful' });
+`;
---
+++ src/stories/src/components/toast/toast-args.ts
@@ -0,0 +1,77 @@
+import { ToastPlacement } from '@tylertech/forge';
+
+export interface IToastProps {
+  message: string;
+  actionText: string;
+  duration: number;
+  placement: ToastPlacement;
+  dismissible: boolean;
+}
+
+export const argTypes = {
+  message: {
+    control: 'text',
+    description: '',
+    table: {
+      category: 'Properties',
+    },
+  },
+  actionText: {
+    control: 'text',
+    description: '',
+    table: {
+      category: 'Properties',
+    },
+  },
+  duration: {
+    control: 'number',
+    description: '',
+    table: {
+      category: 'Properties',
+    },
+  },
+  placement: {
+    control: {
+      type: 'select',
+      labels: {
+        'top-start': 'Top start',
+        'top': 'Top',
+        'top-end': 'Top end',
+        'right-start': 'Right start',
+        'right': 'Right',
+        'right-end': 'Right end',
+        'bottom-start': 'Bottom start',
+        'bottom': 'Bottom',
+        'bottom-end': 'Bottom end',
+        'left-start': 'Left start',
+        'left': 'Left',
+        'left-end': 'Right end'
+      },
+    },
+    options: [
+      'top', 
+      'bottom', 
+      'left', 
+      'right', 
+      'top-left', 
+      'top-right',
+      'bottom-left', 
+      'bottom-right',
+      'left-top',
+      'left-bottom',
+      'right-top',
+      'right-bottom',
+    ],
+    description: '',
+    table: {
+      category: 'Properties',
+    },
+  },
+  dismissible: {
+    control: 'boolean',
+    description: '',
+    table: {
+      category: 'Properties',
+    },
+  },
+};
