8b4c4758123bb5ae9dc52e08303b70f02291776e
Nichols, Kieran <kieran.nichols@tylertech.com>
2024-04-19 16:03:43 -0400

    chore(open-icon): add JSDoc

--- src/lib/open-icon/open-icon.ts
+++ src/lib/open-icon/open-icon.ts
@@ -20,6 +20,29 @@ declare global {
 
 /**
  * @tag forge-open-icon
+ * 
+ * @summary Open icons are used to indicate whether a section is open or closed.
+ * 
+ * @property {boolean} open - Whether the icon is open or closed.
+ * @property {OpenIconOrientation} orientation - The orientation of the icon.
+ * 
+ * @attribute {boolean} open - Whether the icon is open or closed.
+ * @attribute {OpenIconOrientation} orientation - The orientation of the icon.
+ * 
+ * @cssproperty --forge-open-icon-color - The color of the icon.
+ * @cssproperty --forge-open-icon-size - The size of the icon.
+ * @cssproperty --forge-open-icon-height - The height of the icon. Defaults to `size`.
+ * @cssproperty --forge-open-icon-width - The width of the icon. Defaults to `size`.
+ * @cssproperty --forge-open-icon-initial-rotation - The initial rotation of the icon. 
+ * @cssproperty --forge-open-icon-open-rotation - The rotation of the icon when open.
+ * @cssproperty --forge-open-icon-animation-duration - The duration of the open animation.
+ * @cssproperty --forge-open-icon-half-animation-duration - The duration of the open animation when in a half orientation.
+ * @cssproperty --forge-open-icon-animation-timing - The timing function of the open animation.
+ * 
+ * @csspart root - The root element of the icon.
+ * @csspart icon - The icon element.
+ * 
+ * @slot - The icon to display when open.
  */
 @CustomElement({
   name: OPEN_ICON_CONSTANTS.elementName,
