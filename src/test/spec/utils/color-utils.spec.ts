bbf954b9fc3ce2176126d69858946bef92d338a4
Kieran Nichols <kieran.nichols@tylertech.com>
2024-04-23 16:26:47 -0400

    feat: removed MDC dependencies and replace legacy typography and theme system (#541)

--- src/test/spec/utils/color-utils.spec.ts
+++ src/test/spec/utils/color-utils.spec.ts
@@ -1,4 +1,4 @@
-import { getTextColor, randomHexColor } from '@tylertech/forge/utils/color-utils';
+import { randomHexColor } from '@tylertech/forge/utils/color-utils';
 
 interface ITestContext {}
 
@@ -8,27 +8,6 @@ describe('ColorUtils', function(this: ITestContext) {
       const hex = randomHexColor();
 
       expect(hex.length).toBe(7);
-    });
-  });
-
-  describe(`getTextColor`, function(this: ITestContext) {
-    it('should return color', function(this: ITestContext) {
-      const a = getTextColor('A');
-
-      expect(a).toBe('#EF5350');
-    });
-
-    it('should return random color for non alphabetical', function(this: ITestContext) {
-      const one = getTextColor('1');
-      const juan = getTextColor('1');
-
-      expect(one).not.toBe(juan);
-    });
-
-    it('should return undefined color for empty string', function(this: ITestContext) {
-      const one = getTextColor('');
-
-      expect(one).toBeUndefined();
     });
   });
 });
