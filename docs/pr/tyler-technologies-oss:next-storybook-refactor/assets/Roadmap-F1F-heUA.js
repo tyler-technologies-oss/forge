963c6bc644d7d95cabb2544cea51e97120b2b588
DRiFTy17 <DRiFTy17@users.noreply.github.com>
2024-05-23 14:06:21 +0000

    Deploying to gh-pages from @ tyler-technologies-oss/forge@337a183a5f605c62811281d2e15fb8c0bfc7f131 🚀

---
+++ docs/pr/tyler-technologies-oss:next-storybook-refactor/assets/Roadmap-F1F-heUA.js
@@ -0,0 +1,30 @@
+import{j as e}from"./jsx-runtime-C-8lrgAy.js";import{u as s}from"./index-B-31WWn-.js";import{M as o}from"./index-CixprI-j.js";import"./iframe-B-z9UajK.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./chunk-QN4WKJDJ-Bf_F3oir.js";import"./index-DXimoRZY.js";import"./index-DvzDrELh.js";import"./index-DrFu-skq.js";function r(n){const t={a:"a",blockquote:"blockquote",code:"code",h1:"h1",h2:"h2",p:"p",table:"table",tbody:"tbody",td:"td",th:"th",thead:"thead",tr:"tr",...s(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(o,{title:"About/Roadmap"}),`
+`,e.jsx(t.h1,{id:"roadmap",children:"Roadmap"}),`
+`,e.jsx(t.p,{children:`Tyler Forge is constantly evolving to keep up with the latest trends in web development, as well as the needs of our users. Below is a
+list of features that we are currently working on, or have planned for the future. We'll update this list as we make progress on each
+feature, so be sure to check back often!`}),`
+`,e.jsxs(t.blockquote,{children:[`
+`,e.jsxs(t.p,{children:["If you have any feature requests, or would like to see a feature moved up in priority, please let us know by ",e.jsx(t.a,{href:"https://github.com/tyler-technologies-oss/forge/issues",rel:"nofollow",children:"opening an issue"}),`
+or by adding an item to our `,e.jsx(t.a,{href:"https://tylerforge.canny.io/feature-requests",rel:"nofollow",children:"Canny board"}),"."]}),`
+`]}),`
+`,e.jsx(t.h2,{id:"in-progress",children:"In Progress"}),`
+`,e.jsx(t.p,{children:"These are features that we are currently working on, and are expected to be released in the near future."}),`
+`,e.jsxs(t.table,{children:[e.jsx(t.thead,{children:e.jsxs(t.tr,{children:[e.jsx(t.th,{children:"Feature"}),e.jsx(t.th,{children:"Status"}),e.jsx(t.th,{children:"Description"})]})}),e.jsxs(t.tbody,{children:[e.jsxs(t.tr,{children:[e.jsx(t.td,{children:"CSS-only Components"}),e.jsx(t.td,{children:"In Progress"}),e.jsx(t.td,{children:"A set of CSS classes distributed via pre-built stylesheets and Sass mixins for creating components without JavaScript."})]}),e.jsxs(t.tr,{children:[e.jsx(t.td,{children:"Global Configuration"}),e.jsx(t.td,{children:"In Progress"}),e.jsx(t.td,{children:"A new mechanism for configuring global default values for various Forge components."})]})]})]}),`
+`,e.jsx(t.h2,{id:"not-started",children:"Not Started"}),`
+`,e.jsx(t.p,{children:"These are features that we have planned for the future, but have not yet started working on."}),`
+`,e.jsxs(t.table,{children:[e.jsx(t.thead,{children:e.jsxs(t.tr,{children:[e.jsx(t.th,{children:"Feature"}),e.jsx(t.th,{children:"Status"}),e.jsx(t.th,{children:"Description"})]})}),e.jsxs(t.tbody,{children:[e.jsxs(t.tr,{children:[e.jsx(t.td,{children:"Inline Select Dropdown"}),e.jsx(t.td,{children:"Not Started"}),e.jsxs(t.td,{children:["A new select component that allows for directly rendering HTML in the ",e.jsx(t.code,{children:"<forge-option>"})," elements."]})]}),e.jsxs(t.tr,{children:[e.jsx(t.td,{children:"Inline Menu Dropdown"}),e.jsx(t.td,{children:"Not Started"}),e.jsx(t.td,{children:"A new menu component that can be rendered inline with other content."})]}),e.jsxs(t.tr,{children:[e.jsx(t.td,{children:"Inline Autocomplete Dropdown"}),e.jsx(t.td,{children:"Not Started"}),e.jsx(t.td,{children:"A new autocomplete component that can be rendered inline with other content."})]})]})]}),`
+`,e.jsx("style",{children:`
+table {
+  width: 100%;
+  border-collapse: collapse;
+}
+
+td:first-child {
+  width: 250px;
+}
+
+td:first-child,
+td:nth-child(2) {
+  white-space: nowrap;
+}
+`})]})}function m(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(r,{...n})}):r(n)}export{m as default};
