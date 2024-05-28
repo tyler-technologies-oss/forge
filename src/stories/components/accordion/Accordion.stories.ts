2903a21ba6d2c8da6cc5b8bff6e6e0ae406212d5
Paul Lapczynski <70207430+tyl-paul-lapczynski@users.noreply.github.com>
2024-05-28 12:44:52 -0400

    Next storybook refactor accordion (#579)
    * feat: accordion

---
+++ src/stories/components/accordion/Accordion.stories.ts
@@ -0,0 +1,66 @@
+import { html } from 'lit';
+import { type Meta, type StoryObj } from '@storybook/web-components';
+import { customElementStoryRenderer, generateCustomElementArgTypes, standaloneStoryParams } from '../../utils';
+
+import '@tylertech/forge/accordion';
+import '@tylertech/forge/expansion-panel';
+import '@tylertech/forge/divider';
+import '@tylertech/forge/open-icon';
+import './Accordion.scss';
+
+const component = 'forge-accordion';
+
+const meta = {
+  title: 'Components/Accordion',
+  render: args => customElementStoryRenderer(component, args),
+  component,
+  parameters: {
+    actions: { disable: true }
+  },
+  argTypes: {
+    ...generateCustomElementArgTypes({
+      tagName: component,
+      exclude: ['panelSelector']
+    })
+  },
+  args: {
+  },
+} satisfies Meta;
+
+export default meta;
+
+type Story = StoryObj;
+
+export const Demo: Story = {
+  render: () => {
+    return html`
+      <forge-accordion>
+        <forge-expansion-panel>
+          <div  slot="header">
+            Panel One 
+            <forge-open-icon></forge-open-icon>
+          </div>
+          <div>Panel One Content</div>
+        </forge-expansion-panel>
+        <forge-divider></forge-divider>
+        <forge-expansion-panel>
+          <div  slot="header">
+            Panel Two 
+            <forge-open-icon></forge-open-icon>
+          </div>
+          <div>Panel Two Content</div>
+        </forge-expansion-panel>
+        <forge-divider></forge-divider>
+        <forge-expansion-panel>
+          <div  slot="header">
+            Panel Three 
+            <forge-open-icon></forge-open-icon>
+          </div>
+          <div>Panel Three Content</div>
+        </forge-expansion-panel>
+      </forge-accordion>
+    `;
+  }
+};
+
+
