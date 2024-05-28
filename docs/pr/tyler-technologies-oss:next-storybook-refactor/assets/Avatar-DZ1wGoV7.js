2f83ae3ed1552ba8e9c0c93bdd9f6457bc2d3285
DRiFTy17 <DRiFTy17@users.noreply.github.com>
2024-05-28 13:30:59 +0000

    Deploying to gh-pages from @ tyler-technologies-oss/forge@08c97576aa9cc4acc1bc7e45ba485b1abb1e11f7 🚀

---
+++ docs/pr/tyler-technologies-oss:next-storybook-refactor/assets/Avatar-DZ1wGoV7.js
@@ -0,0 +1,33 @@
+import{j as e}from"./jsx-runtime-B7Y0vOC6.js";import{u as a}from"./index-De-eLSCa.js";import{M as r,T as s,C as i}from"./index-DTFa5749.js";import{C as c}from"./CustomArgTypes-B6ok-FuW.js";import{A as h,D as l,W as d,a as m,b as p}from"./Avatar.stories-BAFpxAXr.js";import"./iframe-COcotaQv.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./chunk-QN4WKJDJ-Bf_F3oir.js";import"./index-DXimoRZY.js";import"./index-DvzDrELh.js";import"./index-DrFu-skq.js";import"./base-button-adapter-BJTwNuZc.js";import"./lit-element-Dm2J4qPi.js";import"./icon-button-BCNqjlCf.js";function o(n){const t={blockquote:"blockquote",code:"code",h2:"h2",li:"li",p:"p",ul:"ul",...a(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(r,{of:h}),`
+`,e.jsx(s,{}),`
+`,e.jsx(t.p,{children:`Avatars allow you to provide text or images to display that represent an entity. By default, the avatar will
+display text content as single characters (character count is configurable), or display an icon or image
+based on the URL provided to it.`}),`
+`,e.jsx(i,{of:l}),`
+`,e.jsx(t.h2,{id:"with-icon",children:"With Icon"}),`
+`,e.jsxs(t.p,{children:["You can compose avatars together with icons using the ",e.jsx(t.code,{children:"<forge-icon>"}),` component. The avatar will automatically
+apply the proper dimensions to the icon.`]}),`
+`,e.jsx(i,{of:d}),`
+`,e.jsx(t.h2,{id:"with-image",children:"With Image"}),`
+`,e.jsx(t.p,{children:`When using with an image, the avatar will apply your image as that background of the avatar (replacing the
+background color). The image will be sized to fit the avatar dimensions.`}),`
+`,e.jsx(i,{of:m}),`
+`,e.jsx(t.h2,{id:"with-icon-button",children:"With Icon Button"}),`
+`,e.jsx(t.p,{children:"Avatars can be composed within other components, such as icon buttons."}),`
+`,e.jsx(i,{of:p}),`
+`,e.jsxs(t.blockquote,{children:[`
+`,e.jsx(t.p,{children:"Note: Be sure to provide an accessible label for the icon button."}),`
+`]}),`
+`,e.jsx(t.h2,{id:"api",children:"API"}),`
+`,e.jsx(c,{}),`
+`,e.jsx(t.h2,{id:"accessibility",children:"Accessibility"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsxs(t.li,{children:["Note that the avatar component may not be visible to screen readers.",`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsxs(t.li,{children:["Use ",e.jsx(t.code,{children:"aria-hidden"})," to ensure that the avatar content is ignored by a screen reader."]}),`
+`]}),`
+`]}),`
+`,e.jsx(t.li,{children:"Avoid using the avatar component for any essential navigation."}),`
+`,e.jsx(t.li,{children:"If color conveys important information, provide additional cues for users with color perception deficiencies."}),`
+`,e.jsx(t.li,{children:"Verify that there is sufficient contrast between the foreground text and background to meet WCAG 2.2 requirements."}),`
+`]})]})}function D(n={}){const{wrapper:t}={...a(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o(n)}export{D as default};
