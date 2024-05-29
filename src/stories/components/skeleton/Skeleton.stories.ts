6d4bd0e6eee7125818982facd438ae2f0316249e
Paul Lapczynski <paul.lapczynski@tylertech.com>
2024-05-29 15:52:15 -0400

    feat: add skeleton storybook

--- src/stories/components/skeleton/Skeleton.stories.ts
+++ src/stories/components/skeleton/Skeleton.stories.ts
@@ -1,6 +1,6 @@
-import { html } from 'lit';
+import { html, render } from 'lit';
 import { type Meta, type StoryObj } from '@storybook/web-components';
-import { customElementStoryRenderer, generateCustomElementArgTypes } from '../../utils';
+import { customElementStoryRenderer, generateCustomElementArgTypes, standaloneStoryParams } from '../../utils';
 
 import '@tylertech/forge/skeleton';
 
@@ -29,3 +29,59 @@ export default meta;
 type Story = StoryObj;
 
 export const Demo: Story = {};
+
+export const Profile = {
+  ...standaloneStoryParams,
+  render: () => html`
+  <div style="width: 200px">
+    <forge-skeleton avatar></forge-skeleton>
+    <forge-skeleton text></forge-skeleton>
+    <forge-skeleton text></forge-skeleton>
+    <forge-skeleton text style="width: 75%;"></forge-skeleton>
+  </div>
+  `
+}
+
+export const List = {
+  ...standaloneStoryParams,
+  render: () => html`
+  <div style="width: 200px">
+    <forge-skeleton list-item></forge-skeleton>
+    <forge-skeleton list-item></forge-skeleton>
+    <forge-skeleton list-item></forge-skeleton>
+  </div>
+  `
+}
+
+export const Chips = {
+  ...standaloneStoryParams,
+  render: () => html`
+  <div style="width: 200px">
+    <forge-skeleton chip></forge-skeleton>
+    <forge-skeleton chip></forge-skeleton>
+    <forge-skeleton chip></forge-skeleton>
+  </div>
+  `
+}
+
+export const Buttons = {
+  ...standaloneStoryParams,
+  render: () => html`
+  <div style="width: 200px">
+    <forge-skeleton button></forge-skeleton>
+    <forge-skeleton button></forge-skeleton>
+    <forge-skeleton button></forge-skeleton>
+  </div>
+  `
+}
+
+export const FormField = {
+  ...standaloneStoryParams,
+  render: () => html`
+  <div style="width: 200px">
+    <forge-skeleton form-field></forge-skeleton>
+    <forge-skeleton form-field></forge-skeleton>
+    <forge-skeleton form-field></forge-skeleton>
+  </div>
+  `
+}
