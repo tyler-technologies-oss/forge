47a8dcd83cceaab3771eb1b8250e59b02e329c82
Paul Lapczynski <paul.lapczynski@tylertech.com>
2024-05-29 13:32:33 -0400

    chore: formatting

--- src/stories/components/app-bar/AppBar.stories.ts
+++ src/stories/components/app-bar/AppBar.stories.ts
@@ -45,14 +45,14 @@ export const Full: Story = {
   ...standaloneStoryParams,
   render: () => html`
   <forge-app-bar title-text="Tyler Forge">
-  <forge-app-bar-menu-button slot="start"></forge-app-bar-menu-button>
-  <forge-app-bar-search slot="center">
-      <input type="text" placeholder="Search" />
-  </forge-app-bar-search>
-  <forge-app-bar-help-button slot="end"></forge-app-bar-help-button>
-  <forge-app-bar-notification-button slot="end"></forge-app-bar-notification-button>
-  <forge-app-bar-app-launcher-button slot="end" allow-more="true"></forge-app-bar-app-launcher-button>
-  <forge-app-bar-profile-button slot="end" avatar-text="First Last" full-name="First Last" email="first.last@tylertech.com"></forge-app-bar-profile-button>
+    <forge-app-bar-menu-button slot="start"></forge-app-bar-menu-button>
+    <forge-app-bar-search slot="center">
+        <input type="text" placeholder="Search" />
+    </forge-app-bar-search>
+    <forge-app-bar-help-button slot="end"></forge-app-bar-help-button>
+    <forge-app-bar-notification-button slot="end"></forge-app-bar-notification-button>
+    <forge-app-bar-app-launcher-button slot="end" allow-more="true"></forge-app-bar-app-launcher-button>
+    <forge-app-bar-profile-button slot="end" avatar-text="First Last" full-name="First Last" email="first.last@tylertech.com"></forge-app-bar-profile-button>
   </forge-app-bar>
   `
 }
