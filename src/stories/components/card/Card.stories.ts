f8f9d82dd2b2458691b82821b326d7fdd25468ca
Paul Lapczynski <paul.lapczynski@tylertech.com>
2024-05-29 16:23:35 -0400

    feat: add card storybook

--- src/stories/components/card/Card.stories.ts
+++ src/stories/components/card/Card.stories.ts
@@ -1,6 +1,6 @@
 import { html } from 'lit';
 import { type Meta, type StoryObj } from '@storybook/web-components';
-import { customElementStoryRenderer, generateCustomElementArgTypes } from '../../utils';
+import { customElementStoryRenderer, generateCustomElementArgTypes, standaloneStoryParams } from '../../utils';
 
 import '@tylertech/forge/card';
 
@@ -8,7 +8,11 @@ const component = 'forge-card';
 
 const meta = {
   title: 'Components/Card',
-  render: args => customElementStoryRenderer(component, args),
+  render: args => {
+    const el = customElementStoryRenderer(component, args);
+    el.textContent = args.text;
+    return el;
+  },
   component,
   parameters: {
     actions: { disable: true }
@@ -16,11 +20,11 @@ const meta = {
   argTypes: {
     ...generateCustomElementArgTypes({
       tagName: component,
-      
     }),
   },
   args: {
-
+    raised: false,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias quas sed aliquid cumque sunt iste ad, alias quod adipisci? Nulla, libero necessitatibus enim sint nesciunt provident excepturi dolorum pariatur illum?'
   },
 } satisfies Meta;
 
@@ -28,4 +32,5 @@ export default meta;
 
 type Story = StoryObj;
 
-export const Demo: Story = {};
+export const Demo: Story = {
+};
