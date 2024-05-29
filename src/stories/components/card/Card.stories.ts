e57aca581f7590baf9e51fc24d8c7f76b2080e96
Paul Lapczynski <paul.lapczynski@tylertech.com>
2024-05-29 16:32:48 -0400

    chore: add one more example

--- src/stories/components/card/Card.stories.ts
+++ src/stories/components/card/Card.stories.ts
@@ -1,8 +1,14 @@
-import { html } from 'lit';
+import { html, render } from 'lit';
 import { type Meta, type StoryObj } from '@storybook/web-components';
 import { customElementStoryRenderer, generateCustomElementArgTypes, standaloneStoryParams } from '../../utils';
 
 import '@tylertech/forge/card';
+import '@tylertech/forge/icon-button';
+import '@tylertech/forge/icon';
+import '@tylertech/forge/button';
+import { IconRegistry } from '@tylertech/forge/icon';
+import { tylIconMoreVert } from '@tylertech/tyler-icons/standard';
+import './Card.scss';
 
 const component = 'forge-card';
 
@@ -34,3 +40,39 @@ type Story = StoryObj;
 
 export const Demo: Story = {
 };
+
+export const Styled: Story = {
+  ...standaloneStoryParams,
+  render: () => {
+    IconRegistry.define([tylIconMoreVert]);
+    return html`
+    <div class="demo-card">
+      <forge-card>
+        <div class="forge-card-header-container">
+          <h3 class="forge-typography--headline6">This is the card title</h3>
+          <forge-icon-button>
+              <forge-icon name="more_vert"></forge-icon>
+          </forge-icon-button>
+        </div>
+
+        <div>
+          <p class="forge-typography--body2">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias exercitationem doloremque dolorem ullam, nesciunt quia velit necessitatibus numquam quasi voluptates impedit earum dolores repudiandae facilis totam non quo labore itaque?
+          </p>
+        </div>
+
+        <div class="forge-card-footer">
+          <div>
+            <forge-button>
+              Ok
+            </forge-button>
+            <forge-button>
+              Cancel
+            </forge-button>
+          </div>
+        </div>
+      </forge-card>
+    </div>
+  `;
+  }
+}
