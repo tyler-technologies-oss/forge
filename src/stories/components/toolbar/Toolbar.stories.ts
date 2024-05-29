8da270a96c19431b6817cd325be519a36c5f82cd
Paul Lapczynski <70207430+tyl-paul-lapczynski@users.noreply.github.com>
2024-05-29 15:10:35 -0400

    feat: placeholders to help speed (#583)

---
+++ src/stories/components/toolbar/Toolbar.stories.ts
@@ -0,0 +1,31 @@
+import { html } from 'lit';
+import { type Meta, type StoryObj } from '@storybook/web-components';
+import { customElementStoryRenderer, generateCustomElementArgTypes } from '../../utils';
+
+import '@tylertech/forge/toolbar';
+
+const component = 'forge-toolbar';
+
+const meta = {
+  title: 'Components/Toolbar',
+  render: args => customElementStoryRenderer(component, args),
+  component,
+  parameters: {
+    actions: { disable: true }
+  },
+  argTypes: {
+    ...generateCustomElementArgTypes({
+      tagName: component,
+      
+    }),
+  },
+  args: {
+
+  },
+} satisfies Meta;
+
+export default meta;
+
+type Story = StoryObj;
+
+export const Demo: Story = {};
