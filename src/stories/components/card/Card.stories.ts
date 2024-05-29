cf08f9afb012334063d1440b17b88a702e967a42
Paul Lapczynski <paul.lapczynski@tylertech.com>
2024-05-29 16:39:50 -0400

    chore: add more examples

--- src/stories/components/card/Card.stories.ts
+++ src/stories/components/card/Card.stories.ts
@@ -6,6 +6,9 @@ import '@tylertech/forge/card';
 import '@tylertech/forge/icon-button';
 import '@tylertech/forge/icon';
 import '@tylertech/forge/button';
+import '@tylertech/forge/scaffold';
+import '@tylertech/forge/toolbar';
+
 import { IconRegistry } from '@tylertech/forge/icon';
 import { tylIconMoreVert } from '@tylertech/tyler-icons/standard';
 import './Card.scss';
@@ -49,14 +52,14 @@ export const Styled: Story = {
     <div class="demo-card">
       <forge-card>
         <div class="forge-card-header-container">
-          <h3 class="forge-typography--headline6">This is the card title</h3>
+          <h3 >This is the card title</h3>
           <forge-icon-button>
               <forge-icon name="more_vert"></forge-icon>
           </forge-icon-button>
         </div>
 
         <div>
-          <p class="forge-typography--body2">
+          <p>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias exercitationem doloremque dolorem ullam, nesciunt quia velit necessitatibus numquam quasi voluptates impedit earum dolores repudiandae facilis totam non quo labore itaque?
           </p>
         </div>
@@ -74,5 +77,43 @@ export const Styled: Story = {
       </forge-card>
     </div>
   `;
+  }
+}
+
+export const Scaffold: Story = {
+  ...standaloneStoryParams,
+  render: () => {
+    return html`
+    <forge-card class="scaffold-card">
+      <forge-scaffold>
+        <forge-toolbar slot="header">
+          <h1 slot="start">Lorem ipsum</h1>
+        </forge-toolbar>
+
+        <p slot="body" tabindex="0" class="card-content">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias quas sed
+          aliquid cumque sunt iste ad, alias quod adipisci? Nulla, libero necessitatibus
+          enim sint nesciunt provident excepturi dolorum pariatur illum?
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias quas sed
+          aliquid cumque sunt iste ad, alias quod adipisci? Nulla, libero necessitatibus
+          enim sint nesciunt provident excepturi dolorum pariatur illum?
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias quas sed
+          aliquid cumque sunt iste ad, alias quod adipisci? Nulla, libero necessitatibus
+          enim sint nesciunt provident excepturi dolorum pariatur illum?
+        </p>
+
+        <forge-toolbar slot="footer" inverted>
+          <forge-button type="outlined" slot="end">
+            Cancel
+          </forge-button>
+          <forge-button type="unelevated" slot="end" style="margin-left: 8px;">
+            Ok
+          </forge-button>
+        </forge-toolbar>
+      </forge-scaffold>
+    </forge-card>
+      `;
   }
 }
