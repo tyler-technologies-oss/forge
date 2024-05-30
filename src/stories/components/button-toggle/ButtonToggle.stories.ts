efc8b0cb73fd3dc624fe9e20d2a01d6f100d509f
derekmoss <derek.moss@tylertech.com>
2024-05-30 13:20:21 -0700

    feat: storybook refactor button toggle

---
+++ src/stories/components/button-toggle/ButtonToggle.stories.ts
@@ -0,0 +1,67 @@
+import { html, nothing } from 'lit';
+import { type Meta, type StoryObj } from '@storybook/web-components';
+import { GLOBAL_THEME_OPTIONS, generateCustomElementArgTypes, getCssVariableArgs } from 'src/stories/utils';
+import { styleMap } from 'lit/directives/style-map.js';
+
+import '@tylertech/forge/button-toggle';
+
+const component = 'forge-button-toggle';
+
+const meta = {
+  title: 'Components/Button Toggle',
+  render: args => {
+    const cssVarArgs = getCssVariableArgs(args);
+    const style = cssVarArgs ? styleMap(cssVarArgs) : nothing;
+
+    return html`
+      <forge-button-toggle-group 
+      .outlined=${args.outlined}
+      .multiple=${args.multiple}
+      .stretch=${args.stretch}
+      .mandatory=${args.mandatory}
+      .vertical=${args.vertical}
+      .disabled=${args.disabled}
+      .dense=${args.dense}
+      .theme=${args.theme}
+      ?readonly=${args.readonly}
+      ?required=${args.required}
+      aria-label="Choose communication type"
+      style=${style}>
+        <forge-button-toggle .required=${args.selected} .selected=${args.selected} value="email">By email</forge-button-toggle>
+        <forge-button-toggle value="mail">By mail</forge-button-toggle>
+        <forge-button-toggle value="phone">By phone</forge-button-toggle>
+      </forge-button-toggle-group>
+    `;
+  },
+  component,
+  parameters: {
+    actions: { disable: true }
+  },
+  argTypes: {
+    ...generateCustomElementArgTypes({
+      tagName: `${component}-group`,
+      exclude: ['value', 'required'],
+      controls: {
+        theme: { control: 'select', options: ['default', ...GLOBAL_THEME_OPTIONS] }
+      }
+    }),
+    ...generateCustomElementArgTypes({ tagName: component, exclude: ['value', 'required']}),
+  },
+  args: {
+    outlined: true,
+    multiple: false,
+    stretch: false,
+    mandatory: false,
+    vertical: false,
+    disabled: false,
+    readonly: false,
+    dense: false,
+    selected: false,
+  },
+} satisfies Meta;
+
+export default meta;
+
+type Story = StoryObj;
+
+export const Demo: Story = {};
