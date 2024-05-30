c15697a312a56b70864a0cba8eb5f7493f3650d6
derekmoss <derek.moss@tylertech.com>
2024-05-30 14:09:26 -0700

    feat: storybook refactor checkbox

--- src/stories/components/checkbox/Checkbox.stories.ts
+++ src/stories/components/checkbox/Checkbox.stories.ts
@@ -1,14 +1,36 @@
-import { html } from 'lit';
+import { html, nothing } from 'lit';
+import { styleMap } from 'lit/directives/style-map.js';
 import { type Meta, type StoryObj } from '@storybook/web-components';
-import { customElementStoryRenderer, generateCustomElementArgTypes } from '../../utils';
+import { action } from '@storybook/addon-actions';
+import { customElementStoryRenderer, generateCustomElementArgTypes, getCssVariableArgs } from '../../utils';
 
 import '@tylertech/forge/checkbox';
 
 const component = 'forge-checkbox';
 
+const changeAction = action('forge-checkbox-change');
+
 const meta = {
   title: 'Components/Checkbox',
-  render: args => customElementStoryRenderer(component, args),
+  render: args => {
+    const cssVarArgs = getCssVariableArgs(args);
+    const style = cssVarArgs ? styleMap(cssVarArgs) : nothing;
+
+    return html`
+      <forge-checkbox
+      .checked=${args.checked}
+      .indeterminate=${args.indeterminate}
+      .disabled=${args.disabled}
+      .required=${args.required}
+      .readonly=${args.readonly}
+      .dense=${args.dense}
+      .labelPosition=${args.labelPosition}
+      @change=${changeAction}
+      style=${style}>
+        ${args.text}
+      </forge-checkbox>
+    `;
+  },
   component,
   parameters: {
     actions: { disable: true }
@@ -16,11 +38,22 @@ const meta = {
   argTypes: {
     ...generateCustomElementArgTypes({
       tagName: component,
-      
+      exclude: ['defaultChecked'],
+      controls: {
+        labelPosition: { control: 'select', options: ['start', 'end'] }
+      }
     }),
+    text: { control: 'text' }
   },
   args: {
-
+    text: 'Label',
+    checked: false,
+    indeterminate: false,
+    disabled: false,
+    required: false,
+    readonly: false,
+    dense: false,
+    labelPosition: 'end',
   },
 } satisfies Meta;
 
