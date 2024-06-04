e108ab61705678f62d6734fb405c6a92b2b0b724
Paul Lapczynski <paul.lapczynski@tylertech.com>
2024-06-04 11:28:44 -0400

    feat: add page state to storybook

--- src/stories/components/page-state/PageState.stories.ts
+++ src/stories/components/page-state/PageState.stories.ts
@@ -3,12 +3,25 @@ import { type Meta, type StoryObj } from '@storybook/web-components';
 import { customElementStoryRenderer, generateCustomElementArgTypes } from '../../utils';
 
 import '@tylertech/forge/page-state';
+import '@tylertech/forge/button';
 
 const component = 'forge-page-state';
 
 const meta = {
   title: 'Components/Page State',
-  render: args => customElementStoryRenderer(component, args),
+  render: args => html`
+  <forge-page-state>
+    <img src="https://cdn.forge.tylertech.com/v1/images/spot-hero/404-error-spot-hero.svg" alt="" slot="graphic" />
+    <div slot="title">Nothing but tumbleweeds here...</div>
+    <p slot="message">Even our best explorer couldn't find the page you're looking for. It might have been removed or you may have mistyped the URL.</p>
+    <forge-button variant="raised" slot="action">
+      Go back
+    </forge-button>
+    <forge-button variant="outlined" slot="action">
+      Refresh
+    </forge-button>
+  </forge-page-state>
+  `,
   component,
   parameters: {
     actions: { disable: true }
