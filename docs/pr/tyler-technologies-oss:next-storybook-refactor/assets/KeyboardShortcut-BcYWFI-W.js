c0fc1eded43384960fd3d07e4f05142dcdea0574
tyl-paul-lapczynski <tyl-paul-lapczynski@users.noreply.github.com>
2024-06-04 18:01:03 +0000

    Deploying to gh-pages from @ tyler-technologies-oss/forge@a36138e32dd641700ad4ba9b49ee514cd2304a83 🚀

---
+++ docs/pr/tyler-technologies-oss:next-storybook-refactor/assets/KeyboardShortcut-BcYWFI-W.js
@@ -0,0 +1,6 @@
+import{j as t}from"./jsx-runtime-BNqQs5pY.js";import{u as s}from"./index-B3ST0c0h.js";import{M as n,T as m,C as e}from"./index-B4gBHTxx.js";import{C as a}from"./CustomArgTypes-6S45sPJF.js";import{K as p,D as c}from"./KeyboardShortcut.stories-CUVi24b9.js";import"./iframe-si_H5INT.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./chunk-QN4WKJDJ-Bf_F3oir.js";import"./index-DXimoRZY.js";import"./index-DvzDrELh.js";import"./index-DrFu-skq.js";import"./utils-DyetTxuG.js";import"./keyboard-shortcut-CYFvNlQF.js";import"./base-adapter-BIKyOSkq.js";import"./constants-C96o6uhb.js";function i(o){const r={h2:"h2",...s(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(n,{of:p}),`
+`,t.jsx(m,{}),`
+`,t.jsx(e,{of:c}),`
+`,t.jsx(r.h2,{id:"api",children:"API"}),`
+`,t.jsx(a,{}),`
+`,t.jsx(r.h2,{id:"accessibility",children:"Accessibility"})]})}function K(o={}){const{wrapper:r}={...s(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(i,{...o})}):i(o)}export{K as default};
---
+++ docs/pr/tyler-technologies-oss:next-storybook-refactor/assets/Overlay-B0HlvLHB.js
@@ -0,0 +1,20 @@
+import{j as o}from"./jsx-runtime-BNqQs5pY.js";import{u as i}from"./index-B3ST0c0h.js";import{M as s,T as p,C as n}from"./index-B4gBHTxx.js";import{C as a}from"./CustomArgTypes-6S45sPJF.js";import{O as c,D as l,C as m}from"./Overlay.stories-S3qcXj4e.js";import"./iframe-si_H5INT.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./chunk-QN4WKJDJ-Bf_F3oir.js";import"./index-DXimoRZY.js";import"./index-DvzDrELh.js";import"./index-DrFu-skq.js";import"./utils-DyetTxuG.js";import"./chunk-MZXVCX43-CM0pFb8Z.js";import"./v4-CQkTLCs1.js";import"./lit-element-BOOoordI.js";import"./lit-html-BWgXkSvR.js";import"./style-map-D0ILlpbs.js";import"./directive-CF8sV3Lr.js";import"./ref-BorTy8X1.js";import"./directive-helpers-DYUueT8w.js";import"./decorators-B79PnA5z.js";import"./button-BDRrw9v7.js";import"./icon-V4IE3JYq.js";import"./base-adapter-BIKyOSkq.js";import"./constants-C96o6uhb.js";import"./index-Dh0vMUMR.js";import"./focus-indicator-By3BQe1w.js";import"./state-layer-b0IlkqgO.js";import"./base-button-adapter-CNVfabjy.js";import"./index-Dh2cEqRr.js";import"./with-label-aware-DCBgJY4W.js";import"./with-default-aria-lryJjX2Z.js";import"./event-utils-DC3JW7a-.js";import"./overlay-BAE6BB-F.js";function r(t){const e={a:"a",code:"code",h2:"h2",li:"li",p:"p",ul:"ul",...i(),...t.components};return o.jsxs(o.Fragment,{children:[o.jsx(s,{of:c}),`
+`,o.jsx(p,{}),`
+`,o.jsx(e.p,{children:"Overlays are used to render content in above all other content on the page and positioned around a specified anchor element."}),`
+`,o.jsx(e.p,{children:`An overlay is a low-level building block component that does not provide any visual styles. Its only purpose is to render slotted
+content above all other content on the page. The element is positioned around an anchor element with various positioning options.`}),`
+`,o.jsxs(e.p,{children:["The following components are examples of components that use a ",o.jsx(e.code,{children:"<forge-overlay>"})," internally:"]}),`
+`,o.jsxs(e.ul,{children:[`
+`,o.jsx(e.li,{children:o.jsx(e.a,{href:"?path=/docs/components-tooltip--docs",children:"Tooltip"})}),`
+`,o.jsx(e.li,{children:o.jsx(e.a,{href:"?path=/docs/components-popover--docs",children:"Popover"})}),`
+`,o.jsx(e.li,{children:o.jsx(e.a,{href:"?path=/docs/components-toast--docs",children:"Toast"})}),`
+`]}),`
+`,o.jsxs(e.p,{children:["These components expose similar APIs that are passed down to the ",o.jsx(e.code,{children:"<forge-overlay>"})," component."]}),`
+`,o.jsx(n,{of:l}),`
+`,o.jsx(e.h2,{id:"context-menu",children:"Context Menu"}),`
+`,o.jsx(e.p,{children:"You can use an overlay to create a context menu that appears when the user right-clicks on the page."}),`
+`,o.jsx(n,{of:m}),`
+`,o.jsx(e.h2,{id:"api",children:"API"}),`
+`,o.jsx(a,{}),`
+`,o.jsx(e.h2,{id:"accessibility",children:"Accessibility"}),`
+`,o.jsx(e.p,{children:"Overlays do not provide any semantics by default. It is up to the developer to provide the necessary semantics for the content."})]})}function N(t={}){const{wrapper:e}={...i(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(r,{...t})}):r(t)}export{N as default};
