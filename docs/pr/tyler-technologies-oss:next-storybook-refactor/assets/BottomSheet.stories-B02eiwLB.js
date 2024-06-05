f70711572e4574dd1d96813c42645907ecd634a5
DRiFTy17 <DRiFTy17@users.noreply.github.com>
2024-06-05 15:52:31 +0000

    Deploying to gh-pages from @ tyler-technologies-oss/forge@01cc1247a6ea805c1b2c0c733d548b7050778d3c 🚀

---
+++ docs/pr/tyler-technologies-oss:next-storybook-refactor/assets/BottomSheet.stories-B02eiwLB.js
@@ -0,0 +1,26 @@
+import{x as c,T as d}from"./lit-element-BOOoordI.js";import"./lit-html-BWgXkSvR.js";import{a as f,g as u}from"./utils-BrN94tQ9.js";import{s as g}from"./decorators-B79PnA5z.js";import{o as b}from"./style-map-D0ILlpbs.js";import{n as h,e as y}from"./ref-BorTy8X1.js";import"./bottom-sheet-BLPGhEjn.js";import"./dialog-BgSzVYZs.js";import"./backdrop-DkhyKz6z.js";import"./button-D5XxdyZ6.js";import"./focus-indicator-By3BQe1w.js";import"./index-Dh0vMUMR.js";import"./state-layer-b0IlkqgO.js";import"./toolbar-B2mSDDPY.js";const s="forge-bottom-sheet",S={title:"Components/Bottom Sheet",render:e=>{const r=f(e),l=r?b(r):d,t=y();function m(){t.value.open=!t.value.open}function p(){t.value.open=!1}return c`
+      <forge-button variant="raised" @click=${m}>Show Bottom Sheet</forge-button>
+      <forge-bottom-sheet
+        ${h(t)}
+        aria-labelledby="title"
+        aria-describedby="message"
+        .open=${e.open}
+        .mode=${e.mode}
+        .persistent=${e.persistent}
+        .fullscreen=${e.fullscreen}
+        aria-labelledby="title"
+        aria-describedby="message"
+        style=${l}>
+        <div class="content">
+          <h2 id="title">Bottom Sheet Title</h2>
+          <p id="message">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nec urna et felis.</p>
+        </div>
+        <forge-toolbar>
+          <forge-button slot="end" @click=${p}>Close</forge-button>
+        </forge-toolbar>
+      </forge-bottom-sheet>
+    `},component:s,decorators:[g(`
+    .content {
+      padding: var(--forge-spacing-medium);
+    }
+  `)],argTypes:{...u({tagName:s,controls:{mode:{control:"select",options:["modal","inline-modal","nonmodal"]}}})},args:{open:!1,mode:"modal",persistent:!1,fullscreen:!1}},o={};var n,a,i;o.parameters={...o.parameters,docs:{...(n=o.parameters)==null?void 0:n.docs,source:{originalSource:"{}",...(i=(a=o.parameters)==null?void 0:a.docs)==null?void 0:i.source}}};const v=["Demo"],z=Object.freeze(Object.defineProperty({__proto__:null,Demo:o,__namedExportsOrder:v,default:S},Symbol.toStringTag,{value:"Module"}));export{z as B,o as D};
---
+++ docs/pr/tyler-technologies-oss:next-storybook-refactor/assets/Stack-Cyuo74AQ.js
@@ -0,0 +1,17 @@
+import{j as t}from"./jsx-runtime-tdvS7QP1.js";import{u as n}from"./index-CYGJqqE5.js";import{M as a,T as m,C as i}from"./index-BHI_LK3P.js";import{C as s}from"./CustomArgTypes-Bn_DiUFM.js";import{S as p,D as c,a as l,C as d}from"./Stack.stories-Fx-MHJk0.js";import"./iframe-Dm9GkP7p.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./chunk-QN4WKJDJ-Bf_F3oir.js";import"./index-DXimoRZY.js";import"./index-DvzDrELh.js";import"./index-DrFu-skq.js";import"./utils-BrN94tQ9.js";import"./lit-element-BOOoordI.js";import"./lit-html-BWgXkSvR.js";import"./style-map-D0ILlpbs.js";import"./directive-CF8sV3Lr.js";import"./decorators-B79PnA5z.js";import"./stack-ljBwMmWX.js";import"./base-adapter-BIKyOSkq.js";import"./constants-C96o6uhb.js";import"./text-field-BW11aAL9.js";import"./index-CDQrnaq1.js";import"./base-field-BJUBqWNb.js";import"./index-Dh0vMUMR.js";import"./focus-indicator-By3BQe1w.js";import"./label-DREoufrJ.js";import"./button-D5XxdyZ6.js";import"./icon-V4IE3JYq.js";import"./state-layer-b0IlkqgO.js";import"./base-button-adapter-CRi8-gKl.js";import"./with-label-aware-DCBgJY4W.js";import"./with-default-aria-lryJjX2Z.js";import"./event-utils-DC3JW7a-.js";import"./button-toggle-group-D0JifxTm.js";import"./with-form-associated-e_PToDaQ.js";import"./checkbox-BZl9IGGg.js";import"./icon-button-CZlBA0tM.js";import"./switch-DljXu9M_.js";import"./tooltip-ujb0qApC.js";import"./overlay-BAE6BB-F.js";import"./with-longpress-listener-DSRt_cqb.js";import"./dismissible-stack-BU50KYzw.js";import"./base-field-foundation-BjAPCV1E.js";function r(e){const o={code:"code",h2:"h2",p:"p",...n(),...e.components};return t.jsxs(t.Fragment,{children:[t.jsx(a,{of:p}),`
+`,t.jsx(m,{}),`
+`,t.jsx(o.p,{children:"The stack is a utility component that helps manage spacing and alignment of immediate children along a vertical or horizontal axis."}),`
+`,t.jsx(i,{of:c}),`
+`,t.jsx(o.h2,{id:"disclaimer",children:"Disclaimer"}),`
+`,t.jsxs(o.p,{children:["The stack component is a layout utility component that essentially abstracts the ",t.jsx(o.code,{children:"flex"}),` property of CSS. It is not a replacement for CSS Flexbox or Grid.
+It is a simple utility component that helps manage spacing and alignment of immediate children along a vertical or horizontal axis.`]}),`
+`,t.jsx(o.p,{children:"It is very easy to overuse the stack component and end up with a lot of nested stacks. This can lead to performance issues and can make the codebase harder to maintain."}),`
+`,t.jsx(o.p,{children:"Make sure to use the stack component judiciously and only when necessary, and instead rely on CSS Flexbox and Grid for more complex layouts."}),`
+`,t.jsx(o.h2,{id:"simple-vertical-form",children:"Simple Vertical Form"}),`
+`,t.jsx(o.p,{children:"You can use the stack to create a simple vertical form layout with even spacing between fields."}),`
+`,t.jsx(i,{of:l}),`
+`,t.jsx(o.h2,{id:"complex-form",children:"Complex Form"}),`
+`,t.jsx(o.p,{children:"You can also use the stack to create a more complex form layout with different spacing between fields, or multiple columns by nesting stacks."}),`
+`,t.jsx(i,{of:d}),`
+`,t.jsx(o.h2,{id:"api",children:"API"}),`
+`,t.jsx(s,{})]})}function it(e={}){const{wrapper:o}={...n(),...e.components};return o?t.jsx(o,{...e,children:t.jsx(r,{...e})}):r(e)}export{it as default};
