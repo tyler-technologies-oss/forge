7bde12abc01e472f6963de07361ba580e2903954
Nichols, Kieran <kieran.nichols@tylertech.com>
2024-06-05 14:04:52 -0400

    chore: add ability to hide stories from sidebar via "hidden" tag

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -3,6 +3,9 @@ import { addons } from '@storybook/manager-api';
 addons.setConfig({
   sidebar: {
     showRoots: true,
-    collapsedRoots: ['Getting Started', 'Frameworks', 'Components', 'Tokens', 'About']
+    collapsedRoots: ['Getting Started', 'Frameworks', 'Components', 'Tokens', 'About'],
+    filters: {
+      hidden: item => !item.tags?.includes('hidden')
+    }
   },
 });
