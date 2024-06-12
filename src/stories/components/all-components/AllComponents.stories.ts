a3c4814f322fa941a6ecc37918611ac25ce62238
nickonometry <nick.andrews@tylertech.com>
2024-06-12 14:02:04 -0400

    chore: created a view all stories page to setup automated ARC platform scanning

---
+++ src/stories/components/all-components/AllComponents.stories.ts
@@ -0,0 +1,12 @@
+import { type Meta, type StoryObj } from '@storybook/web-components';
+
+const meta = {
+  title: 'Components/All Components'
+} satisfies Meta;
+
+export const containerStyles = {
+  display: 'grid',
+  gap: '32px'
+};
+
+export default meta;
