5e6bfd29f8c74ad655f67a0a21952c25ffcf76d4
Kieran Nichols <kieran.nichols@tylertech.com>
2024-07-09 11:05:07 -0400

    docs: add temporary banner with link to v2.x docs (#646)

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -1,5 +1,10 @@
 import { addons } from '@storybook/manager-api';
 
+const banner = document.createElement('div');
+banner.classList.add('forge-docs-banner');
+banner.innerHTML = '<p>You are currently viewing the latest v3.x docs. To view the legacy v2.x docs, click <a href="https://forge.tylerdev.io/version-2" target="_blank" rel="noreferrer noopener">here</a>.</p>';
+document.body.prepend(banner);
+
 addons.setConfig({
   sidebar: {
     showRoots: true,
