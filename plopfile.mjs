4e0e53d4e8ee7fce300d1f8164c60c1dd75f6226
Nichols, Kieran <kieran.nichols@tylertech.com>
2024-07-31 13:11:04 -0400

    chore: add tokens generator and adjust default styles

--- plopfile.mjs
+++ plopfile.mjs
@@ -1,5 +1,5 @@
 export default function (plop) {
-  // Create a new component
+  // Scaffold out the file structure for a new component
   plop.setGenerator('component', {
     description: 'Create a new component',
     prompts: [
@@ -15,6 +15,30 @@ export default function (plop) {
         destination: 'src/lib/{{dashCase name}}',
         base: 'templates/component',
         templateFiles: 'templates/component/**'
+      },
+      {
+        type: 'add',
+        path: 'src/lib/core/styles/tokens/{{dashCase name}}/_tokens.scss',
+        templateFile: 'templates/tokens/_tokens.scss.hbs'
+      }
+    ]
+  });
+
+  // Scaffold out a new tokens file in core styles for a component
+  plop.setGenerator('tokens', {
+    description: 'Create a new tokens file for a component',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Component name'
+      }
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: 'src/lib/core/styles/tokens/{{dashCase name}}/_tokens.scss',
+        templateFile: 'templates/tokens/_tokens.scss.hbs'
       }
     ]
   });
