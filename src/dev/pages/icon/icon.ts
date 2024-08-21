431ec977fac876c23a8bbc6f1a21dccf2f9fc1eb
Nichols, Kieran <kieran.nichols@tylertech.com>
2024-08-21 10:21:07 -0400

    feat(icon): added CSS-only utility classes/stylesheet

--- src/dev/pages/icon/icon.ts
+++ src/dev/pages/icon/icon.ts
@@ -1,5 +1,6 @@
 import '$src/shared';
 import '@tylertech/forge/icon';
+import '@tylertech/forge/icon/forge-icon.scss';
 import { IconRegistry } from '@tylertech/forge/icon';
 import { tylIconActionLauncher } from '@tylertech/tyler-icons/custom';
 import { tylIconCode, tylIconFace, tylIconPalette } from '@tylertech/tyler-icons/standard';
