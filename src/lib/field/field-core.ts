ab9cb4525f55dddb8935e1b931885a8fde56c2f6
Nichols, Kieran <kieran.nichols@tylertech.com>
2024-08-23 16:04:31 -0400

    feat: allow for `variant` property on field, text-field, and select to be globally configurable

--- src/lib/field/field-core.ts
+++ src/lib/field/field-core.ts
@@ -60,7 +60,7 @@ export class FieldCore implements IFieldCore {
 
   public initialize(): void {
     this._adapter.addRootListener('slotchange', this._slotChangeListener);
-    this._adapter.tryApplyGlobalConfiguration(['labelPosition']);
+    this._adapter.tryApplyGlobalConfiguration(['labelPosition', 'variant']);
     this._adapter.setLabelPosition(this._labelPosition);
 
     if (this._popoverIcon) {
