dc9803e57dc4ff4216c322f60887b0110942562b
Sam Richardson <sam.richardson@tylertech.com>
2024-09-03 11:46:13 -0400

    docs(switch): add css only documentation

--- src/lib/switch/switch-core.ts
+++ src/lib/switch/switch-core.ts
@@ -22,7 +22,7 @@ export class SwitchCore implements ISwitchCore {
   private _disabled = false;
   private _required = false;
   private _readonly = false;
-  private _icon: SwitchIconVisibility = 'on';
+  private _icon: SwitchIconVisibility = 'both';
   private _labelPosition: SwitchLabelPosition = 'end';
 
   private get _submittedValue(): string | null {
