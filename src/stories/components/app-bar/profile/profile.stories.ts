90c2652141d036ea7382a0b16f441970b18a4e6d
Kieran Nichols <kieran.nichols@tylertech.com>
2024-09-09 15:40:18 -0400

    feat: bump to latest Forge CLI (#681)

--- src/stories/components/app-bar/profile/profile.stories.ts
+++ src/stories/components/app-bar/profile/profile.stories.ts
@@ -1,39 +1,53 @@
-import { html } from 'lit';
 import { type Meta, type StoryObj } from '@storybook/web-components';
-import { customElementStoryRenderer, generateCustomElementArgTypes, standaloneStoryParams } from '../../../utils';
+import { action } from '@storybook/addon-actions';
+import { html } from 'lit';
+import { generateCustomElementArgTypes } from '../../../utils';
 
 import '@tylertech/forge/app-bar';
-import { IMenuOption } from '@tylertech/forge';
 
 const component = 'forge-app-bar-profile-button';
+const profileClickedAction = action('forge-profile-card-profile');
+const signOutClickedAction = action('forge-profile-card-sign-out');
 
 const meta = {
   title: 'Components/App Bar/Profile',
-  render: args => customElementStoryRenderer(component, args),
-  component,
-  parameters: {
-    actions: { disable: true }
+  render: ({ profileButton, profileButtonText, signOutButton, signOutButtonText, open, fullName, email, avatarLetterCount = 2 }) => {
+    return html`
+      <forge-app-bar title-text="Profile">
+        <forge-app-bar-profile-button
+          slot="end"
+          @forge-profile-card-profile=${profileClickedAction}
+          @forge-profile-card-sign-out=${signOutClickedAction}
+          .avatarLetterCount=${avatarLetterCount}
+          .profileButton=${profileButton}
+          .profileButtonText=${profileButtonText}
+          .signOutButton=${signOutButton}
+          .signOutButtonText=${signOutButtonText}
+          .fullName=${fullName}
+          .email=${email}
+          .open=${open}>
+        </forge-app-bar-profile-button>
+      </forge-app-bar>
+    `;
   },
+  component,
   argTypes: {
     ...generateCustomElementArgTypes({
-      tagName: component
+      tagName: component,
+      exclude: ['avatarIcon', 'avatarImageUrl', 'avatarText', 'popupElement', 'profileCardBuilder']
     })
   },
-  args: {}
+  args: {
+    email: 'first.last@tylertech.com',
+    fullName: 'First Last',
+    open: false,
+    profileButton: false,
+    signOutButton: true
+  }
 } satisfies Meta;
 
 export default meta;
 
 type Story = StoryObj;
 
-export const Demo: Story = {
-  ...standaloneStoryParams,
-  render: () => {
-    return html`
-      <forge-app-bar title-text="Profile">
-        <forge-app-bar-profile-button slot="end" profile-button="true" avatar-text="First Last" full-name="First Last" email="first.last@tylertech.com">
-        </forge-app-bar-profile-button>
-      </forge-app-bar>
-    `;
-  }
-};
+export const Demo: Story = {};
