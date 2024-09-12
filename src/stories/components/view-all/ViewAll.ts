65c4a6bc50b9a9fdc290b0b0af8c28f7ee280b83
Nick Andrews <nick.andrews@tylertech.com>
2024-09-12 06:54:44 -0400

    Origin/feat/create view all stories page (#684)
    * chore: created a view all stories page to setup automated ARC platform scanning
    
    * chore: removed URL from sidebar
    
    * chore: added hidden tag to the MDX file as well

---
+++ src/stories/components/view-all/ViewAll.ts
@@ -0,0 +1,9 @@
+import { type Meta, type StoryObj } from '@storybook/web-components';
+
+const meta = {
+  title: 'Components/View All',
+  tags: ['hidden'],
+  id: 'view-all-components'
+} satisfies Meta;
+
+export default meta;
