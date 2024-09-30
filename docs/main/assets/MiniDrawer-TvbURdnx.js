12223546f0a06a2f4f521ae5a2ad1445ac258859
DRiFTy17 <DRiFTy17@users.noreply.github.com>
2024-09-30 15:23:30 +0000

    Deploying to gh-pages from @ tyler-technologies-oss/forge@d7c3ee3ca249e995db04bd936d409e1f1c820561 🚀

---
+++ docs/main/assets/MiniDrawer-TvbURdnx.js
@@ -0,0 +1,21 @@
+import{j as r}from"./jsx-runtime-BVEr3Owy.js";import{u as n}from"./index-DDbNBnRE.js";import{ae as s,af as p,ag as e}from"./index-SrqNcHCc.js";import{C as m}from"./CustomArgTypes-DcGUe_fd.js";import{M as a,D as d,H as c}from"./MiniDrawer.stories-Cq7nxLbT.js";import"./iframe-J02bvO-g.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-D-8MO0q_.js";import"./index-DxKRhftL.js";import"./index-DrFu-skq.js";import"./utils-Dj42C_k3.js";import"./lit-element-Dk2-kgKT.js";import"./lit-html-DZH-Jm0H.js";import"./chunk-454WOBUV-CM0pFb8Z.js";import"./v4-CQkTLCs1.js";import"./index-ByifSpfC.js";import"./icon-DHpZ4R73.js";import"./constants-DjE6emXm.js";import"./base-adapter-F7QHxK2H.js";import"./index-Dh0vMUMR.js";import"./ref-9TtedaQt.js";import"./directive-helpers-DnQ__Q7V.js";import"./directive-Ctav8iJK.js";import"./mini-drawer-COWeblG_.js";import"./base-drawer-CHX_xDf6.js";import"./event-utils-CyAMYupJ.js";import"./focus-indicator-_fDu4ZqT.js";import"./list-ePbvhvCk.js";import"./state-layer-DTKAXCUq.js";import"./with-default-aria-DCLjqsVH.js";import"./toolbar-CtEd8mqT.js";import"./scaffold-R2qvsZCm.js";import"./card-DdiAyW6J.js";import"./app-bar-profile-button-B17Ml3ru.js";import"./icon-button-XdSjYqUR.js";import"./base-button-adapter-WOmen6Ii.js";import"./with-label-aware-CLWydNrR.js";import"./tooltip-CoCQ3Otm.js";import"./overlay-DWm8nYOy.js";import"./with-longpress-listener-D-8wsf8o.js";import"./dismissible-stack-DoZLb9q6.js";import"./badge-CO5a_--I.js";import"./menu-n2vU0IDR.js";import"./list-dropdown-aware-core-BJT0uMnR.js";import"./list-dropdown-IlYW5PKf.js";import"./event-utils-C1SDeUaq.js";import"./linear-progress-CcMix19v.js";import"./popover-tgjxHp7t.js";import"./skeleton-Cs99PVGD.js";import"./a11y-BxM9_46k.js";import"./base-component-delegate-C6iOaLFP.js";import"./avatar-Cja6atCs.js";import"./button-C5f1g9CL.js";function t(i){const o={a:"a",blockquote:"blockquote",code:"code",h2:"h2",li:"li",p:"p",strong:"strong",ul:"ul",...n(),...i.components};return r.jsxs(r.Fragment,{children:[r.jsx(s,{of:a}),`
+`,r.jsx(p,{}),`
+`,r.jsx(o.p,{children:"Mini drawers are used to display secondary content, and are typically used for navigation menu purposes."}),`
+`,r.jsx(o.p,{children:"There are two types of mini drawers:"}),`
+`,r.jsxs(o.ul,{children:[`
+`,r.jsxs(o.li,{children:[r.jsx(o.strong,{children:"dismissible:"})," can be hidden by the user via a button provided by the developer"]}),`
+`,r.jsxs(o.li,{children:[r.jsx(o.strong,{children:"permanent:"})," always visible"]}),`
+`]}),`
+`,r.jsxs(o.p,{children:["The mini drawer component is a variant of the standard ",r.jsx(o.a,{href:"?path=/docs/components-drawer--docs",children:"Drawer"})," component, but with a smaller width."]}),`
+`,r.jsx(e,{of:d}),`
+`,r.jsx(o.h2,{id:"hover",children:"Hover"}),`
+`,r.jsx(o.p,{children:"The mini drawer can be configured to open to a larger width when hovered with a pointer. This is useful for desktop applications where the user has a mouse."}),`
+`,r.jsx(e,{of:c}),`
+`,r.jsxs(o.blockquote,{children:[`
+`,r.jsxs(o.p,{children:[r.jsx(o.strong,{children:"Note:"})," The hover feature is not available on touch devices."]}),`
+`]}),`
+`,r.jsx(o.h2,{id:"api",children:"API"}),`
+`,r.jsx(m,{}),`
+`,r.jsx(o.h2,{id:"accessibility",children:"Accessibility"}),`
+`,r.jsxs(o.p,{children:["The drawer component does ",r.jsx(o.strong,{children:"not"})," provide any semantics by default. Developers should provide their own ",r.jsx(o.code,{children:"<aside>"}),` for navigation,
+or various ARIA roles in other scenarios.`]})]})}function hr(i={}){const{wrapper:o}={...n(),...i.components};return o?r.jsx(o,{...i,children:r.jsx(t,{...i})}):t(i)}export{hr as default};
---
+++ docs/main/assets/index-BtlFb7jo.js
@@ -0,0 +1,1 @@
+import{M as a,u as n}from"./index-DDbNBnRE.js";import"./index-SrqNcHCc.js";import"./iframe-J02bvO-g.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-D-8MO0q_.js";import"./index-DxKRhftL.js";import"./index-DrFu-skq.js";export{a as MDXProvider,n as useMDXComponents};
