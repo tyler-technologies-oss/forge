c687ad703840377cec7d464ef55a282b853d4662
DRiFTy17 <DRiFTy17@users.noreply.github.com>
2024-10-09 12:34:52 +0000

    Deploying to gh-pages from @ tyler-technologies-oss/forge@2c89759ec23d61e8fe1406a5fc1ed270c6073b4c 🚀

---
+++ docs/main/assets/FloatingActionButton-6-x2GB9c.js
@@ -0,0 +1,33 @@
+import{j as n}from"./jsx-runtime-09Psuw5f.js";import{u as r}from"./index-CifnwB8G.js";import{ae as s,af as a,ag as i}from"./index-DS3-dU_A.js";import{C as c}from"./CustomArgTypes-BNrPzZVJ.js";import{C as l}from"./CssOnlyInformation-C_u6VLNH.js";import{F as d,D as h,E as p,W as m,C as x}from"./FloatingActionButton.stories-BVZuwDYO.js";import"./iframe-CQnZhdaF.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-D-8MO0q_.js";import"./index-DxKRhftL.js";import"./index-DrFu-skq.js";import"./utils-CRuDW25z.js";import"./lit-element-Dk2-kgKT.js";import"./lit-html-DZH-Jm0H.js";import"./style-map-DxfbqtuX.js";import"./directive-Ctav8iJK.js";import"./class-map-D93gIiBE.js";import"./index-ByifSpfC.js";import"./icon-DHpZ4R73.js";import"./constants-DjE6emXm.js";import"./base-adapter-F7QHxK2H.js";import"./index-Dh0vMUMR.js";import"./floating-action-button-CB3ucuU_.js";import"./base-button-adapter-WOmen6Ii.js";import"./with-label-aware-CLWydNrR.js";import"./with-default-aria-DCLjqsVH.js";import"./focus-indicator-_fDu4ZqT.js";import"./state-layer-DTKAXCUq.js";import"./button-C5f1g9CL.js";function o(t){const e={code:"code",h2:"h2",li:"li",p:"p",pre:"pre",ul:"ul",...r(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(s,{of:d}),`
+`,n.jsx(a,{}),`
+`,n.jsx(e.p,{children:"Use floating action buttons to represent the primary action on a screen within an application. It's recommended to only use one floating action button per screen."}),`
+`,n.jsx(i,{of:h}),`
+`,n.jsx(e.h2,{id:"positioning",children:"Positioning"}),`
+`,n.jsx(e.p,{children:"Typically you will position floating action buttons manually on the screen, for example to apply a fixed position in the bottom-right you could use this CSS:"}),`
+`,n.jsx(e.pre,{children:n.jsx(e.code,{className:"language-css",children:`.bottom-right {
+  position: fixed;
+  bottom: var(--forge-spacing-medium);
+  right: var(--forge-spacing-medium);
+}
+`})}),`
+`,n.jsx(e.h2,{id:"extended",children:"Extended"}),`
+`,n.jsx(e.p,{children:"Extended floating action buttons are larger and have a text label."}),`
+`,n.jsx(i,{of:p}),`
+`,n.jsx(e.h2,{id:"with-anchor",children:"With Anchor"}),`
+`,n.jsxs(e.p,{children:["You can nest an ",n.jsx(e.code,{children:"<a>"})," element inside the floating action button to create a link."]}),`
+`,n.jsx(i,{of:m}),`
+`,n.jsx(e.h2,{id:"api",children:"API"}),`
+`,n.jsx(c,{}),`
+`,n.jsx(e.h2,{id:"accessibility",children:"Accessibility"}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsxs(e.li,{children:["Buttons containing only icons should be given a meaningful label via ",n.jsx(e.code,{children:"aria-label"})," or ",n.jsx(e.code,{children:"aria-labelledby"}),"."]}),`
+`,n.jsxs(e.li,{children:["Avoid using capitalized text because screen readers will read the text character-by-character. Instead use ",n.jsx(e.code,{children:"text-transform: uppercase"}),"."]}),`
+`,n.jsx(e.li,{children:"Ensure the FAB can be reached by keyboard navigation."}),`
+`,n.jsx(e.li,{children:"Ensure that there is a distinct visual cue when the FAB is in focus."}),`
+`,n.jsx(e.li,{children:"Verify that there is sufficient contrast between the foreground text and background to meet WCAG requirements."}),`
+`,n.jsx(e.li,{children:"Ensure that buttons placed above other content on the page have proper contrast ratio."}),`
+`]}),`
+`,n.jsx(e.h2,{id:"css-only",children:"CSS-Only"}),`
+`,n.jsx(e.p,{children:"The floating action button component is also available as a CSS-only component without the need for JavaScript."}),`
+`,n.jsx(i,{of:x}),`
+`,n.jsx(l,{})]})}function R(t={}){const{wrapper:e}={...r(),...t.components};return e?n.jsx(e,{...t,children:n.jsx(o,{...t})}):o(t)}export{R as default};
---
+++ docs/main/assets/Tabs-CML8u_yp.js
@@ -0,0 +1,20 @@
+import{j as e}from"./jsx-runtime-09Psuw5f.js";import{u as r}from"./index-CifnwB8G.js";import{ae as a,af as s,ag as o}from"./index-DS3-dU_A.js";import{C as c}from"./CustomArgTypes-BNrPzZVJ.js";import{T as l,D as d,S as p}from"./Tabs.stories-BNxZ8KSh.js";import"./iframe-CQnZhdaF.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-D-8MO0q_.js";import"./index-DxKRhftL.js";import"./index-DrFu-skq.js";import"./utils-CRuDW25z.js";import"./lit-element-Dk2-kgKT.js";import"./lit-html-DZH-Jm0H.js";import"./chunk-454WOBUV-CM0pFb8Z.js";import"./v4-CQkTLCs1.js";import"./style-map-DxfbqtuX.js";import"./directive-Ctav8iJK.js";import"./index-ByifSpfC.js";import"./index-fxMNKkgx.js";import"./icon-DHpZ4R73.js";import"./constants-DjE6emXm.js";import"./base-adapter-F7QHxK2H.js";import"./index-Dh0vMUMR.js";import"./tab-bar-BGusw8aD.js";import"./icon-button-XdSjYqUR.js";import"./base-button-adapter-WOmen6Ii.js";import"./with-label-aware-CLWydNrR.js";import"./with-default-aria-DCLjqsVH.js";import"./focus-indicator-_fDu4ZqT.js";import"./state-layer-DTKAXCUq.js";function i(n){const t={code:"code",h2:"h2",li:"li",p:"p",ul:"ul",...r(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(a,{of:l}),`
+`,e.jsx(s,{}),`
+`,e.jsx(t.p,{children:`Tabs are used to quickly navigate between different views, and group related content any level of hierarchy. They may be used as primary
+or secondary navigation within a page.`}),`
+`,e.jsx(o,{of:d}),`
+`,e.jsx(t.h2,{id:"secondary",children:"Secondary"}),`
+`,e.jsx(t.p,{children:"Secondary tabs are used within the main content of your page."}),`
+`,e.jsx(o,{of:p}),`
+`,e.jsx(t.h2,{id:"api",children:"API"}),`
+`,e.jsx(c,{}),`
+`,e.jsx(t.h2,{id:"accessibility",children:"Accessibility"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsx(t.li,{children:"Ensure that all of the controls that are accessible by a mouse are also accessible by keyboard."}),`
+`,e.jsx(t.li,{children:"Ensure each control can be updated or activated by the keyboard."}),`
+`,e.jsx(t.li,{children:"Ensure that only the selected tab content is reachable by keyboard."}),`
+`]}),`
+`,e.jsxs(t.p,{children:["It's expected that you will need to provide the ",e.jsx(t.code,{children:"tablist"})," and ",e.jsx(t.code,{children:"tabpanel"}),` roles to your elements to indicate to assistive technologies
+which elements are responsible for the tabs and tab content respectively. Each tab should contain an `,e.jsx(t.code,{children:"aria-controls"}),` property identifying
+its corresponding tab content. In addition, each tab content element should be labelled by a tab. These elements need to be wrapped in an
+element containing a `,e.jsx(t.code,{children:"tablist"})," role:"]})]})}function J(n={}){const{wrapper:t}={...r(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(i,{...n})}):i(n)}export{J as default};
