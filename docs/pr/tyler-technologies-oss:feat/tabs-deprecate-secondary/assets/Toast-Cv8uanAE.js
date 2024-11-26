469fa7934293f1aa8db11f94d4e486526a134a1f
DRiFTy17 <DRiFTy17@users.noreply.github.com>
2024-11-26 20:30:15 +0000

    Deploying to gh-pages from @ tyler-technologies-oss/forge@62056969431648587ce8411b06f94c7bfb76dd8b 🚀

---
+++ docs/pr/tyler-technologies-oss:feat/tabs-deprecate-secondary/assets/Toast-Cv8uanAE.js
@@ -0,0 +1,44 @@
+import{ae as e,af as r,ag as a,ah as o}from"./index-BlHPO2GA.js";import{u as n}from"./index-C82fMyDs.js";import{C as c}from"./CustomArgTypes-CFUMVjC1.js";import{T as l,D as m,a as p}from"./Toast.stories-B5rg69tO.js";import"./iframe-DsRP_sey.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-D-8MO0q_.js";import"./index-BHYIh-Xd.js";import"./index-DrFu-skq.js";import"./utils-CUtp3IhO.js";import"./lit-element-CgJqSpuc.js";import"./lit-html-paDGiEfB.js";import"./chunk-D5ZWXAHU-CGElDDNX.js";import"./v4-CQkTLCs1.js";import"./style-map-C9nPWcxA.js";import"./directive-CF8sV3Lr.js";import"./ref-DJjbfkOF.js";import"./directive-helpers-DcB0QpIl.js";import"./constants-CFf81ck9.js";import"./toast-eRimfdvt.js";import"./base-adapter-Dh44vCkH.js";import"./index-ByifSpfC.js";import"./button-CVZhEkBO.js";import"./icon-FszQmWVN.js";import"./index-BmocOEUj.js";import"./focus-indicator-DesOnyyZ.js";import"./state-layer-COSQHCpS.js";import"./base-button-adapter-BS886vuU.js";import"./with-label-aware-CjYHyB6d.js";import"./with-default-aria-CAIVLMQ_.js";import"./dialog-DMZjj_XW.js";import"./backdrop-DIWDpEO-.js";import"./dismissible-stack-DVth9mRg.js";import"./icon-button-DxSYWoFH.js";import"./overlay-DUpFUxF7.js";function i(t){const s={a:"a",blockquote:"blockquote",code:"code",h2:"h2",li:"li",p:"p",pre:"pre",ul:"ul",...n(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(r,{of:l}),`
+`,e.jsx(a,{}),`
+`,e.jsx(s.p,{children:"Toasts are non-modal notifications that appear in response to user interactions. They can optionally provide a dismissible button, but automatically dismiss after a set duration."}),`
+`,e.jsx(o,{of:m}),`
+`,e.jsx(s.h2,{id:"dismissible",children:"Dismissible"}),`
+`,e.jsxs(s.p,{children:["Toasts can be dismissed by the user when setting ",e.jsx(s.code,{children:"dismissible"})," to ",e.jsx(s.code,{children:"true"}),"."]}),`
+`,e.jsx(o,{of:p}),`
+`,e.jsx(s.h2,{id:"dynamic-usage",children:"Dynamic Usage"}),`
+`,e.jsx(s.p,{children:"Toasts are typically created dynamically in response to user interactions. The following example demonstrates how to create a toast from JavaScript."}),`
+`,e.jsx(s.pre,{children:e.jsx(s.code,{className:"language-javascript",children:`ToastComponent.present({ message: 'Save successful' });
+`})}),`
+`,e.jsxs(s.blockquote,{children:[`
+`,e.jsxs(s.p,{children:["Toasts will automatically dismiss after the ",e.jsx(s.code,{children:"duration"})," elapses and remove themselves from the DOM."]}),`
+`]}),`
+`,e.jsx(s.h2,{id:"declarative-usage",children:"Declarative Usage"}),`
+`,e.jsxs(s.p,{children:["Toasts can also be used inline declaratively in your HTML and toggled via the ",e.jsx(s.code,{children:"open"})," property/attribute."]}),`
+`,e.jsx(s.pre,{children:e.jsx(s.code,{className:"language-html",children:`<forge-toast open>Save successful</forge-toast>
+`})}),`
+`,e.jsxs(s.blockquote,{children:[`
+`,e.jsxs(s.p,{children:["Inline toasts do not automatically remove themselves from the DOM. You must toggle the ",e.jsx(s.code,{children:"open"})," attribute to hide the toast."]}),`
+`]}),`
+`,e.jsx(s.h2,{id:"angular-usage",children:"Angular Usage"}),`
+`,e.jsxs(s.p,{children:["The ",e.jsx(s.a,{href:"?path=/docs/frameworks-angular--docs",children:"Angular adapter"})," provides a ",e.jsx(s.code,{children:"ToastService"})," that can be used to show toasts from your Angular components."]}),`
+`,e.jsx(s.pre,{children:e.jsx(s.code,{className:"language-typescript",children:`import { ToastService } from '@tylertech/forge-angular';
+
+@Component({
+  selector: 'app-my-component',
+  template: \`<button (click)="showToast()">Show Toast</button>\`
+})
+export class MyComponent {
+  constructor(private toastService: ToastService) {}
+
+  showToast() {
+    this.toastService.show({ message: 'Save successful' });
+  }
+}
+`})}),`
+`,e.jsx(s.h2,{id:"api",children:"API"}),`
+`,e.jsx(c,{}),`
+`,e.jsx(s.h2,{id:"accessibility",children:"Accessibility"}),`
+`,e.jsxs(s.ul,{children:[`
+`,e.jsx(s.li,{children:"Ensure that the dismiss button is accessible by keyboard."}),`
+`,e.jsx(s.li,{children:"If color conveys important information, provide additional cues for users with color perception deficiencies."}),`
+`]})]})}function K(t={}){const{wrapper:s}={...n(),...t.components};return s?e.jsx(s,{...t,children:e.jsx(i,{...t})}):i(t)}export{K as default};
---
+++ docs/pr/tyler-technologies-oss:feat/tabs-deprecate-secondary/assets/Vue-BBP4EthS.js
@@ -0,0 +1,39 @@
+import{ae as e,af as s}from"./index-BlHPO2GA.js";import{u as r}from"./index-C82fMyDs.js";import"./iframe-DsRP_sey.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-D-8MO0q_.js";import"./index-BHYIh-Xd.js";import"./index-DrFu-skq.js";function o(t){const n={a:"a",code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...r(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(s,{title:"Frameworks/Vue"}),`
+`,e.jsx(n.h1,{id:"vue",children:"Vue"}),`
+`,e.jsxs(n.p,{children:["Vue has ",e.jsx(n.a,{href:"https://custom-elements-everywhere.com/#vue",rel:"nofollow",children:"great support"})," for custom elements and can be used directly."]}),`
+`,e.jsx(n.h2,{id:"installation",children:"Installation"}),`
+`,e.jsx(n.p,{children:"To use Forge in your Vue application, install the following package from npm:"}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-bash",children:`npm install @tylertech/forge
+`})}),`
+`,e.jsx(n.h2,{id:"loading-the-tyler-font",children:"Loading the Tyler Font"}),`
+`,e.jsxs(n.p,{children:["Edit your ",e.jsx(n.code,{children:"index.html"})," file and add the following ",e.jsx(n.code,{children:"<link>"})," tag to the ",e.jsx(n.code,{children:"<head>"})," section load the Tyler font:"]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-html",children:`<link rel="stylesheet" href="https://cdn.forge.tylertech.com/v1/css/tyler-font.css" />
+`})}),`
+`,e.jsx(n.p,{children:"This will configure the default font family for your application, and load the required font weights and styles."}),`
+`,e.jsx(n.h2,{id:"importing-components",children:"Importing Components"}),`
+`,e.jsxs(n.p,{children:["To use Forge components in your Vue application, import the components you need from the ",e.jsx(n.code,{children:"@tylertech/forge"})," package."]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-html",children:`<template>
+  <div>
+    <forge-card>
+      <forge-button>Click me</forge-button>
+    </forge-card>
+  </div>
+</template>
+
+<script>
+  import { defineButtonComponent, defineCardComponent } from '@tylertech/forge';
+
+  defineButtonComponent();
+  defineCardComponent();
+
+  export default {
+    name: 'App'
+  };
+<\/script>
+`})}),`
+`,e.jsx(n.h2,{id:"importing-styles",children:"Importing Styles"}),`
+`,e.jsxs(n.p,{children:["To use the default Forge styles in your Vue application, import the ",e.jsx(n.code,{children:"forge.css"})," file from the ",e.jsx(n.code,{children:"@tylertech/forge"})," package."]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-vue",children:`<style>
+@import '@tylertech/forge/dist/forge.css';
+</style>
+`})})]})}function f(t={}){const{wrapper:n}={...r(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(o,{...t})}):o(t)}export{f as default};
