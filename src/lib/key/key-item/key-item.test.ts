2b9f3f43cb2a86132ee7b443055ba5e6fc74d163
Sam Richardson <sam.richardson@tylertech.com>
2025-01-21 11:55:56 -0500

    test(key): fixkey item test

--- src/lib/key/key-item/key-item.test.ts
+++ src/lib/key/key-item/key-item.test.ts
@@ -1,12 +1,12 @@
 import { expect } from '@esm-bundle/chai';
-import { fixture, html } from '@open-wc/testing';
-import { IKeyItemComponent } from './key-item';
+import { elementUpdated, fixture, html } from '@open-wc/testing';
+import { KeyItemComponent } from './key-item';
 
 import './key-item';
 
 describe('Key Item', () => {
   it('should instantiate shadow root', async () => {
-    const el = await fixture<IKeyItemComponent>(html`<forge-key-item></forge-key-item>`);
+    const el = await fixture<KeyItemComponent>(html`<forge-key-item></forge-key-item>`);
 
     expect(el.shadowRoot).not.to.be.null;
   });
