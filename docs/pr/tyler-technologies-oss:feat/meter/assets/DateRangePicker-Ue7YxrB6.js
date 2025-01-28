7e2d192987e1b03264239fb54c6c5cc4afd245d3
samrichardsontylertech <samrichardsontylertech@users.noreply.github.com>
2025-01-28 21:22:59 +0000

    Deploying to gh-pages from @ tyler-technologies-oss/forge@8137bc528c3dfd60bdca9f2a40b1d8c116bd0dab 🚀

---
+++ docs/pr/tyler-technologies-oss:feat/meter/assets/DateRangePicker-Ue7YxrB6.js
@@ -0,0 +1,22 @@
+import{j as e,M as r,T as s,C as a}from"./index-Csoc5hMa.js";import{useMDXComponents as o}from"./index-BoSWM9Lc.js";import{C as d}from"./CustomArgTypes-L6kiB7Yq.js";import{D as p,a as c}from"./DateRangePicker.stories-sO3w00Nu.js";import"./iframe-Bi5wsXGt.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-D-8MO0q_.js";import"./index-BHYIh-Xd.js";import"./index-DrFu-skq.js";import"./utils-CfUP_kYQ.js";import"./lit-element-JplMEnZc.js";import"./lit-html-paDGiEfB.js";import"./feature-detection-ONR9WHvu.js";import"./date-range-picker-CL7pfA5X.js";import"./constants-BjnHqKgS.js";import"./base-adapter-CQdYccXX.js";import"./index-ByifSpfC.js";import"./calendar-C9slwyY2.js";import"./tooltip-D0ywHBAv.js";import"./focus-indicator-R2otSvsR.js";import"./index-BmocOEUj.js";import"./overlay-DAcircNE.js";import"./with-longpress-listener-S3ft74cg.js";import"./dismissible-stack-9mJiid_W.js";import"./with-default-aria-BcIvJ7-x.js";import"./a11y-utils-BOPvdiVn.js";import"./state-layer-B7GOb8iB.js";import"./event-utils-C1SDeUaq.js";import"./button-vPgaRyW5.js";import"./icon-B5S0VGIT.js";import"./base-button-adapter-OmzAW3c3.js";import"./with-label-aware-DAaZnhel.js";import"./icon-button-DipNuXsM.js";import"./base-date-picker-core-AaWlVST0.js";import"./a11y-BxM9_46k.js";import"./popover-Dq2OmXS_.js";import"./text-field-DZnzThaA.js";import"./base-field-BQVpYEWj.js";import"./label-CVpYv1kf.js";import"./button-toggle-group-7alPFAgU.js";import"./with-form-associated-Dm8KqB07.js";import"./checkbox-e65WGDEs.js";import"./switch-BRBW-vzp.js";function n(t){const i={code:"code",h2:"h2",li:"li",p:"p",ul:"ul",...o(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(r,{of:p}),`
+`,e.jsx(s,{}),`
+`,e.jsxs(i.p,{children:["The date range picker is very similar to the usage of the standard datepicker component, the only difference is that it allows for the selection of two dates that define a start and end range. This means that when setting up the component, you must provide it two ",e.jsx(i.code,{children:"<input>"})," elements, one for the start date and one for the end date."]}),`
+`,e.jsx(i.p,{children:"There are many configuration options available through the component API, along with date masking options that force the dates to be entered using a specific format."}),`
+`,e.jsx(a,{of:c}),`
+`,e.jsx(i.h2,{id:"api",children:"API"}),`
+`,e.jsx(d,{}),`
+`,e.jsx(i.h2,{id:"accessibility",children:"Accessibility"}),`
+`,e.jsxs(i.ul,{children:[`
+`,e.jsx(i.li,{children:"When using a screen reader, ensure keyboard navigation in the calendar is announced."}),`
+`,e.jsxs(i.li,{children:["Be sure that you add the proper ",e.jsx(i.code,{children:"aria-label"})," to the ",e.jsx(i.code,{children:"<input>"})," element if necessary."]}),`
+`,e.jsxs(i.li,{children:["The date picker component will add the following ARIA attributes to the ",e.jsx(i.code,{children:"<input>"})," element for you:",`
+`,e.jsxs(i.ul,{children:[`
+`,e.jsx(i.li,{children:e.jsx(i.code,{children:"aria-live"})}),`
+`,e.jsx(i.li,{children:e.jsx(i.code,{children:"aria-atomic"})}),`
+`,e.jsx(i.li,{children:e.jsx(i.code,{children:"aria-haspopup"})}),`
+`,e.jsx(i.li,{children:e.jsx(i.code,{children:"aria-expanded"})}),`
+`,e.jsx(i.li,{children:e.jsx(i.code,{children:"aria-owns"})}),`
+`,e.jsx(i.li,{children:e.jsx(i.code,{children:"aria-disabled"})}),`
+`]}),`
+`]}),`
+`]})]})}function ee(t={}){const{wrapper:i}={...o(),...t.components};return i?e.jsx(i,{...t,children:e.jsx(n,{...t})}):n(t)}export{ee as default};
---
+++ docs/pr/tyler-technologies-oss:feat/meter/assets/ViewSwitcher-ChE3155I.js
@@ -0,0 +1,21 @@
+import{j as e,M as s,T as r,C as l}from"./index-Csoc5hMa.js";import{useMDXComponents as o}from"./index-BoSWM9Lc.js";import{C as c}from"./CustomArgTypes-L6kiB7Yq.js";import{V as h,D as a}from"./ViewSwitcher.stories-BI6lKlUd.js";import"./iframe-Bi5wsXGt.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-D-8MO0q_.js";import"./index-BHYIh-Xd.js";import"./index-DrFu-skq.js";import"./utils-CfUP_kYQ.js";import"./lit-element-JplMEnZc.js";import"./lit-html-paDGiEfB.js";import"./feature-detection-ONR9WHvu.js";import"./view-switcher-DNTvJD0u.js";import"./constants-BjnHqKgS.js";import"./base-adapter-CQdYccXX.js";import"./tab-bar-0hPfWDTL.js";import"./icon-B5S0VGIT.js";import"./index-BmocOEUj.js";import"./icon-button-DipNuXsM.js";import"./base-button-adapter-OmzAW3c3.js";import"./index-ByifSpfC.js";import"./with-label-aware-DAaZnhel.js";import"./with-default-aria-BcIvJ7-x.js";import"./a11y-utils-BOPvdiVn.js";import"./focus-indicator-R2otSvsR.js";import"./state-layer-B7GOb8iB.js";import"./ref-DJjbfkOF.js";import"./directive-helpers-DcB0QpIl.js";import"./directive-CF8sV3Lr.js";import"./decorators-DOnQS6BC.js";function n(t){const i={blockquote:"blockquote",code:"code",h2:"h2",li:"li",p:"p",strong:"strong",ul:"ul",...o(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(s,{of:h}),`
+`,e.jsx(r,{}),`
+`,e.jsx(i.p,{children:"The view switcher component allows users to toggle between different views of content. This component is often used in conjunction with tabs to control which view actively displayed."}),`
+`,e.jsxs(i.blockquote,{children:[`
+`,e.jsxs(i.p,{children:[e.jsx(i.strong,{children:"Note"}),": Content hidden by this component is still present in the DOM unless you control it manually."]}),`
+`]}),`
+`,e.jsx(l,{of:a}),`
+`,e.jsx(i.h2,{id:"animations",children:"Animations"}),`
+`,e.jsxs(i.p,{children:["The view switcher can animate between views using the views via the ",e.jsx(i.code,{children:"animationType"})," property/attribute. The following animations are available:"]}),`
+`,e.jsxs(i.ul,{children:[`
+`,e.jsxs(i.li,{children:[e.jsx(i.code,{children:"slide"}),": The new view slides in from the right while the old view slides out to the left."]}),`
+`,e.jsxs(i.li,{children:[e.jsx(i.code,{children:"fade"}),": The new view fades in while the old view fades out."]}),`
+`,e.jsxs(i.li,{children:[e.jsx(i.code,{children:"none"}),": The new view replaces the old view without any animation. This is the default."]}),`
+`]}),`
+`,e.jsx(i.h2,{id:"api",children:"API"}),`
+`,e.jsx(c,{}),`
+`,e.jsx(i.h2,{id:"accessibility",children:"Accessibility"}),`
+`,e.jsxs(i.ul,{children:[`
+`,e.jsxs(i.li,{children:["Verify that you can tab into the ",e.jsx(i.code,{children:"<forge-view>"})," content."]}),`
+`,e.jsxs(i.li,{children:["Verify that you can access elements within ",e.jsx(i.code,{children:"<forge-view>"})," content using only the keyboard."]}),`
+`]})]})}function z(t={}){const{wrapper:i}={...o(),...t.components};return i?e.jsx(i,{...t,children:e.jsx(n,{...t})}):n(t)}export{z as default};
