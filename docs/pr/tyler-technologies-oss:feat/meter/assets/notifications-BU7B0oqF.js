6dda719260a8239bf072f9ee1810c9812a2ffb8d
samrichardsontylertech <samrichardsontylertech@users.noreply.github.com>
2025-01-29 17:14:48 +0000

    Deploying to gh-pages from @ tyler-technologies-oss/forge@86e44e5d63de218defe3d151ac6508e65464f66b 🚀

---
+++ docs/pr/tyler-technologies-oss:feat/meter/assets/notifications-BU7B0oqF.js
@@ -0,0 +1,19 @@
+import{j as t,M as r,T as s,C as p}from"./index-C6Hdcxq5.js";import{useMDXComponents as e}from"./index-CsBpHjjB.js";import{C as c}from"./CustomArgTypes-DI-WbSD2.js";import{N as m,D as a}from"./notifications.stories-Dxw9B8lZ.js";import"./iframe-DI60IBBq.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-D-8MO0q_.js";import"./index-BHYIh-Xd.js";import"./index-DrFu-skq.js";import"./utils-bkiwdWVT.js";import"./lit-element-JplMEnZc.js";import"./lit-html-paDGiEfB.js";import"./feature-detection-ONR9WHvu.js";import"./app-bar-profile-button-CSm9dK8_.js";import"./constants-BjnHqKgS.js";import"./base-adapter-CQdYccXX.js";import"./focus-indicator-R2otSvsR.js";import"./index-BmocOEUj.js";import"./state-layer-B7GOb8iB.js";import"./index-ByifSpfC.js";import"./icon-B5S0VGIT.js";import"./icon-button-DipNuXsM.js";import"./base-button-adapter-OmzAW3c3.js";import"./with-label-aware-DAaZnhel.js";import"./with-default-aria-BcIvJ7-x.js";import"./a11y-utils-BOPvdiVn.js";import"./tooltip-D0ywHBAv.js";import"./overlay-DAcircNE.js";import"./with-longpress-listener-S3ft74cg.js";import"./dismissible-stack-9mJiid_W.js";import"./badge-CzgFSHGZ.js";import"./menu-C_FdmaDG.js";import"./list-CfkU0djA.js";import"./list-dropdown-aware-core-CaA2Y1xF.js";import"./list-dropdown-DebMMgrE.js";import"./event-utils-C1SDeUaq.js";import"./linear-progress-DPUjJFYN.js";import"./popover-Dq2OmXS_.js";import"./skeleton-Dfdgg-pt.js";import"./a11y-BxM9_46k.js";import"./base-component-delegate-D9tVS5jC.js";import"./avatar-Du1LPt_G.js";import"./button-vPgaRyW5.js";import"./toolbar-Lhya2ayG.js";function n(i){const o={blockquote:"blockquote",code:"code",h2:"h2",li:"li",p:"p",ul:"ul",...e(),...i.components};return t.jsxs(t.Fragment,{children:[t.jsx(r,{of:m}),`
+`,t.jsx(s,{}),`
+`,t.jsxs(o.p,{children:["For convenience Forge provides the ",t.jsx(o.code,{children:"<forge-app-bar-notification-button>"})," component. This component uses an icon-button with a predefined icon. The power of this component is that it will ensure a consistent experience for users across all applications that consume it."]}),`
+`,t.jsx(o.p,{children:"If your application provides system notifications, use this component to show those notifications to the user."}),`
+`,t.jsxs(o.blockquote,{children:[`
+`,t.jsxs(o.p,{children:["It is expected that you place this component in the ",t.jsx(o.code,{children:"end"})," slot of the app-bar."]}),`
+`]}),`
+`,t.jsx(o.h2,{id:"default",children:"Default"}),`
+`,t.jsx(o.p,{children:"The notification button provides a consistent icon, as well as options to control an optional built-in badge."}),`
+`,t.jsx(p,{of:a}),`
+`,t.jsxs(o.blockquote,{children:[`
+`,t.jsx(o.p,{children:"In the future this component may have the ability to display notifications in a common format, but that functionality does not exist yet."}),`
+`]}),`
+`,t.jsx(o.h2,{id:"api",children:"API"}),`
+`,t.jsx(c,{}),`
+`,t.jsx(o.h2,{id:"accessibility",children:"Accessibility"}),`
+`,t.jsxs(o.ul,{children:[`
+`,t.jsx(o.li,{children:"The component will handle adding necessary ARIA attributes for you, as well as ensure the proper color contrast ratio is used."}),`
+`]})]})}function ot(i={}){const{wrapper:o}={...e(),...i.components};return o?t.jsx(o,{...i,children:t.jsx(n,{...i})}):n(i)}export{ot as default};
