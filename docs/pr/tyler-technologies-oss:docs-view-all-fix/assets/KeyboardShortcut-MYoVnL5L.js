66fa47ce1ae687cf78abfd348f4af28ae1c990bb
DRiFTy17 <DRiFTy17@users.noreply.github.com>
2025-02-04 14:45:14 +0000

    Deploying to gh-pages from @ tyler-technologies-oss/forge@533871e0bc96eff5534e5dc1b9142d7c8e252cb7 🚀

---
+++ docs/pr/tyler-technologies-oss:docs-view-all-fix/assets/KeyboardShortcut-MYoVnL5L.js
@@ -0,0 +1,38 @@
+import{j as e,M as i,T as s,C as c}from"./index-Ggnb1H2z.js";import{useMDXComponents as n}from"./index-CxOWqXOh.js";import{C as a}from"./CustomArgTypes-BiEDufQE.js";import{K as l,D as h}from"./KeyboardShortcut.stories-CWIgSUzh.js";import"./iframe-CohUvtCd.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-D-8MO0q_.js";import"./index-BHYIh-Xd.js";import"./index-DrFu-skq.js";import"./utils-cDewhVsU.js";import"./lit-element-JplMEnZc.js";import"./lit-html-paDGiEfB.js";import"./chunk-D5ZWXAHU-CGElDDNX.js";import"./v4-CQkTLCs1.js";import"./feature-detection-ONR9WHvu.js";import"./keyboard-shortcut-7Y52yWVL.js";import"./constants-BjnHqKgS.js";import"./base-adapter-CQdYccXX.js";import"./button-vPgaRyW5.js";import"./icon-B5S0VGIT.js";import"./index-BmocOEUj.js";import"./focus-indicator-R2otSvsR.js";import"./state-layer-B7GOb8iB.js";import"./base-button-adapter-OmzAW3c3.js";import"./index-ByifSpfC.js";import"./with-label-aware-DAaZnhel.js";import"./with-default-aria-BcIvJ7-x.js";import"./a11y-utils-BOPvdiVn.js";function r(o){const t={a:"a",blockquote:"blockquote",code:"code",h2:"h2",h3:"h3",li:"li",p:"p",pre:"pre",strong:"strong",ul:"ul",...n(),...o.components};return e.jsxs(e.Fragment,{children:[e.jsx(i,{of:l}),`
+`,e.jsx(s,{}),`
+`,e.jsxs(t.p,{children:[`The keyboard shortcut component provides a simple way to attach keyboard driven functionality to an
+element via the DOM. By default placing `,e.jsx(t.code,{children:"<forge-keyboard-shortcut>"}),` as a sibling after an element
+enables the shortcut when focus is within the element.`]}),`
+`,e.jsxs(t.p,{children:["The shortcut can also be enabled on the entire document body with the ",e.jsx(t.code,{children:"global"}),` attribute or other
+elements with the `,e.jsx(t.code,{children:"target"})," attribute which accepts a CSS selector."]}),`
+`,e.jsx(c,{of:h}),`
+`,e.jsx(t.h3,{id:"targeting-a-different-host-element",children:"Targeting a different host element"}),`
+`,e.jsxs(t.p,{children:["Keyboard shortcut can also target a specific element using the ",e.jsx(t.code,{children:"target"})," attribute:"]}),`
+`,e.jsx(t.pre,{children:e.jsx(t.code,{className:"language-html",children:`<forge-button type="raised" id="shortcut-target">
+  <button type="button">Button</button>
+</forge-button>
+<p>Some other element</p>
+<forge-keyboard-shortcut id="shortcut" key="shift+a" target="#shortcut-target"></forge-keyboard-shortcut>
+`})}),`
+`,e.jsxs(t.blockquote,{children:[`
+`,e.jsxs(t.p,{children:[e.jsx(t.strong,{children:"Note:"})," the ",e.jsx(t.code,{children:"target"})," attribute must be a valid CSS selector."]}),`
+`]}),`
+`,e.jsx(t.h3,{id:"targeting-the-document-body",children:"Targeting the document body"}),`
+`,e.jsxs(t.p,{children:["Additionally, keyboard shortcut can be set to work across the whole page using the ",e.jsx(t.code,{children:"global"})," attribute:"]}),`
+`,e.jsx(t.pre,{children:e.jsx(t.code,{className:"language-html",children:`<forge-keyboard-shortcut id="shortcut" key="shift+a" global></forge-keyboard-shortcut>
+`})}),`
+`,e.jsx(t.h2,{id:"api",children:"API"}),`
+`,e.jsx(a,{}),`
+`,e.jsx(t.h2,{id:"accessibility",children:"Accessibility"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsx(t.li,{children:"Ensure that the shortcut is described in a logical, visible spot on the page. Include its function and the key combination that activates it."}),`
+`,e.jsx(t.li,{children:"Don't override browser, operating system, or assistive technology shortcuts."}),`
+`,e.jsxs(t.li,{children:["Set ",e.jsx(t.code,{children:"aria-keyshortcuts"}),` on any element that is focused or activated by a global shortcut. See the
+`,e.jsx(t.a,{href:"https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-keyshortcuts",rel:"nofollow",children:"MDN docs"})," for more information."]}),`
+`,e.jsxs(t.li,{children:["Avoid using the ",e.jsx(t.code,{children:"accesskey"})," attribute. See the ",e.jsx(t.a,{href:"https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/accesskey",rel:"nofollow",children:"MDN docs"})," for more information."]}),`
+`,e.jsx(t.li,{children:"Don't duplicate behavior provided by the browser. A form will automatically submit when enter is pressed if it includes a submit button."}),`
+`]}),`
+`,e.jsxs(t.blockquote,{children:[`
+`,e.jsxs(t.p,{children:[`Even though the keyboard shortcut component exists in the DOM, the element itself doesn't affect
+accessibility or layout due to having its `,e.jsx(t.code,{children:"display"})," style property set to ",e.jsx(t.code,{children:"none"}),"."]}),`
+`]})]})}function L(o={}){const{wrapper:t}={...n(),...o.components};return t?e.jsx(t,{...o,children:e.jsx(r,{...o})}):r(o)}export{L as default};
---
+++ docs/pr/tyler-technologies-oss:docs-view-all-fix/assets/Popover-CRbYY5Yp.js
@@ -0,0 +1,30 @@
+import{j as e,M as s,T as a,C as i}from"./index-Ggnb1H2z.js";import{useMDXComponents as r}from"./index-CxOWqXOh.js";import{C as p}from"./CustomArgTypes-BiEDufQE.js";import{P as l,D as d,N as h}from"./Popover.stories-CRtdbMlL.js";import"./iframe-CohUvtCd.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-D-8MO0q_.js";import"./index-BHYIh-Xd.js";import"./index-DrFu-skq.js";import"./utils-cDewhVsU.js";import"./lit-element-JplMEnZc.js";import"./lit-html-paDGiEfB.js";import"./chunk-D5ZWXAHU-CGElDDNX.js";import"./v4-CQkTLCs1.js";import"./style-map-C9nPWcxA.js";import"./directive-CF8sV3Lr.js";import"./ref-DJjbfkOF.js";import"./directive-helpers-DcB0QpIl.js";import"./feature-detection-ONR9WHvu.js";import"./toast-C7zU_3Xa.js";import"./constants-BjnHqKgS.js";import"./base-adapter-CQdYccXX.js";import"./index-ByifSpfC.js";import"./button-vPgaRyW5.js";import"./icon-B5S0VGIT.js";import"./index-BmocOEUj.js";import"./focus-indicator-R2otSvsR.js";import"./state-layer-B7GOb8iB.js";import"./base-button-adapter-OmzAW3c3.js";import"./with-label-aware-DAaZnhel.js";import"./with-default-aria-BcIvJ7-x.js";import"./a11y-utils-BOPvdiVn.js";import"./dialog-B9LsXUuI.js";import"./backdrop-BIqxiwQa.js";import"./dismissible-stack-9mJiid_W.js";import"./icon-button-DfODsaKD.js";import"./overlay-DAcircNE.js";import"./popover-Dq2OmXS_.js";import"./with-longpress-listener-S3ft74cg.js";import"./scaffold-BAruaYLU.js";import"./toolbar-Lhya2ayG.js";import"./text-field-DHXYud3A.js";import"./base-field-BSRSGQL0.js";import"./label-BOjkkgiS.js";import"./button-toggle-group-Cft-ohT1.js";import"./with-form-associated-Dm8KqB07.js";import"./checkbox-e65WGDEs.js";import"./switch-BRBW-vzp.js";import"./tooltip-D0ywHBAv.js";function n(o){const t={code:"code",h2:"h2",li:"li",p:"p",strong:"strong",ul:"ul",...r(),...o.components};return e.jsxs(e.Fragment,{children:[e.jsx(s,{of:l}),`
+`,e.jsx(a,{}),`
+`,e.jsx(t.p,{children:`Popovers are used to display content on top of other content. They are used to show additional information related to the content that is currently
+displayed on the screen, and is typically anchored to a specific element or area on the screen that triggered it to open.`}),`
+`,e.jsx(i,{of:d}),`
+`,e.jsx(t.h2,{id:"semantics",children:"Semantics"}),`
+`,e.jsx(t.p,{children:`Popovers do not have any semantic meaning by default. This means that it's up to the developer to ensure that the content inside the popover is
+accessible if it needs to be. This can be done by adding the appropriate ARIA attributes to the popover itself, or the content within it.`}),`
+`,e.jsx(t.h2,{id:"popovers-vs-dialogs",children:"Popovers vs Dialogs"}),`
+`,e.jsx(t.p,{children:`There is a lot of overlap between popovers and dialogs, and it can be difficult to know when to use one over the other. Here are some guidelines to help
+you decide:`}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsxs(t.li,{children:[e.jsx(t.strong,{children:"Popovers"}),` are used to display additional information related to the content that is currently displayed on the screen. They are typically anchored to
+a specific element or area on the screen. Popovers are transient and non-modal, meaning that they do not block the user from interacting with the rest
+of the page.`]}),`
+`,e.jsxs(t.li,{children:[e.jsx(t.strong,{children:"Dialogs"}),` are used to display content that requires the user's immediate attention. They are typically modal (but can be non-modal), meaning that they
+block the user from interacting with the rest of the page until they are dismissed. Dialogs are typically used for things like confirmation messages,
+alerts, and forms.`]}),`
+`]}),`
+`,e.jsx(t.p,{children:`There are some cases where you may need to blur these lines a bit (and you can use either component), especially if the design of your application calls
+for it. Just be sure to consider the user experience and accessibility implications.`}),`
+`,e.jsx(t.h2,{id:"non-modal-popoverdialog",children:"Non-modal Popover/Dialog"}),`
+`,e.jsxs(t.p,{children:[`If you need to create a popover that behaves like a dialog (i.e. it is non-modal and does not block the user from interacting with the rest of the page),
+you can use the `,e.jsx(t.code,{children:"<forge-popover>"})," and add the proper ARIA attributes to make it behave like a dialog."]}),`
+`,e.jsx(i,{of:h}),`
+`,e.jsxs(t.p,{children:["In this example, the popover is presented with the ",e.jsx(t.code,{children:'role="dialog"'})," and ",e.jsx(t.code,{children:'aria-modal="false"'}),` attributes. This tells screen readers that the popover is a
+dialog, but it is not modal. This specific example will gracefully handle user entry into a form, by ensuring that the user does not lose valuable data
+by accidentally closing the popover.`]}),`
+`,e.jsx(t.h2,{id:"api",children:"API"}),`
+`,e.jsx(p,{})]})}function ae(o={}){const{wrapper:t}={...r(),...o.components};return t?e.jsx(t,{...o,children:e.jsx(n,{...o})}):n(o)}export{ae as default};
