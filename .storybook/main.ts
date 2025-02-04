0220eeaf50a71bd37057c527944909863c04da36
Kieran Nichols <kieran.nichols@tylertech.com>
2025-02-04 13:51:20 -0500

    docs: fix google analytics tracking for preview pages (#806)

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -26,6 +26,7 @@ const config: StorybookConfig = {
       },
     },
     'storybook-addon-tag-badges',
+    './addons/google-analytics/register.js',
   ],
   staticDirs: ['../src/stories/assets'],
   framework: {
