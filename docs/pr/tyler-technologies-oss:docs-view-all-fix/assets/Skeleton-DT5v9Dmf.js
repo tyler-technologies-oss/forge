460aca6dee3d08758e3377608b52ec22eff2ef0f
DRiFTy17 <DRiFTy17@users.noreply.github.com>
2025-02-04 14:45:22 +0000

    Deploying to gh-pages from @ tyler-technologies-oss/forge@533871e0bc96eff5534e5dc1b9142d7c8e252cb7 🚀

---
+++ docs/pr/tyler-technologies-oss:docs-view-all-fix/assets/Skeleton-DT5v9Dmf.js
@@ -0,0 +1,24 @@
+import{j as e,M as r,T as a,C as s}from"./index-BRW3KzJT.js";import{useMDXComponents as i}from"./index-DJOjw4IC.js";import{C as l}from"./CustomArgTypes-93vlBbNO.js";import{C as c}from"./CssOnlyInformation-DfZu_yGv.js";import{S as d,D as h,P as m,L as p,C as f,B as u,F as x,a as j}from"./Skeleton.stories-BzNQVK7Q.js";import"./iframe-DCidbgMh.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-D-8MO0q_.js";import"./index-BHYIh-Xd.js";import"./index-DrFu-skq.js";import"./utils-cDewhVsU.js";import"./lit-element-JplMEnZc.js";import"./lit-html-paDGiEfB.js";import"./class-map-D55lQyt8.js";import"./directive-CF8sV3Lr.js";import"./style-map-C9nPWcxA.js";import"./feature-detection-ONR9WHvu.js";import"./skeleton-Dfdgg-pt.js";import"./constants-BjnHqKgS.js";function o(t){const n={code:"code",h2:"h2",p:"p",...i(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(r,{of:d}),`
+`,e.jsx(a,{}),`
+`,e.jsx(n.p,{children:'The skeleton component is a generic, low-level component that can be used to build loading placeholder pages/elements called "skeleton screens". These screens represent the block-level visual elements of a page, or section of a page, where content is intended to be loaded sometime in the future. Using skeleton screens gives the illusion of speed to the user, and can often reduce frustration for users during long running processes.'}),`
+`,e.jsx(n.p,{children:"There are a few built-in variants that can be used by adding an attribute to the element. These built-in variants are specifically designed to mimic other components in the Forge library for ease of use."}),`
+`,e.jsx(n.h2,{id:"default",children:"Default"}),`
+`,e.jsx(s,{of:h}),`
+`,e.jsx(n.h2,{id:"profile",children:"Profile"}),`
+`,e.jsx(s,{of:m}),`
+`,e.jsx(n.h2,{id:"list",children:"list"}),`
+`,e.jsx(s,{of:p}),`
+`,e.jsx(n.h2,{id:"chips",children:"Chips"}),`
+`,e.jsx(s,{of:f}),`
+`,e.jsx(n.h2,{id:"buttons",children:"Buttons"}),`
+`,e.jsx(s,{of:u}),`
+`,e.jsx(n.h2,{id:"form-field",children:"Form Field"}),`
+`,e.jsx(s,{of:x}),`
+`,e.jsx(n.h2,{id:"api",children:"API"}),`
+`,e.jsx(l,{}),`
+`,e.jsx(n.h2,{id:"accessibility",children:"Accessibility"}),`
+`,e.jsxs(n.p,{children:["Ensure that the skeleton component does not interfere with screen readers by including the ",e.jsx(n.code,{children:'aria-hidden="true"'})," attribute."]}),`
+`,e.jsx(n.h2,{id:"css-only",children:"CSS-Only"}),`
+`,e.jsx(n.p,{children:"The skeleton component is also available as a CSS-only component without the need for JavaScript."}),`
+`,e.jsx(s,{of:j}),`
+`,e.jsx(c,{})]})}function I(t={}){const{wrapper:n}={...i(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(o,{...t})}):o(t)}export{I as default};
---
+++ docs/pr/tyler-technologies-oss:docs-view-all-fix/assets/search-Dc9dluJN.js
@@ -0,0 +1,20 @@
+import{j as t,M as n,T as s,C as p}from"./index-BRW3KzJT.js";import{useMDXComponents as o}from"./index-DJOjw4IC.js";import{C as a}from"./CustomArgTypes-93vlBbNO.js";import{S as m,D as c}from"./search.stories-r-iusbXo.js";import"./iframe-DCidbgMh.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-D-8MO0q_.js";import"./index-BHYIh-Xd.js";import"./index-DrFu-skq.js";import"./utils-cDewhVsU.js";import"./lit-element-JplMEnZc.js";import"./lit-html-paDGiEfB.js";import"./feature-detection-ONR9WHvu.js";import"./app-bar-profile-button-gPuhr404.js";import"./constants-BjnHqKgS.js";import"./base-adapter-CQdYccXX.js";import"./focus-indicator-R2otSvsR.js";import"./index-BmocOEUj.js";import"./state-layer-B7GOb8iB.js";import"./index-ByifSpfC.js";import"./icon-B5S0VGIT.js";import"./icon-button-DfODsaKD.js";import"./base-button-adapter-OmzAW3c3.js";import"./with-label-aware-DAaZnhel.js";import"./with-default-aria-BcIvJ7-x.js";import"./a11y-utils-BOPvdiVn.js";import"./tooltip-D0ywHBAv.js";import"./overlay-DAcircNE.js";import"./with-longpress-listener-S3ft74cg.js";import"./dismissible-stack-9mJiid_W.js";import"./badge-CzgFSHGZ.js";import"./menu-BsKFCp0U.js";import"./list-DndFp_bX.js";import"./list-dropdown-aware-core-CDYm9sj0.js";import"./list-dropdown-CXmPPylA.js";import"./event-utils-C1SDeUaq.js";import"./linear-progress-DPUjJFYN.js";import"./popover-Dq2OmXS_.js";import"./skeleton-Dfdgg-pt.js";import"./a11y-BxM9_46k.js";import"./base-component-delegate-D9tVS5jC.js";import"./avatar-Du1LPt_G.js";import"./button-vPgaRyW5.js";import"./toolbar-Lhya2ayG.js";function r(i){const e={code:"code",h2:"h2",li:"li",p:"p",pre:"pre",ul:"ul",...o(),...i.components};return t.jsxs(t.Fragment,{children:[t.jsx(n,{of:m}),`
+`,t.jsx(s,{}),`
+`,t.jsxs(e.p,{children:["The app-bar search component is essentially just a strictly-styled ",t.jsx(e.code,{children:"<input>"})," element that fits in with the app-bar ecosystem. It provides some additional functionality through its public API, but at the very least it gives a common component to use for a familiar search experience for users."]}),`
+`,t.jsx(e.h2,{id:"default",children:"Default"}),`
+`,t.jsx(p,{of:c}),`
+`,t.jsx(e.h2,{id:"api",children:"API"}),`
+`,t.jsx(a,{}),`
+`,t.jsx(e.h2,{id:"accessibility",children:"Accessibility"}),`
+`,t.jsxs(e.ul,{children:[`
+`,t.jsxs(e.li,{children:["Ensure that you provide a placeholder attribute for the ",t.jsx(e.code,{children:"<input>"})," element to help give users an idea of what to enter."]}),`
+`,t.jsxs(e.li,{children:["Ensure that you add an aria-label or aria-labelledby attribute to the ",t.jsx(e.code,{children:"<input>"}),"."]}),`
+`,t.jsx(e.li,{children:"If using a custom theme, make sure proper color contrast ratios are met and adjust using CSS custom properties if not."}),`
+`]}),`
+`,t.jsx(e.h2,{id:"types",children:"Types"}),`
+`,t.jsx(e.pre,{children:t.jsx(e.code,{className:"language-typescript",children:`interface IAppBarSearchEventData {
+  value: string;
+  combined: boolean;
+  context: string;
+}
+`})})]})}function et(i={}){const{wrapper:e}={...o(),...i.components};return e?t.jsx(e,{...i,children:t.jsx(r,{...i})}):r(i)}export{et as default};
