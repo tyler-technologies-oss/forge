0a9d1f261e69711e16f4f893bf12a0d50b8eb696
DRiFTy17 <DRiFTy17@users.noreply.github.com>
2025-02-27 14:39:31 +0000

    Deploying to gh-pages from @ tyler-technologies-oss/forge@8bc3cb2a04c5c67295a80eb0f02eee894666400f 🚀

---
+++ docs/main/assets/StateLayer-CJq8rc0N.js
@@ -0,0 +1,23 @@
+import{j as e,M as a,T as r,C as o}from"./index-D8Y1Xo_F.js";import{useMDXComponents as s}from"./index-tWRZnc61.js";import{C as c}from"./CustomArgTypes-BwwbQ9aG.js";import{C as h}from"./CssOnlyInformation-DjTU0kbM.js";import{S as d,D as l,W as p,C as m}from"./StateLayer.stories-CL6C5c25.js";import"./iframe-C3TB-rMS.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-BHYIh-Xd.js";import"./index-DrFu-skq.js";import"./utils-BbKlM6X4.js";import"./lit-html-paDGiEfB.js";import"./style-map-C9nPWcxA.js";import"./directive-CF8sV3Lr.js";import"./feature-detection-DRCh51Sa.js";import"./state-layer-IxmMcKDT.js";import"./constants-9n5_0r7k.js";import"./base-adapter-B6TJxM93.js";import"./focus-indicator-DzT8BbE-.js";import"./index-BmocOEUj.js";import"./card-C10NOeAn.js";function i(n){const t={blockquote:"blockquote",code:"code",h2:"h2",p:"p",...s(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(a,{of:d}),`
+`,e.jsx(r,{}),`
+`,e.jsx(t.p,{children:`State layers are used to indicate interaction states for an element, such as hover and pressed. These states provide visual feedback to the user when
+they interact with an element, and are especially useful for interactive elements like buttons. They are typically used in conjunction with focus
+indicators to provide a complete set of visual feedback for interactive elements. These are building block components that are typically used internally
+within other components such as buttons.`}),`
+`,e.jsx(t.p,{children:`Additionally, state layers provide a "ripple" effect that animates from the point of interaction to the bounds of the element. This effect is used to
+indicate that the user's interaction has been received and is being processed.`}),`
+`,e.jsxs(t.p,{children:["When using a state layer, it's important to note that they are absolutely positioned and should be used with a parent element that has ",e.jsx(t.code,{children:"position: relative"}),`
+set to ensure that the bounds of interaction state are contained within the parent element.`]}),`
+`,e.jsx(o,{of:l}),`
+`,e.jsx(t.h2,{id:"with-cards",children:"With Cards"}),`
+`,e.jsx(t.p,{children:"A common design pattern is an interactive card with a state layer and focus indicator that describes to the user that the card is interactive."}),`
+`,e.jsx(o,{of:p}),`
+`,e.jsxs(t.blockquote,{children:[`
+`,e.jsx(t.p,{children:"Remember to always provide a focus indicator for interactive elements to ensure that users can navigate your site using a keyboard."}),`
+`]}),`
+`,e.jsx(t.h2,{id:"api",children:"API"}),`
+`,e.jsx(c,{}),`
+`,e.jsx(t.h2,{id:"css-only",children:"CSS-Only"}),`
+`,e.jsx(t.p,{children:"The state-layer component is also available as a CSS-only component without the need for JavaScript."}),`
+`,e.jsx(o,{of:m}),`
+`,e.jsx(h,{})]})}function I(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(i,{...n})}):i(n)}export{I as default};
