e6b9bd037b3110bb8735973850b8dd9a8af274dd
samrichardsontylertech <samrichardsontylertech@users.noreply.github.com>
2025-03-19 20:20:53 +0000

    Deploying to gh-pages from @ tyler-technologies-oss/forge@eff5b7b80286587fe38a489fd325bc08d9a633af 🚀

---
+++ docs/pr/tyler-technologies-oss:lit-tree/assets/ChipField-BHOoxy62.js
@@ -0,0 +1,24 @@
+import{j as e,M as s,T as a,C as o}from"./index-C3smSjdf.js";import{useMDXComponents as n}from"./index-BwOk33sS.js";import{C as l}from"./CustomArgTypes-oZjAMOAn.js";import{C as p,D as d,W as c}from"./ChipField.stories-BAm-9uVp.js";import"./iframe-wsTFzD1_.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-BHYIh-Xd.js";import"./index-DrFu-skq.js";import"./utils-DlykE8tv.js";import"./lit-element-JplMEnZc.js";import"./lit-html-paDGiEfB.js";import"./style-map-C9nPWcxA.js";import"./directive-CF8sV3Lr.js";import"./index-Cf3axooF.js";import"./v4-CQkTLCs1.js";import"./ref-DJjbfkOF.js";import"./directive-helpers-DcB0QpIl.js";import"./feature-detection-DRCh51Sa.js";import"./autocomplete-CGfUhCJ6.js";import"./constants-9n5_0r7k.js";import"./base-adapter-B6TJxM93.js";import"./index-CbZAylpk.js";import"./divider-75u-P60s.js";import"./icon-B5R9pr_c.js";import"./index-BgGCUUFB.js";import"./linear-progress-CqfIuBkR.js";import"./with-default-aria-B0dk5gj8.js";import"./a11y-utils-DJ_tX8xT.js";import"./list-DDpi_hz2.js";import"./state-layer-DCupnvce.js";import"./utils-CYJ0zQHl.js";import"./focus-indicator-CFAnWQXZ.js";import"./list-dropdown-aware-core-jpNXvvuc.js";import"./list-dropdown-1yKbx-dp.js";import"./event-utils-C1SDeUaq.js";import"./popover-pwa-lLuJ.js";import"./overlay-DyVP2d0e.js";import"./with-longpress-listener-BdUe1dXe.js";import"./dismissible-stack-kdhTFvbL.js";import"./skeleton-D2S3-1Sc.js";import"./a11y-BxM9_46k.js";import"./text-field-AvpciTks.js";import"./base-field-CiyMRbdM.js";import"./label-DRSqqPa2.js";import"./button-BRfTMEeh.js";import"./base-button-adapter-BFQz2IKA.js";import"./with-label-aware-OEbK3wHg.js";import"./button-toggle-group-Dvk8YwvA.js";import"./with-form-associated-Bje5Hee1.js";import"./checkbox-qSS9ySuf.js";import"./icon-button-nddYOp-w.js";import"./switch-BhcBCb-7.js";import"./tooltip-BhK_7K0r.js";import"./chip-set-DUTG-iwG.js";function r(t){const i={code:"code",h2:"h2",li:"li",p:"p",ul:"ul",...n(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(s,{of:p}),`
+`,e.jsx(a,{}),`
+`,e.jsx(i.p,{children:"Chip fields are a specialized variant of text field that allows users to input multiple values in a single field and represent them as chips."}),`
+`,e.jsx(o,{of:d}),`
+`,e.jsx(i.h2,{id:"deprecation-notice",children:"Deprecation Notice"}),`
+`,e.jsxs(i.p,{children:["The ",e.jsx(i.code,{children:"<forge-chip-field>"})," component is deprecated and will be removed in a future release. Existing components such as ",e.jsx(i.code,{children:"<forge-text-field>"}),` and
+`,e.jsx(i.code,{children:"<forge-select>"})," will be able to be used to create similar functionality which removes the current need for this specialized component."]}),`
+`,e.jsx(i.h2,{id:"with-autocomplete",children:"With Autocomplete"}),`
+`,e.jsx(i.p,{children:"It is common to use an autocomplete component with a chip field to provide suggestions to the user as they type."}),`
+`,e.jsx(o,{of:c}),`
+`,e.jsx(i.h2,{id:"api",children:"API"}),`
+`,e.jsx(l,{}),`
+`,e.jsx(i.h2,{id:"accessibility",children:"Accessibility"}),`
+`,e.jsxs(i.ul,{children:[`
+`,e.jsxs(i.li,{children:["Add an ",e.jsx(i.code,{children:"id"})," to your ",e.jsx(i.code,{children:"<input>"})," element and bind it to your ",e.jsx(i.code,{children:"<label>"})," element using the for attribute on the ",e.jsx(i.code,{children:"<label>"}),"."]}),`
+`,e.jsxs(i.li,{children:["Ensure that the chips that are added to the field have descriptive ",e.jsx(i.code,{children:"aria-label"})," or ",e.jsx(i.code,{children:"aria-labelledby"})," attributes."]}),`
+`,e.jsxs(i.li,{children:["Make sure that the member chips that are added can be accessed via the keyboard left and right arrows.",`
+`,e.jsxs(i.ul,{children:[`
+`,e.jsx(i.li,{children:"There should be a distinct visual cue that indicates which chip is focused"}),`
+`]}),`
+`]}),`
+`,e.jsx(i.li,{children:"Similarly, make sure that when a chip is focused using the arrow keys, that the backspace and delete keys do remove them."}),`
+`,e.jsx(i.li,{children:"Ensure that when disabled, the entire field as well as the member chips all appear visually and interactively disabled."}),`
+`]})]})}function me(t={}){const{wrapper:i}={...n(),...t.components};return i?e.jsx(i,{...t,children:e.jsx(r,{...t})}):r(t)}export{me as default};
---
+++ docs/pr/tyler-technologies-oss:lit-tree/assets/Switch-BXdW7j55.js
@@ -0,0 +1,23 @@
+import{j as e,M as s,T as l,C as o}from"./index-C3smSjdf.js";import{useMDXComponents as r}from"./index-BwOk33sS.js";import{C as a}from"./CssOnlyInformation-xXm0vhqR.js";import{C as c}from"./CustomArgTypes-oZjAMOAn.js";import{S as m,D as h,C as p}from"./Switch.stories-D7SGaHpJ.js";import"./iframe-wsTFzD1_.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-BHYIh-Xd.js";import"./index-DrFu-skq.js";import"./utils-DlykE8tv.js";import"./feature-detection-DRCh51Sa.js";import"./icon-B5R9pr_c.js";import"./constants-9n5_0r7k.js";import"./base-adapter-B6TJxM93.js";import"./index-BgGCUUFB.js";import"./index-CbZAylpk.js";import"./lit-element-JplMEnZc.js";import"./lit-html-paDGiEfB.js";import"./class-map-D55lQyt8.js";import"./directive-CF8sV3Lr.js";import"./switch-BhcBCb-7.js";import"./with-form-associated-Bje5Hee1.js";import"./with-default-aria-B0dk5gj8.js";import"./a11y-utils-DJ_tX8xT.js";import"./with-label-aware-OEbK3wHg.js";import"./focus-indicator-CFAnWQXZ.js";import"./utils-CYJ0zQHl.js";import"./state-layer-DCupnvce.js";function n(i){const t={code:"code",h2:"h2",li:"li",p:"p",ul:"ul",...r(),...i.components};return e.jsxs(e.Fragment,{children:[e.jsx(s,{of:m}),`
+`,e.jsx(l,{}),`
+`,e.jsx(t.p,{children:"Switches toggle the state of a single setting on or off."}),`
+`,e.jsx(t.p,{children:"Use switches to:"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsx(t.li,{children:"Toggle a single item on or off, on mobile and tablet"}),`
+`,e.jsx(t.li,{children:"Immediately activate or deactivate something"}),`
+`]}),`
+`,e.jsx(o,{of:h}),`
+`,e.jsx(t.h2,{id:"api",children:"API"}),`
+`,e.jsx(c,{}),`
+`,e.jsx(t.h2,{id:"accessibility",children:"Accessibility"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsx(t.li,{children:"Ensure that all of the controls that are accessible by a mouse are also accessible by keyboard."}),`
+`,e.jsx(t.li,{children:"Ensure the controls are reachable by the tab key."}),`
+`,e.jsx(t.li,{children:"Ensure each control can be updated or activated by the keyboard."}),`
+`,e.jsxs(t.li,{children:["The input element will receive the proper ARIA attribute ",e.jsx(t.code,{children:"aria-checked"}),"."]}),`
+`,e.jsxs(t.li,{children:["The input element will receive the proper role attribute ",e.jsx(t.code,{children:'role="switch"'}),"."]}),`
+`]}),`
+`,e.jsx(t.h2,{id:"css-only",children:"CSS-Only"}),`
+`,e.jsx(t.p,{children:"The switch component is also available as a CSS-only component without the need for JavaScript."}),`
+`,e.jsx(o,{of:p}),`
+`,e.jsx(a,{})]})}function q(i={}){const{wrapper:t}={...r(),...i.components};return t?e.jsx(t,{...i,children:e.jsx(n,{...i})}):n(i)}export{q as default};
