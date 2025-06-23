d09622876c706b4fe425e0f9fff42d05f998a1f2
DRiFTy17 <DRiFTy17@users.noreply.github.com>
2025-06-23 14:34:36 +0000

    Deploying to gh-pages from @ tyler-technologies-oss/forge@494482b83fdbf9294ef5b9036a21aa5dd2e87397 🚀

---
+++ docs/pr/tyler-technologies-oss:sb-link-typography-docs/assets/Button-BHhXXgEP.js
@@ -0,0 +1,53 @@
+import{j as e,M as r,T as a,C as t}from"./blocks-Dpx4gxlh.js";import{useMDXComponents as s}from"./index-D7_J1WOm.js";import{C as c}from"./CustomArgTypes-ClWxoKPv.js";import{C as l}from"./CssOnlyInformation-BKvqRDUU.js";import{B as h,D as d,V as u,T as p,A as x,W as b,a as j,C as m}from"./Button.stories-BnijOFuY.js";import"./iframe-CmPrhNEs.js";import"./_commonjsHelpers-CqkleIqs.js";import"./utils-gw6R9s7u.js";import"./class-map-CguuqOih.js";import"./directive-CJw_OlP2.js";import"./icon-ANstxuR5.js";import"./constants-NErMj_Tj.js";import"./feature-detection-CY6TVbRZ.js";import"./base-adapter-Ch0oiIsw.js";import"./index-CiLSBptl.js";import"./button-DKtxCkrw.js";import"./focus-indicator-Cgfkaa3d.js";import"./utils-CRxrUqQD.js";import"./state-layer-BVsNuAhs.js";import"./base-button-adapter-DBeYtn0B.js";import"./with-label-aware-DkCFYjRm.js";import"./with-default-aria-srFr2cNz.js";import"./a11y-utils-DGb1vALN.js";import"./circular-progress-pTvFqlis.js";function i(o){const n={blockquote:"blockquote",code:"code",h2:"h2",li:"li",p:"p",strong:"strong",ul:"ul",...s(),...o.components};return e.jsxs(e.Fragment,{children:[e.jsx(r,{of:h}),`
+`,e.jsx(a,{}),`
+`,e.jsx(n.p,{children:"Buttons are used to trigger actions or events in an application. Forge provides a set of button styles that can be used to create a consistent user experience."}),`
+`,e.jsx(t,{of:d}),`
+`,e.jsx(n.h2,{id:"variants",children:"Variants"}),`
+`,e.jsx(n.p,{children:"Forge buttons come in several variants to match the style of your application, and can be used in a variety of contexts based on emphasis and hierarchy."}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[e.jsx(n.strong,{children:"Text:"})," A button with no background or border, and only text content."]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.strong,{children:"Outlined:"})," A button with a transparent background and a border."]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.strong,{children:"Filled:"})," A button with a solid background color."]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.strong,{children:"Raised:"})," A button with a raised appearance."]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.strong,{children:"Link:"})," A button with no background or border, and only text content that appears as a link."]}),`
+`]}),`
+`,e.jsx(t,{of:u}),`
+`,e.jsx(n.h2,{id:"themed",children:"Themed"}),`
+`,e.jsxs(n.p,{children:["Forge buttons can be themed using the built-in ",e.jsx(n.code,{children:"theme"})," attribute, which will apply the appropriate color scheme based on the current theme."]}),`
+`,e.jsx(t,{of:p}),`
+`,e.jsxs(n.blockquote,{children:[`
+`,e.jsx(n.p,{children:"Additionally, you can always provide your own theme via the CSS custom properties noted below."}),`
+`]}),`
+`,e.jsx(n.h2,{id:"anchor",children:"Anchor"}),`
+`,e.jsxs(n.p,{children:["Forge buttons support providing a slotted ",e.jsx(n.code,{children:"<a>"})," element that allows for you to visually style an anchor tag as a button."]}),`
+`,e.jsx(t,{of:x}),`
+`,e.jsxs(n.blockquote,{children:[`
+`,e.jsxs(n.p,{children:[e.jsx(n.strong,{children:"Note:"})," you should not use slots with anchor buttons because the ",e.jsx(n.code,{children:"<a>"})," element itself becomes the full content of the button."]}),`
+`]}),`
+`,e.jsx(n.h2,{id:"with-icon",children:"With Icon"}),`
+`,e.jsx(n.p,{children:"Forge buttons can include an icon to provide additional context or visual interest."}),`
+`,e.jsx(t,{of:b}),`
+`,e.jsx(n.h2,{id:"with-circular-progress",children:"With Circular Progress"}),`
+`,e.jsx(n.p,{children:"Forge buttons can include a circular progress indicator to provide feedback to the user when an action is in progress."}),`
+`,e.jsx(t,{of:j}),`
+`,e.jsx(n.h2,{id:"api",children:"API"}),`
+`,e.jsx(c,{}),`
+`,e.jsx(n.h2,{id:"accessibility",children:"Accessibility"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:["Verify that you can reach every button by keyboard navigation.",`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Ensure that there is a visual cue that the button is currently in focus."}),`
+`]}),`
+`]}),`
+`,e.jsxs(n.li,{children:["Verify that the button has a visible label that describes the action that will be taken when the button is clicked.",`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:["Alternatively, use an ",e.jsx(n.code,{children:"aria-label"})," attribute to provide a descriptive label for the button."]}),`
+`]}),`
+`]}),`
+`,e.jsx(n.li,{children:"Verify that pressing the space bar or enter key while focused on a button will activate the button in the same manner as if it had been clicked with a mouse."}),`
+`,e.jsx(n.li,{children:"Verify that there is sufficient contrast between the foreground text and background to meet WCAG requirements."}),`
+`]}),`
+`,e.jsx(n.h2,{id:"css-only",children:"CSS-Only"}),`
+`,e.jsx(n.p,{children:"The button component is also available as a CSS-only component without the need for JavaScript."}),`
+`,e.jsx(t,{of:m}),`
+`,e.jsx(l,{})]})}function _(o={}){const{wrapper:n}={...s(),...o.components};return n?e.jsx(n,{...o,children:e.jsx(i,{...o})}):i(o)}export{_ as default};
---
+++ docs/pr/tyler-technologies-oss:sb-link-typography-docs/assets/Stepper-V0JOHxxW.js
@@ -0,0 +1,19 @@
+import{j as e,M as s,T as a,C as n}from"./blocks-Dpx4gxlh.js";import{useMDXComponents as i}from"./index-D7_J1WOm.js";import{C as l}from"./CustomArgTypes-ClWxoKPv.js";import{S as p,D as c,V as m}from"./Stepper.stories-yFZB9IBQ.js";import"./iframe-CmPrhNEs.js";import"./_commonjsHelpers-CqkleIqs.js";import"./utils-gw6R9s7u.js";import"./style-map-CJX1Xp8j.js";import"./directive-CJw_OlP2.js";import"./feature-detection-CY6TVbRZ.js";import"./stepper-BX-XnE7A.js";import"./constants-NErMj_Tj.js";import"./base-adapter-Ch0oiIsw.js";import"./icon-ANstxuR5.js";import"./index-CiLSBptl.js";import"./state-layer-BVsNuAhs.js";import"./utils-CRxrUqQD.js";import"./expansion-panel-D_Sh7JhR.js";import"./focus-indicator-Cgfkaa3d.js";import"./event-utils-C1SDeUaq.js";function o(r){const t={h2:"h2",li:"li",p:"p",ul:"ul",...i(),...r.components};return e.jsxs(e.Fragment,{children:[e.jsx(s,{of:p}),`
+`,e.jsx(a,{}),`
+`,e.jsx(t.p,{children:"Steppers display progress through a sequence by breaking it up into multiple logical and numbered steps. They may also be used for navigation."}),`
+`,e.jsx(n,{of:c}),`
+`,e.jsx(t.h2,{id:"linear-vs-non-linear",children:"Linear vs Non-linear"}),`
+`,e.jsx(t.p,{children:`Steppers are either linear or non-linear. This means that for linear steps the user needs to complete one step in order to move on to the next one. Non-linear steps
+allow for a more flexible way of navigation by allowing the user to jump to any step at any time.`}),`
+`,e.jsx(t.h2,{id:"vertical",children:"Vertical"}),`
+`,e.jsx(t.p,{children:"Vertical steppers are designed for narrow layouts. They are ideal for mobile devices or drawers adjacent to the content they relate to."}),`
+`,e.jsx(n,{of:m}),`
+`,e.jsx(t.h2,{id:"api",children:"API"}),`
+`,e.jsx(l,{}),`
+`,e.jsx(t.h2,{id:"accessibility",children:"Accessibility"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsx(t.li,{children:"Ensure that all of the controls that are accessible by a mouse are also accessible by keyboard."}),`
+`,e.jsx(t.li,{children:"Ensure the controls are reachable by the tab key."}),`
+`,e.jsx(t.li,{children:"Ensure each control can be updated or activated by the keyboard."}),`
+`,e.jsx(t.li,{children:"The step element will receive the proper ARIA attribute aria-selected as necessary."}),`
+`]})]})}function X(r={}){const{wrapper:t}={...i(),...r.components};return t?e.jsx(t,{...r,children:e.jsx(o,{...r})}):o(r)}export{X as default};
