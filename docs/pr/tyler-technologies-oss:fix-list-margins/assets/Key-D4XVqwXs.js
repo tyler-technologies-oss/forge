a021c40e191f570c248c0569dd3217accb22f313
DRiFTy17 <DRiFTy17@users.noreply.github.com>
2025-07-07 14:52:12 +0000

    Deploying to gh-pages from @ tyler-technologies-oss/forge@884ade38a7f35900c2f6e7248d619a6e43d0efe6 🚀

---
+++ docs/pr/tyler-technologies-oss:fix-list-margins/assets/Key-D4XVqwXs.js
@@ -0,0 +1,10 @@
+import{j as t,M as n,T as a,C as r}from"./blocks-DBtwW2si.js";import{useMDXComponents as s}from"./index-BH3Squ8u.js";import{C as m}from"./CustomArgTypes-BPGi-6-9.js";import{K as c,D as l}from"./Key.stories-CU7WxpYS.js";import"./iframe-bgwZ8wT-.js";import"./_commonjsHelpers-CqkleIqs.js";import"./utils-CFmF9mkM.js";import"./feature-detection-uS6p5jc8.js";import"./key-item-CXr4Xd3w.js";import"./base-lit-element-Bhu_w1nI.js";import"./a11y-utils-fzPGYZJ6.js";import"./state-BZb3AzG7.js";import"./class-map-CqfuZyvQ.js";import"./directive-CJw_OlP2.js";function o(i){const e={h2:"h2",p:"p",...s(),...i.components};return t.jsxs(t.Fragment,{children:[t.jsx(n,{of:c}),`
+`,t.jsx(a,{}),`
+`,t.jsx(e.p,{children:"Keys are used to label data in a chart or visualization."}),`
+`,t.jsx(r,{of:l}),`
+`,t.jsx(e.h2,{id:"api",children:"API"}),`
+`,t.jsx(m,{}),`
+`,t.jsx(e.h2,{id:"accessibility",children:"Accessibility"}),`
+`,t.jsx(e.p,{children:`Ensure that the icon colors of the key items are easily distinguishable and match the color of the
+associated visualization.`}),`
+`,t.jsx(e.p,{children:"The value slot may be used to create an accessible text alternative for data in the visualization."})]})}function g(i={}){const{wrapper:e}={...s(),...i.components};return e?t.jsx(e,{...i,children:t.jsx(o,{...i})}):o(i)}export{g as default};
---
+++ docs/pr/tyler-technologies-oss:fix-list-margins/assets/state-BZb3AzG7.js
@@ -0,0 +1,5 @@
+import{n as r}from"./base-lit-element-Bhu_w1nI.js";/**
+ * @license
+ * Copyright 2017 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */function n(t){return r({...t,state:!0,attribute:!1})}export{n as r};
