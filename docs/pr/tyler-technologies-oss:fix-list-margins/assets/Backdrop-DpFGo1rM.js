2bb691ff05e0505d602780a1ad5fdfb914b60751
DRiFTy17 <DRiFTy17@users.noreply.github.com>
2025-07-07 14:52:19 +0000

    Deploying to gh-pages from @ tyler-technologies-oss/forge@884ade38a7f35900c2f6e7248d619a6e43d0efe6 🚀

--- docs/pr/tyler-technologies-oss:fix-list-margins/assets/Backdrop-DpFGo1rM.js
+++
@@ -1,8 +0,0 @@
-import{j as o,M as a,T as s,C as i}from"./blocks-DBtwW2si.js";import{useMDXComponents as r}from"./index-BH3Squ8u.js";import{C as p}from"./CustomArgTypes-BPGi-6-9.js";import{B as m,D as c}from"./Backdrop.stories-LpRToxr7.js";import"./iframe-bgwZ8wT-.js";import"./_commonjsHelpers-CqkleIqs.js";import"./utils-CFmF9mkM.js";import"./style-map-VStFbSZG.js";import"./directive-CJw_OlP2.js";import"./feature-detection-uS6p5jc8.js";import"./backdrop-BZvWLwDX.js";import"./constants-wOq9K3uV.js";function e(t){const n={h2:"h2",p:"p",...r(),...t.components};return o.jsxs(o.Fragment,{children:[o.jsx(a,{of:m}),`
-`,o.jsx(s,{}),`
-`,o.jsx(n.p,{children:`Backdrops are building block components that are used to provide a visual overlay for a component or page. They are typically used to provide
-separation between some floating content and its background.`}),`
-`,o.jsx(n.p,{children:"The dialog component is a good example of a component that uses a backdrop."}),`
-`,o.jsx(i,{of:c}),`
-`,o.jsx(n.h2,{id:"api",children:"API"}),`
-`,o.jsx(p,{})]})}function k(t={}){const{wrapper:n}={...r(),...t.components};return n?o.jsx(n,{...t,children:o.jsx(e,{...t})}):e(t)}export{k as default};
