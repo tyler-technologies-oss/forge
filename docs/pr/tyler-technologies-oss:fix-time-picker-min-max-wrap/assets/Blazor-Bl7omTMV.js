d68f43ce92ab453a172da9dd537c0ae2db94e596
DRiFTy17 <DRiFTy17@users.noreply.github.com>
2025-07-22 14:19:52 +0000

    Deploying to gh-pages from @ tyler-technologies-oss/forge@9b221e4e963943ced7771e1a6ae7e28fb7003a70 🚀

--- docs/pr/tyler-technologies-oss:fix-time-picker-min-max-wrap/assets/Blazor-Bl7omTMV.js
+++
@@ -1,3 +0,0 @@
-import{j as t,M as a}from"./blocks-DFxSlAdD.js";import{useMDXComponents as o}from"./index-CuhpVLEM.js";import"./iframe-Cnwcw40-.js";import"./_commonjsHelpers-CqkleIqs.js";function r(n){const e={h1:"h1",p:"p",...o(),...n.components};return t.jsxs(t.Fragment,{children:[t.jsx(a,{title:"Frameworks/Blazor"}),`
-`,t.jsx(e.h1,{id:"blazor",children:"Blazor"}),`
-`,t.jsx(e.p,{children:"We are currently working on the Blazor adapter for Forge, stay tuned for updates!"})]})}function l(n={}){const{wrapper:e}={...o(),...n.components};return e?t.jsx(e,{...n,children:t.jsx(r,{...n})}):r(n)}export{l as default};
---
+++ docs/pr/tyler-technologies-oss:fix-time-picker-min-max-wrap/assets/Skeleton-Mijll_ZC.js
@@ -0,0 +1,24 @@
+import{j as e,M as r,T as a,C as s}from"./blocks-B_kN-fZT.js";import{useMDXComponents as i}from"./index-BXeu3yKk.js";import{C as l}from"./CustomArgTypes-BDwPr3zi.js";import{C as c}from"./CssOnlyInformation-BT8WctVE.js";import{S as d,D as h,P as m,L as p,C as f,B as u,F as x,a as j}from"./Skeleton.stories-DLBAc8d5.js";import"./iframe-BBvvcFSq.js";import"./_commonjsHelpers-CqkleIqs.js";import"./utils-Bnph8n13.js";import"./class-map-QgCHjGYN.js";import"./directive-CJw_OlP2.js";import"./style-map-CctqU2vY.js";import"./feature-detection-uS6p5jc8.js";import"./skeleton-C4EH8VF8.js";import"./constants-wOq9K3uV.js";function o(t){const n={code:"code",h2:"h2",p:"p",...i(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(r,{of:d}),`
+`,e.jsx(a,{}),`
+`,e.jsx(n.p,{children:'The skeleton component is a generic, low-level component that can be used to build loading placeholder pages/elements called "skeleton screens". These screens represent the block-level visual elements of a page, or section of a page, where content is intended to be loaded sometime in the future. Using skeleton screens gives the illusion of speed to the user, and can often reduce frustration for users during long running processes.'}),`
+`,e.jsx(n.p,{children:"There are a few built-in variants that can be used by adding an attribute to the element. These built-in variants are specifically designed to mimic other components in the Forge library for ease of use."}),`
+`,e.jsx(n.h2,{id:"default",children:"Default"}),`
+`,e.jsx(s,{of:h}),`
+`,e.jsx(n.h2,{id:"profile",children:"Profile"}),`
+`,e.jsx(s,{of:m}),`
+`,e.jsx(n.h2,{id:"list",children:"list"}),`
+`,e.jsx(s,{of:p}),`
+`,e.jsx(n.h2,{id:"chips",children:"Chips"}),`
+`,e.jsx(s,{of:f}),`
+`,e.jsx(n.h2,{id:"buttons",children:"Buttons"}),`
+`,e.jsx(s,{of:u}),`
+`,e.jsx(n.h2,{id:"form-field",children:"Form Field"}),`
+`,e.jsx(s,{of:x}),`
+`,e.jsx(n.h2,{id:"api",children:"API"}),`
+`,e.jsx(l,{}),`
+`,e.jsx(n.h2,{id:"accessibility",children:"Accessibility"}),`
+`,e.jsxs(n.p,{children:["Ensure that the skeleton component does not interfere with screen readers by including the ",e.jsx(n.code,{children:'aria-hidden="true"'})," attribute."]}),`
+`,e.jsx(n.h2,{id:"css-only",children:"CSS-Only"}),`
+`,e.jsx(n.p,{children:"The skeleton component is also available as a CSS-only component without the need for JavaScript."}),`
+`,e.jsx(s,{of:j}),`
+`,e.jsx(c,{})]})}function B(t={}){const{wrapper:n}={...i(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(o,{...t})}):o(t)}export{B as default};
