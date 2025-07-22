507803cfef392e1b116843eca8915cb142bfca62
DRiFTy17 <DRiFTy17@users.noreply.github.com>
2025-07-22 15:24:44 +0000

    Deploying to gh-pages from @ tyler-technologies-oss/forge@384e6b0f0aac703900486cf83329d592575e3b71 🚀

---
+++ docs/pr/tyler-technologies-oss:fix-time-picker-min-max-wrap/assets/Backdrop-DLCSJyKw.js
@@ -0,0 +1,8 @@
+import{j as o,M as a,T as s,C as i}from"./blocks-BTp7l5NO.js";import{useMDXComponents as r}from"./index-cGhAmLir.js";import{C as p}from"./CustomArgTypes-CZQzHYhg.js";import{B as m,D as c}from"./Backdrop.stories-Cb1h0a9W.js";import"./iframe-DgPlgY2v.js";import"./_commonjsHelpers-CqkleIqs.js";import"./utils-Bnph8n13.js";import"./style-map-C_BEBmZn.js";import"./directive-CJw_OlP2.js";import"./feature-detection-uS6p5jc8.js";import"./backdrop-BZvWLwDX.js";import"./constants-wOq9K3uV.js";function e(t){const n={h2:"h2",p:"p",...r(),...t.components};return o.jsxs(o.Fragment,{children:[o.jsx(a,{of:m}),`
+`,o.jsx(s,{}),`
+`,o.jsx(n.p,{children:`Backdrops are building block components that are used to provide a visual overlay for a component or page. They are typically used to provide
+separation between some floating content and its background.`}),`
+`,o.jsx(n.p,{children:"The dialog component is a good example of a component that uses a backdrop."}),`
+`,o.jsx(i,{of:c}),`
+`,o.jsx(n.h2,{id:"api",children:"API"}),`
+`,o.jsx(p,{})]})}function k(t={}){const{wrapper:n}={...r(),...t.components};return n?o.jsx(n,{...t,children:o.jsx(e,{...t})}):e(t)}export{k as default};
---
+++ docs/pr/tyler-technologies-oss:fix-time-picker-min-max-wrap/assets/Key-DkWcqhhx.js
@@ -0,0 +1,10 @@
+import{j as t,M as n,T as a,C as r}from"./blocks-BTp7l5NO.js";import{useMDXComponents as s}from"./index-cGhAmLir.js";import{C as m}from"./CustomArgTypes-CZQzHYhg.js";import{K as c,D as l}from"./Key.stories-XUfmlUg2.js";import"./iframe-DgPlgY2v.js";import"./_commonjsHelpers-CqkleIqs.js";import"./utils-Bnph8n13.js";import"./feature-detection-uS6p5jc8.js";import"./key-item-Dn2LfObW.js";import"./base-lit-element-DD48r0Bf.js";import"./a11y-utils-fzPGYZJ6.js";import"./state-DGPIVT6j.js";import"./class-map-VyyRAxMT.js";import"./directive-CJw_OlP2.js";function o(i){const e={h2:"h2",p:"p",...s(),...i.components};return t.jsxs(t.Fragment,{children:[t.jsx(n,{of:c}),`
+`,t.jsx(a,{}),`
+`,t.jsx(e.p,{children:"Keys are used to label data in a chart or visualization."}),`
+`,t.jsx(r,{of:l}),`
+`,t.jsx(e.h2,{id:"api",children:"API"}),`
+`,t.jsx(m,{}),`
+`,t.jsx(e.h2,{id:"accessibility",children:"Accessibility"}),`
+`,t.jsx(e.p,{children:`Ensure that the icon colors of the key items are easily distinguishable and match the color of the
+associated visualization.`}),`
+`,t.jsx(e.p,{children:"The value slot may be used to create an accessible text alternative for data in the visualization."})]})}function g(i={}){const{wrapper:e}={...s(),...i.components};return e?t.jsx(e,{...i,children:t.jsx(o,{...i})}):o(i)}export{g as default};
