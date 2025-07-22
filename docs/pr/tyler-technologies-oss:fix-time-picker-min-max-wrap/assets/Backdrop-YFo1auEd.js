5c59ed897762d98b8c1740a46419fc7c90943b78
DRiFTy17 <DRiFTy17@users.noreply.github.com>
2025-07-22 15:10:17 +0000

    Deploying to gh-pages from @ tyler-technologies-oss/forge@64ec41bc34dc2d9f9a1fd8a0203b186d28f4c3c6 🚀

--- docs/pr/tyler-technologies-oss:fix-time-picker-min-max-wrap/assets/Backdrop-YFo1auEd.js
+++
@@ -1,8 +0,0 @@
-import{j as o,M as a,T as s,C as i}from"./blocks-B_kN-fZT.js";import{useMDXComponents as r}from"./index-BXeu3yKk.js";import{C as p}from"./CustomArgTypes-BDwPr3zi.js";import{B as m,D as c}from"./Backdrop.stories-wHE5ASm2.js";import"./iframe-BBvvcFSq.js";import"./_commonjsHelpers-CqkleIqs.js";import"./utils-Bnph8n13.js";import"./style-map-CctqU2vY.js";import"./directive-CJw_OlP2.js";import"./feature-detection-uS6p5jc8.js";import"./backdrop-BZvWLwDX.js";import"./constants-wOq9K3uV.js";function e(t){const n={h2:"h2",p:"p",...r(),...t.components};return o.jsxs(o.Fragment,{children:[o.jsx(a,{of:m}),`
-`,o.jsx(s,{}),`
-`,o.jsx(n.p,{children:`Backdrops are building block components that are used to provide a visual overlay for a component or page. They are typically used to provide
-separation between some floating content and its background.`}),`
-`,o.jsx(n.p,{children:"The dialog component is a good example of a component that uses a backdrop."}),`
-`,o.jsx(i,{of:c}),`
-`,o.jsx(n.h2,{id:"api",children:"API"}),`
-`,o.jsx(p,{})]})}function k(t={}){const{wrapper:n}={...r(),...t.components};return n?o.jsx(n,{...t,children:o.jsx(e,{...t})}):e(t)}export{k as default};
---
+++ docs/pr/tyler-technologies-oss:fix-time-picker-min-max-wrap/assets/Card-DjMNGg0G.js
@@ -0,0 +1,22 @@
+import{j as e,M as s,T as a,C as t}from"./blocks-C6Jrb_II.js";import{useMDXComponents as i}from"./index-BNSwP12w.js";import{C as c}from"./CustomArgTypes-DTd5VyuI.js";import{C as d}from"./CssOnlyInformation-BDWFWTn7.js";import{C as l,D as p,S as m,a as h}from"./Card.stories-DX1EyotK.js";import"./iframe-DR51lazJ.js";import"./_commonjsHelpers-CqkleIqs.js";import"./utils-Bnph8n13.js";import"./feature-detection-uS6p5jc8.js";import"./icon-B8CdcxqJ.js";import"./constants-wOq9K3uV.js";import"./base-adapter-Mla2Q9YN.js";import"./index-CiLSBptl.js";import"./decorators-BqHlIeRn.js";import"./class-map-DIxnoqOn.js";import"./directive-CJw_OlP2.js";import"./style-map-BPdMOxGN.js";import"./card-Hoc-29Ft.js";import"./base-lit-element-CwV61BG7.js";import"./utils-CRxrUqQD.js";import"./icon-button-DkluvO-9.js";import"./base-button-adapter-D0zqnDMQ.js";import"./with-label-aware-D31hYnqk.js";import"./with-default-aria-CUUWGrPB.js";import"./a11y-utils-fzPGYZJ6.js";import"./focus-indicator-IWpzSXYP.js";import"./state-layer-BFwsAUDA.js";import"./button-r2EMLpWm.js";import"./scaffold-CGyusmPL.js";import"./toolbar-Byb6kcao.js";function r(n){const o={blockquote:"blockquote",code:"code",h2:"h2",li:"li",p:"p",ul:"ul",...i(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(s,{of:l}),`
+`,e.jsx(a,{}),`
+`,e.jsx(o.p,{children:"Cards are used to group related content and actions together in a single container."}),`
+`,e.jsx(o.h2,{id:"basic",children:"Basic"}),`
+`,e.jsx(t,{of:p}),`
+`,e.jsxs(o.blockquote,{children:[`
+`,e.jsxs(o.p,{children:["By default the card component will apply a ",e.jsx(o.code,{children:"padding"})," of ",e.jsx(o.code,{children:"16px"})," to the entire internal container element."]}),`
+`,e.jsxs(o.p,{children:["Use the ",e.jsx(o.code,{children:"--forge-card-padding"})," CSS custom property to set a custom padding value."]}),`
+`]}),`
+`,e.jsx(t,{of:m}),`
+`,e.jsx(o.h2,{id:"api",children:"API"}),`
+`,e.jsx(c,{}),`
+`,e.jsx(o.h2,{id:"accessibility",children:"Accessibility"}),`
+`,e.jsxs(o.ul,{children:[`
+`,e.jsx(o.li,{children:"The card component should not interfere with tab flow."}),`
+`,e.jsxs(o.li,{children:["Add a ",e.jsx(o.code,{children:"role"})," attribute if this card has any applicable role that should be known to screen readers."]}),`
+`,e.jsxs(o.li,{children:["Consider using the ",e.jsx(o.code,{children:'"group"'})," role on the ",e.jsx(o.code,{children:"<forge-card>"})," element if the content is related and separate from other surrounding items."]}),`
+`]}),`
+`,e.jsx(o.h2,{id:"css-only",children:"CSS-Only"}),`
+`,e.jsx(o.p,{children:"The card component is also available as a CSS-only component without the need for JavaScript."}),`
+`,e.jsx(t,{of:h}),`
+`,e.jsx(d,{})]})}function G(n={}){const{wrapper:o}={...i(),...n.components};return o?e.jsx(o,{...n,children:e.jsx(r,{...n})}):r(n)}export{G as default};
