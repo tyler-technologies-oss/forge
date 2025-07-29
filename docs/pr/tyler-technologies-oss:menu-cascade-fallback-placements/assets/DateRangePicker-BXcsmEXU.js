845b09529aad9655b40b29f5659d93a2315e3718
DRiFTy17 <DRiFTy17@users.noreply.github.com>
2025-07-29 13:12:46 +0000

    Deploying to gh-pages from @ tyler-technologies-oss/forge@9ac8f1437c6df3a6e1c3ab8cd86713c81541355d 🚀

---
+++ docs/pr/tyler-technologies-oss:menu-cascade-fallback-placements/assets/DateRangePicker-BXcsmEXU.js
@@ -0,0 +1,22 @@
+import{j as e,M as r,T as s,C as a}from"./blocks-ChJum9up.js";import{useMDXComponents as o}from"./index-sLXGAegV.js";import{C as d}from"./CustomArgTypes-DYl_-taf.js";import{D as c,a as p}from"./DateRangePicker.stories-eU-AP9VS.js";import"./iframe-Chnhjlzc.js";import"./_commonjsHelpers-CqkleIqs.js";import"./utils-aEn5vBmw.js";import"./feature-detection-uS6p5jc8.js";import"./date-range-picker-ZqI-4J_o.js";import"./constants-wOq9K3uV.js";import"./base-adapter-Mla2Q9YN.js";import"./icon-B8CdcxqJ.js";import"./index-CiLSBptl.js";import"./calendar-Buw9wM2z.js";import"./tooltip-CcxiN_AO.js";import"./utils-CRxrUqQD.js";import"./overlay-D-bkGTD9.js";import"./with-longpress-listener-CGXzI-TM.js";import"./dismissible-stack-BdWcv7_4.js";import"./with-default-aria-CUUWGrPB.js";import"./a11y-utils-fzPGYZJ6.js";import"./focus-indicator-IWpzSXYP.js";import"./state-layer-BFwsAUDA.js";import"./event-utils-C1SDeUaq.js";import"./button-r2EMLpWm.js";import"./base-button-adapter-D0zqnDMQ.js";import"./with-label-aware-D31hYnqk.js";import"./icon-button-DkluvO-9.js";import"./base-date-picker-core-Cz6B3pho.js";import"./a11y-BxM9_46k.js";import"./popover-xi3V_Oll.js";import"./text-field-su1dEfif.js";import"./base-field-WJtNENB5.js";import"./label-BSASIOtP.js";import"./button-toggle-group-D5jBldBo.js";import"./with-form-associated-BeE6NBc1.js";import"./checkbox-DOmkbh7U.js";import"./switch-Bt2bdQXJ.js";function n(t){const i={code:"code",h2:"h2",li:"li",p:"p",ul:"ul",...o(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(r,{of:c}),`
+`,e.jsx(s,{}),`
+`,e.jsxs(i.p,{children:["The date range picker is very similar to the usage of the standard datepicker component, the only difference is that it allows for the selection of two dates that define a start and end range. This means that when setting up the component, you must provide it two ",e.jsx(i.code,{children:"<input>"})," elements, one for the start date and one for the end date."]}),`
+`,e.jsx(i.p,{children:"There are many configuration options available through the component API, along with date masking options that force the dates to be entered using a specific format."}),`
+`,e.jsx(a,{of:p}),`
+`,e.jsx(i.h2,{id:"api",children:"API"}),`
+`,e.jsx(d,{}),`
+`,e.jsx(i.h2,{id:"accessibility",children:"Accessibility"}),`
+`,e.jsxs(i.ul,{children:[`
+`,e.jsx(i.li,{children:"When using a screen reader, ensure keyboard navigation in the calendar is announced."}),`
+`,e.jsxs(i.li,{children:["Be sure that you add the proper ",e.jsx(i.code,{children:"aria-label"})," to the ",e.jsx(i.code,{children:"<input>"})," element if necessary."]}),`
+`,e.jsxs(i.li,{children:["The date picker component will add the following ARIA attributes to the ",e.jsx(i.code,{children:"<input>"})," element for you:",`
+`,e.jsxs(i.ul,{children:[`
+`,e.jsx(i.li,{children:e.jsx(i.code,{children:"aria-live"})}),`
+`,e.jsx(i.li,{children:e.jsx(i.code,{children:"aria-atomic"})}),`
+`,e.jsx(i.li,{children:e.jsx(i.code,{children:"aria-haspopup"})}),`
+`,e.jsx(i.li,{children:e.jsx(i.code,{children:"aria-expanded"})}),`
+`,e.jsx(i.li,{children:e.jsx(i.code,{children:"aria-owns"})}),`
+`,e.jsx(i.li,{children:e.jsx(i.code,{children:"aria-disabled"})}),`
+`]}),`
+`]}),`
+`]})]})}function U(t={}){const{wrapper:i}={...o(),...t.components};return i?e.jsx(i,{...t,children:e.jsx(n,{...t})}):n(t)}export{U as default};
---
+++ docs/pr/tyler-technologies-oss:menu-cascade-fallback-placements/assets/profile-B12dvoNp.js
@@ -0,0 +1,53 @@
+import{j as t,M as m,T as p,C as i}from"./blocks-ChJum9up.js";import{useMDXComponents as r}from"./index-sLXGAegV.js";import{C as s}from"./CustomArgTypes-DYl_-taf.js";import{P as l,D as a,W as c}from"./profile.stories-D1S5EEJx.js";import"./iframe-Chnhjlzc.js";import"./_commonjsHelpers-CqkleIqs.js";import"./utils-aEn5vBmw.js";import"./feature-detection-uS6p5jc8.js";import"./accordion-BremYYNV.js";import"./constants-wOq9K3uV.js";import"./base-adapter-Mla2Q9YN.js";import"./expansion-panel-Cn_4I3Ho.js";import"./icon-B8CdcxqJ.js";import"./index-CiLSBptl.js";import"./utils-CRxrUqQD.js";import"./app-bar-profile-button-Dg8938gW.js";import"./state-layer-BFwsAUDA.js";import"./focus-indicator-IWpzSXYP.js";import"./icon-button-DkluvO-9.js";import"./base-button-adapter-D0zqnDMQ.js";import"./with-label-aware-D31hYnqk.js";import"./with-default-aria-CUUWGrPB.js";import"./a11y-utils-fzPGYZJ6.js";import"./tooltip-CcxiN_AO.js";import"./overlay-D-bkGTD9.js";import"./with-longpress-listener-CGXzI-TM.js";import"./dismissible-stack-BdWcv7_4.js";import"./badge-aVnrGA1x.js";import"./base-lit-element-DPaPv8de.js";import"./menu-BfPyyeAQ.js";import"./list-z5iQB-6r.js";import"./event-utils-zQ4FLDwK.js";import"./list-dropdown-aware-core-P1eltbhy.js";import"./list-dropdown-C9nqfntS.js";import"./event-utils-C1SDeUaq.js";import"./linear-progress-2PahUgVv.js";import"./popover-xi3V_Oll.js";import"./skeleton-C4EH8VF8.js";import"./a11y-BxM9_46k.js";import"./base-component-delegate-CN_SSLUw.js";import"./avatar-ClnTn700.js";import"./state-_bO2bw1d.js";import"./style-map-BpXBe1Q3.js";import"./directive-CJw_OlP2.js";import"./class-map-DEPl7sMB.js";import"./avatar-constants-jPVIytDz.js";import"./button-r2EMLpWm.js";import"./toolbar-Byb6kcao.js";import"./autocomplete-65q8AomI.js";import"./divider-DoNAUeHX.js";import"./text-field-su1dEfif.js";import"./base-field-WJtNENB5.js";import"./label-BSASIOtP.js";import"./button-toggle-group-D5jBldBo.js";import"./with-form-associated-BeE6NBc1.js";import"./checkbox-DOmkbh7U.js";import"./switch-Bt2bdQXJ.js";import"./chip-set-BBt44dwa.js";import"./backdrop-BZvWLwDX.js";import"./banner-iv-1_6nx.js";import"./bottom-sheet-BZd-gbTi.js";import"./dialog-JQ2PS6Se.js";import"./button-area-Dgvn0HIt.js";import"./calendar-Buw9wM2z.js";import"./card-D981bX2R.js";import"./circular-progress-xrl2HF46.js";import"./color-picker-DD2G-EWp.js";import"./date-picker-f5U61SnV.js";import"./base-date-picker-core-Cz6B3pho.js";import"./date-range-picker-ZqI-4J_o.js";import"./base-drawer-CIBCdxIp.js";import"./drawer-rHXDK_gj.js";import"./modal-drawer-DysXIZBQ.js";import"./mini-drawer-B7aElFMq.js";import"./file-picker-C4QqMTt4.js";import"./floating-action-button-Chj1MDjC.js";import"./inline-message-By3BVHSa.js";import"./key-item-B1zs3neY.js";import"./keyboard-shortcut-Cs_3tUZu.js";import"./label-value-CaouEyrO.js";import"./meter-group-csZGWF_p.js";import"./page-state-em5vC-QK.js";import"./paginator-BnGE3WE_.js";import"./select-CjZzPkzG.js";import"./scaffold-CGyusmPL.js";import"./select-dropdown-DQ6smdjT.js";import"./skip-link-7_m7DPxL.js";import"./slider-BQev95re.js";import"./split-view-BVDvrgXl.js";import"./stack-niTWfPr5.js";import"./stepper-DMgB9a3z.js";import"./table-CgQEfyOI.js";import"./tab-bar-R18Qi337.js";import"./time-picker-DjqM89li.js";import"./toast-DVotDAeY.js";import"./view-switcher-EfMcYRc9.js";import"./deprecated-icon-button-Ch8JZU3u.js";import"./split-button-40Nu64fp.js";function n(o){const e={blockquote:"blockquote",code:"code",h2:"h2",li:"li",p:"p",pre:"pre",ul:"ul",...r(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(m,{of:l}),`
+`,t.jsx(p,{}),`
+`,t.jsxs(e.p,{children:["For convenience Forge provides the ",t.jsx(e.code,{children:"<forge-app-bar-profile-button>"})," component. This component uses an icon-button (with a predefined avatar), and is responsible for displaying profile information within a popup. The power of this component is that it will ensure a consistent experience for users across all applications that consume it."]}),`
+`,t.jsxs(e.p,{children:["If necessary, you can use the ",t.jsx(e.code,{children:"profileCardBuilder"})," API to provide custom content within the profile card popup."]}),`
+`,t.jsxs(e.blockquote,{children:[`
+`,t.jsx(e.p,{children:"It is expected that you place this component in the end slot of the app-bar for consistency and familiarity for users."}),`
+`]}),`
+`,t.jsx(e.h2,{id:"common",children:"Common"}),`
+`,t.jsx(e.p,{children:"A common usage of the profile button will require you to provide some basic information about the user so that it can properly display the data in a familiar location for users. This information needs to be available prior to the user opening the popup."}),`
+`,t.jsx(i,{of:a}),`
+`,t.jsx(e.h2,{id:"custom-content",children:"Custom content"}),`
+`,t.jsx(e.p,{children:`The profile card allows for custom content to be placed within its template. This is provided in the form of a "builder" callback that you can use to generate DOM
+that the component will render for you. You will need to use JavaScript to accomplish this.`}),`
+`,t.jsx(i,{of:c}),`
+`,t.jsx(e.p,{children:`The following snippet demonstrates basic usage in vanilla JavaScript, but if you're using this within a framework you can use the same concept, or if you prefer to avoid
+using vanilla DOM APIs within framework code you can always create a dynamic framework component and return the element instance from the builder callback.`}),`
+`,t.jsx(e.pre,{children:t.jsx(e.code,{className:"language-typescript",children:`profileButton.profileCardBuilder = () => {
+  const listElement = document.createElement('forge-list');
+  listElement.addEventListener('forge-list-item-select', ({ detail }) => {
+    console.log('[profile-card] Selected custom item:', detail.value);
+  });
+  listElement.style.setProperty('--forge-list-padding', '0');
+  listElement.appendChild(document.createElement('forge-divider'));
+  listElement.appendChild(buildListItemElement('My Reports', 'assignment', 'reports'));
+  listElement.appendChild(buildListItemElement('My Workflow', 'work_outline', 'workflow'));
+  listElement.appendChild(buildListItemElement('My Alerts', 'warning', 'alerts'));
+  listElement.appendChild(buildListItemElement('My Preferences', 'settings', 'preferences'));
+  return listElement;
+};
+
+function buildListItemElement(text: string, icon: string, value: string): HTMLElement {
+  const listItemElement = document.createElement('forge-list-item');
+  listItemElement.value = value;
+
+  const iconElement = document.createElement('forge-icon');
+  iconElement.slot = 'leading';
+  iconElement.name = icon;
+  listItemElement.appendChild(iconElement);
+
+  const buttonElement = document.createElement('button');
+  buttonElement.type = 'button';
+  buttonElement.innerText = text;
+  listItemElement.appendChild(buttonElement);
+
+  return listItemElement;
+}
+`})}),`
+`,t.jsx(e.h2,{id:"api",children:"API"}),`
+`,t.jsx(s,{}),`
+`,t.jsx(e.h2,{id:"accessibility",children:"Accessibility"}),`
+`,t.jsxs(e.ul,{children:[`
+`,t.jsx(e.li,{children:"The component will handle adding necessary ARIA attributes for you, as well as ensure the proper color contrast ratio is used."}),`
+`]})]})}function oe(o={}){const{wrapper:e}={...r(),...o.components};return e?t.jsx(e,{...o,children:t.jsx(n,{...o})}):n(o)}export{oe as default};
