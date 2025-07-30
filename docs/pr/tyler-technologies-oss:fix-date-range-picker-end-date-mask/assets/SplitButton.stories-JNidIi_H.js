42a3e77e07def0905a03927bf863eaa4576d7de7
DRiFTy17 <DRiFTy17@users.noreply.github.com>
2025-07-30 17:14:51 +0000

    Deploying to gh-pages from @ tyler-technologies-oss/forge@3e1ff99b13ff14e39feca588e25183a0dda5b2fa 🚀

---
+++ docs/pr/tyler-technologies-oss:fix-date-range-picker-end-date-mask/assets/SplitButton.stories-JNidIi_H.js
@@ -0,0 +1,14 @@
+import{z as i,A as l,I as m}from"./icon-B8CdcxqJ.js";import"./feature-detection-uS6p5jc8.js";import{S as d}from"./split-button-40Nu64fp.js";import"./button-r2EMLpWm.js";import"./focus-indicator-IWpzSXYP.js";import"./index-CiLSBptl.js";import"./state-layer-BFwsAUDA.js";import{E as c,x as u}from"./iframe-DIGFEk6P.js";import{o as f}from"./style-map-3pRPob4x.js";import{g,G as b,a as S}from"./utils-qIDk0Vql.js";import"./menu-BfPyyeAQ.js";import"./linear-progress-2PahUgVv.js";import"./list-z5iQB-6r.js";import"./popover-xi3V_Oll.js";import"./overlay-D-bkGTD9.js";import"./skeleton-C4EH8VF8.js";m.define([i,l]);const r="forge-split-button",T={title:"Components/Split Button",render:e=>{const p=[{label:"Schedule send",value:"schedule",leadingIcon:i.name,leadingIconType:"component"},{label:"Save draft",value:"draft",leadingIcon:l.name,leadingIconType:"component"}],o=S(e);return u`
+      <forge-split-button
+        variant=${e.variant}
+        theme=${e.theme}
+        ?disabled=${e.disabled}
+        ?dense=${e.dense}
+        ?pill=${e.pill}
+        style=${o?f(o):c}>
+        <forge-button style="min-width: 100px;">Send</forge-button>
+        <forge-menu .options=${p}>
+          <forge-button aria-label="Show menu" popover-icon></forge-button>
+        </forge-menu>
+      </forge-split-button>
+    `},component:r,parameters:{actions:{disable:!0}},argTypes:{...g({tagName:r,controls:{variant:{control:{type:"select"},options:["text","outlined","filled","raised"]},theme:{control:{type:"select"},options:b}}})},args:{variant:"raised",theme:d.defaults.DEFAULT_THEME,disabled:!1,dense:!1,pill:!1}},t={};var n,a,s;t.parameters={...t.parameters,docs:{...(n=t.parameters)==null?void 0:n.docs,source:{originalSource:"{}",...(s=(a=t.parameters)==null?void 0:a.docs)==null?void 0:s.source}}};const y=["Demo"],j=Object.freeze(Object.defineProperty({__proto__:null,Demo:t,__namedExportsOrder:y,default:T},Symbol.toStringTag,{value:"Module"}));export{t as D,j as S};
