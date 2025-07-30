aff66cc8e9db1c54717fa43f10250a93abf2b6dd
DRiFTy17 <nicholskb17@gmail.com>
2025-07-30 17:48:19 +0000

    Deploying to gh-pages from @ tyler-technologies-oss/forge@b614ccee217a02c719413d290cc9d613619c9a48 🚀

--- docs/main/assets/Blazor-DWa3O8pW.js
+++
@@ -1,3 +0,0 @@
-import{j as t,M as a}from"./blocks-BzeOhL_2.js";import{useMDXComponents as o}from"./index-C9eO6J1M.js";import"./iframe-lbQL-3vk.js";import"./_commonjsHelpers-CqkleIqs.js";function r(n){const e={h1:"h1",p:"p",...o(),...n.components};return t.jsxs(t.Fragment,{children:[t.jsx(a,{title:"Frameworks/Blazor"}),`
-`,t.jsx(e.h1,{id:"blazor",children:"Blazor"}),`
-`,t.jsx(e.p,{children:"We are currently working on the Blazor adapter for Forge, stay tuned for updates!"})]})}function l(n={}){const{wrapper:e}={...o(),...n.components};return e?t.jsx(e,{...n,children:t.jsx(r,{...n})}):r(n)}export{l as default};
