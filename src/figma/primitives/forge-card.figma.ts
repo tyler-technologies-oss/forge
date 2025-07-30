94be9b088aa072961048e655df27d30340cb7200
Rob Sturgill <74620886+robsturgill-tyler@users.noreply.github.com>
2025-07-30 12:19:35 -0400

    Update forge-card.figma.ts

--- src/figma/primitives/forge-card.figma.ts
+++ src/figma/primitives/forge-card.figma.ts
@@ -2,9 +2,9 @@ import figma, { html } from '@figma/code-connect/html';
 
 figma.connect('<FIGMA_CARD_CARD>', {
   props: {
-    elevated: figma.enum('Style', {
-      Elevated: true
+    raised: figma.enum('Style', {
+      Raised: true
     })
   },
-  example: props => html`<forge-card raised="${props.elevated}"></forge-card>`
+  example: props => html`<forge-card raised="${props.raised}"></forge-card>`
 });
