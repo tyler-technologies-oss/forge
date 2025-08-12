39dbfeec9b395fab1bf888677ba466203cf7a40e
Rob Sturgill <rob.sturgill@tylertech.com>
2025-08-12 13:29:26 -0400

    feat: add checkbox and radio components to Figma integration

---
+++ src/figma/primitives/forge-checkbox.figma.ts
@@ -0,0 +1,34 @@
+import figma, { html } from '@figma/code-connect/html';
+
+const sharedProps = {
+  checked: figma.enum('Checked', {
+    true: html`checked`,
+    Indeterminate: html`indeterminate`
+  }),
+  state: figma.enum('State', {
+    Disabled: 'disabled'
+  })
+};
+
+// single checkbox component
+figma.connect('<FIGMA_CHECKBOX_CHECKBOX>', {
+  props: {
+    ...sharedProps
+  },
+  example: props => html`<forge-checkbox ${props.checked} ${props.state}></forge-checkbox>`
+});
+
+// checkbox label component
+figma.connect('<FIGMA_CHECKBOX_CHECKBOX_LABEL>', {
+  props: {
+    label: figma.string('Label'),
+    labelPosition: figma.boolean('Start label', {
+      true: html`label-position="start"`,
+      false: undefined
+    }),
+    checkboxProps: figma.nestedProps('forge-checkbox', {
+      ...sharedProps
+    })
+  },
+  example: props => html`<forge-checkbox ${props.checkboxProps.checked} ${props.checkboxProps.state} ${props.labelPosition}>${props.label}</forge-checkbox>`
+});
---
+++ src/figma/primitives/forge-radio.figma.ts
@@ -0,0 +1,34 @@
+import figma, { html } from '@figma/code-connect/html';
+
+const sharedProps = {
+  checked: figma.boolean('Checked', {
+    true: html`checked`,
+    false: undefined
+  }),
+  state: figma.enum('State', {
+    Disabled: 'disabled'
+  })
+};
+
+// single radio component
+figma.connect('<FIGMA_RADIO_RADIO>', {
+  props: {
+    ...sharedProps
+  },
+  example: props => html`<forge-radio ${props.checked} ${props.state}></forge-radio>`
+});
+
+// radio label component
+figma.connect('<FIGMA_RADIO_RADIO_LABEL>', {
+  props: {
+    label: figma.string('Label'),
+    labelPosition: figma.boolean('Start label', {
+      true: html`label-position="start"`,
+      false: undefined
+    }),
+    radioProps: figma.nestedProps('forge-radio', {
+      ...sharedProps
+    })
+  },
+  example: props => html`<forge-radio ${props.radioProps.checked} ${props.radioProps.state} ${props.labelPosition}>${props.label}</forge-radio>`
+});
