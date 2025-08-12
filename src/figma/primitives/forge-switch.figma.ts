980cdf565ba8c5adf53b682fbd79de449c08319f
Rob Sturgill <rob.sturgill@tylertech.com>
2025-08-12 17:04:12 -0400

    feat: add switch and switch label components to Figma integration

---
+++ src/figma/primitives/forge-switch.figma.ts
@@ -0,0 +1,36 @@
+import figma, { html } from '@figma/code-connect/html';
+
+const sharedProps = {
+  checked: figma.boolean('Switch', {
+    true: html`checked`,
+    false: undefined
+  }),
+  state: figma.enum('State', {
+    Disabled: 'disabled'
+  })
+};
+
+// single switch component
+figma.connect('<FIGMA_SWITCH_SWITCH>', {
+  props: {
+    ...sharedProps
+  },
+
+  example: props => html`<forge-switch ${props.checked} ${props.state}></forge-switch>`
+});
+
+// switch & label component
+figma.connect('<FIGMA_SWITCH_SWITCH_LABEL>', {
+  props: {
+    label: figma.string('Label'),
+    labelPosition: figma.boolean('Start label', {
+      true: html`label-position="start"`,
+      false: undefined
+    }),
+    switchProps: figma.nestedProps('forge-switch', {
+      ...sharedProps
+    })
+  },
+
+  example: props => html`<forge-switch ${props.switchProps.checked} ${props.switchProps.state} ${props.labelPosition}>${props.label}</forge-switch>`
+});
