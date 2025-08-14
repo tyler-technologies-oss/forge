ea1ceca49d823be5df390c9152d61c88bdbb95af
Rob Sturgill <rob.sturgill@tylertech.com>
2025-08-14 15:47:41 -0400

    feat: add forge-banner component and update forge-button for neutral variant

---
+++ src/figma/primitives/forge-banner.figma.ts
@@ -0,0 +1,60 @@
+import figma, { html } from '@figma/code-connect/html';
+
+const sharedProps = {
+  iconSlot: figma.boolean('Icon slot', {
+    true: html`<forge-icon slot="icon" name="icon_name"></forge-icon>`,
+    false: undefined
+  }),
+  text: figma.string('Text'),
+  canDismiss: figma.boolean('canDismiss'),
+  buttonSlot: figma.boolean('Button slot', {
+    true: figma.children('.button-neutral'),
+    false: undefined
+  })
+};
+
+// banner
+figma.connect('<FIGMA_BANNER_BANNER>', {
+  props: {
+    ...sharedProps
+  },
+
+  example: props => html` <forge-banner> ${props.text} ${props.iconSlot} </forge-banner>`
+});
+
+//banner with button slot
+figma.connect('<FIGMA_BANNER_BANNER>', {
+  variant: { 'Button slot': true },
+  props: {
+    ...sharedProps,
+    buttonInstance: figma.nestedProps('.button-neutral', {
+      type: figma.enum('Type', {
+        Raised: 'raised',
+        Outlined: 'outlined',
+        Text: 'text',
+        Tonal: 'tonal',
+        Filled: 'filled'
+      }),
+      disabled: figma.enum('State', {
+        Disabled: true
+      }),
+      text: figma.string('Text'),
+      startInstance: figma.boolean('Start slot', {
+        true: html`<forge-icon name="icon_name" slot="start"></forge-icon>`,
+        false: undefined
+      }),
+      endInstance: figma.boolean('End slot', {
+        true: html`<forge-icon name="icon_name" slot="end"></forge-icon>`,
+        false: undefined
+      })
+    })
+  },
+
+  example: props =>
+    html` <forge-banner>
+      ${props.text} ${props.iconSlot}
+      <forge-button slot="button" variant="${props.buttonInstance.type}" disabled="${props.buttonInstance.disabled}">
+        ${props.buttonInstance.startInstance} ${props.buttonInstance.text} ${props.buttonInstance.endInstance}</forge-button
+      >
+    </forge-banner>`
+});
--- src/figma/primitives/forge-button.figma.ts
+++ src/figma/primitives/forge-button.figma.ts
@@ -1,36 +1,47 @@
 import figma, { html } from '@figma/code-connect/html';
 
+const sharedProps = {
+  text: figma.string('Text'),
+  startInstance: figma.boolean('Start slot', {
+    true: html`<forge-icon name="icon_name" slot="start"></forge-icon>`,
+    false: undefined
+  }),
+  endInstance: figma.boolean('End slot', {
+    true: html`<forge-icon name="icon_name" slot="end"></forge-icon>`,
+    false: undefined
+  }),
+  // startInstanceName: figma.children(".forge-icon"),
+  startInstanceName: figma.nestedProps('.forge-icon', {
+    icon: figma.instance('Icon')
+  }),
+  type: figma.enum('Type', {
+    Raised: 'raised',
+    Outlined: 'outlined',
+    Text: 'text',
+    Tonal: 'tonal',
+    Filled: 'filled'
+  }),
+  disabled: figma.enum('State', {
+    Disabled: true
+  })
+};
+
 // button component
 figma.connect('<FIGMA_BUTTON_BUTTON>', {
   props: {
-    text: figma.string('Text'),
-    startInstance: figma.boolean('Start slot', {
-      true: html`<forge-icon name="icon-name" slot="start"></forge-icon>`,
-      false: undefined
-    }),
-    endInstance: figma.boolean('End slot', {
-      true: html`<forge-icon name="icon-name" slot="end"></forge-icon>`,
-      false: undefined
-    }),
-
-    // startInstanceName: figma.children(".forge-icon"),
-    startInstanceName: figma.nestedProps('.forge-icon', {
-      icon: figma.instance('Icon')
-    }),
-    type: figma.enum('Type', {
-      Raised: 'raised',
-      Outlined: 'outlined',
-      Text: 'text',
-      Tonal: 'tonal',
-      Filled: 'filled'
-    }),
-    disabled: figma.enum('State', {
-      Disabled: true
-    })
+    ...sharedProps
   },
   // alternative to get the icon name dynamically
   // <forge-icon name="${props.startInstanceName.icon}" slot="start"></forge-icon>
-  example: props => html`
-    <forge-button variant="${props.type}" disabled="${props.disabled}"> ${props.startInstance} ${props.text} ${props.endInstance} </forge-button>
-  `
+  example: props =>
+    html` <forge-button variant="${props.type}" disabled="${props.disabled}"> ${props.startInstance} ${props.text} ${props.endInstance}</forge-button>`
+});
+
+//private neutral button for forge-banner component
+figma.connect('<FIGMA_BUTTON_BUTTON_NEUTRAL>', {
+  props: {
+    ...sharedProps
+  },
+  example: props =>
+    html` <forge-button variant="${props.type}" disabled="${props.disabled}"> ${props.startInstance} ${props.text} ${props.endInstance}</forge-button>`
 });
