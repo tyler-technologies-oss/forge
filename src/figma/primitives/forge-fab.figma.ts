07bcae602c62a8479b8283fe5fc07b0fff6911b5
Rob Sturgill <rob.sturgill@tylertech.com>
2025-08-14 14:07:51 -0400

    feat: update Figma integration with new FAB component and version bump for code-connect

---
+++ src/figma/primitives/forge-fab.figma.ts
@@ -0,0 +1,42 @@
+import figma, { html } from '@figma/code-connect/html';
+
+const sharedProps = {
+  type: figma.enum('Type', {
+    Extended: 'extended',
+    Default: 'default'
+  }),
+  text: figma.string('Text'),
+  startSlot: figma.boolean('Start slot', {
+    true: html`<forge-icon name="icon-name"></forge-icon>`,
+    false: undefined
+  }),
+  popoverIcon: figma.boolean('popoverIcon', {
+    true: html`popover-icon`,
+    false: undefined
+  }),
+  state: figma.enum('State', {
+    Disabled: 'disabled'
+  })
+};
+
+figma.connect('<FIGMA_FAB_FAB>', {
+  variant: { Type: 'Default' },
+  props: {
+    ...sharedProps
+  },
+
+  example: props => html` <forge-fab ${props.popoverIcon} ${props.state}> ${props.startSlot} </forge-fab>`
+});
+
+figma.connect('<FIGMA_FAB_FAB>', {
+  variant: { Type: 'Extended' },
+  props: {
+    ...sharedProps
+  },
+
+  example: props =>
+    html` <forge-fab ${props.popoverIcon} ${props.state}>
+      ${props.startSlot}
+      <span slot="label">${props.text}</span>
+    </forge-fab>`
+});
