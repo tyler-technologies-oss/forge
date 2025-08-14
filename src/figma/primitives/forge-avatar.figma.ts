2d993b2a9d70fc2aa0ebf5d6cf96dcdf90d8a4e3
Rob Sturgill <rob.sturgill@tylertech.com>
2025-08-14 14:33:29 -0400

    feat: add avatar and divider components to Figma integration

---
+++ src/figma/primitives/forge-avatar.figma.ts
@@ -0,0 +1,28 @@
+import figma, { html } from '@figma/code-connect/html';
+
+figma.connect('<FIGMA_AVATAR_AVATAR>', {
+  variant: { Type: 'Initials' },
+  props: {
+    text: figma.string('Text')
+  },
+
+  example: props => html`<forge-avatar text="${props.text}"></forge-avatar>`
+});
+
+figma.connect('<FIGMA_AVATAR_AVATAR>', {
+  variant: { Type: 'Icon' },
+  props: {
+    icon: figma.instance('Icon')
+  },
+
+  example: props => html`<forge-avatar>${props.icon}</forge-avatar>`
+});
+
+figma.connect('<FIGMA_AVATAR_AVATAR>', {
+  variant: { Type: 'Image' },
+  props: {
+    image: figma.instance('Image')
+  },
+
+  example: props => html`<forge-avatar image-url="./path-to-image.jpg"></forge-avatar>`
+});
---
+++ src/figma/primitives/forge-divider.figma.ts
@@ -0,0 +1,6 @@
+import figma, { html } from '@figma/code-connect/html';
+
+figma.connect('<FIGMA_DIVIDER_DIVIDER>', {
+  props: {},
+  example: props => html`<forge-divider></forge-divider>`
+});
