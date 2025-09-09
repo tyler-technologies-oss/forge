01d0a71d3715f0dd771eebf8862717df4a20dbd4
DRiFTy17 <DRiFTy17@users.noreply.github.com>
2025-09-09 16:23:33 +0000

    Deploying to gh-pages from @ tyler-technologies-oss/forge@7d16772a7a3242762aac21cff1765bf5d248c5e3 🚀

---
+++ docs/pr/tyler-technologies-oss:dialog-move-boundary/assets/FocusIndicator-CxDCXZT8.js
@@ -0,0 +1,16 @@
+import{j as t,M as e,T as a,C as s}from"./blocks-4jlNIKwi.js";import{useMDXComponents as r}from"./index-Bf60kVIf.js";import{C as c}from"./CustomArgTypes-HdxpZdHE.js";import{C as h}from"./CssOnlyInformation-BbO0v4xf.js";import{F as m,D as p,W as l,C as d}from"./FocusIndicator.stories-C0owE-LU.js";import"./iframe-BAzchtDj.js";import"./_commonjsHelpers-CqkleIqs.js";import"./utils-BuPomTDA.js";import"./style-map-BJNrGraq.js";import"./directive-CJw_OlP2.js";import"./class-map-DnbHoqzh.js";import"./feature-detection-uS6p5jc8.js";import"./focus-indicator-IWpzSXYP.js";import"./constants-wOq9K3uV.js";import"./base-adapter-Mla2Q9YN.js";import"./utils-CRxrUqQD.js";import"./index-CiLSBptl.js";import"./card-CPFjjSKo.js";import"./base-lit-element-DK0Kll3E.js";function i(n){const o={h2:"h2",p:"p",...r(),...n.components};return t.jsxs(t.Fragment,{children:[t.jsx(e,{of:m}),`
+`,t.jsx(a,{}),`
+`,t.jsx(o.p,{children:`Focus indicators (also referred to as focus rings) are a visual indicator that highlights the element that currently has keyboard focus.
+This is important for users who navigate the web using a keyboard or other assistive technology.`}),`
+`,t.jsx(o.p,{children:`Focus indicators are building block components that are usually composed together within the internals of other components that allow
+for interactivity, such as buttons, text fields, sliders... etc.`}),`
+`,t.jsx(s,{of:p}),`
+`,t.jsx(o.h2,{id:"with-cards",children:"With Cards"}),`
+`,t.jsx(o.p,{children:"A common design pattern is an interactive card with a focus indicator that highlights the card when it receives focus."}),`
+`,t.jsx(s,{of:l}),`
+`,t.jsx(o.h2,{id:"api",children:"API"}),`
+`,t.jsx(c,{}),`
+`,t.jsx(o.h2,{id:"css-only",children:"CSS-Only"}),`
+`,t.jsx(o.p,{children:"The focus-indicator component is also available as a CSS-only component without the need for JavaScript."}),`
+`,t.jsx(s,{of:d}),`
+`,t.jsx(h,{})]})}function W(n={}){const{wrapper:o}={...r(),...n.components};return o?t.jsx(o,{...n,children:t.jsx(i,{...n})}):i(n)}export{W as default};
