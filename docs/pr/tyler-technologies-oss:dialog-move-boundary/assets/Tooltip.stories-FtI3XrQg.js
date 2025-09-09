1e334f6a9c18debf206db370f3e11f14873edaaa
DRiFTy17 <DRiFTy17@users.noreply.github.com>
2025-09-09 15:59:20 +0000

    Deploying to gh-pages from @ tyler-technologies-oss/forge@7d16772a7a3242762aac21cff1765bf5d248c5e3 🚀

---
+++ docs/pr/tyler-technologies-oss:dialog-move-boundary/assets/Tooltip.stories-FtI3XrQg.js
@@ -0,0 +1,16 @@
+import{E as m,x as f}from"./iframe-BJgJTvSF.js";import{o as u}from"./style-map-UupG38-a.js";import{g as d,O as s,e as T,a as y}from"./utils-BuPomTDA.js";import"./feature-detection-uS6p5jc8.js";import{T as t}from"./tooltip-CcxiN_AO.js";import"./overlay-D-bkGTD9.js";import"./button-r2EMLpWm.js";import"./focus-indicator-IWpzSXYP.js";import"./index-CiLSBptl.js";import"./state-layer-BFwsAUDA.js";const a="forge-tooltip",g={title:"Components/Tooltip",render:e=>{var l;const r=y(e),c=r?u(r):m;return f`
+      <forge-button id="my-button" variant="raised">Hover me</forge-button>
+      <forge-tooltip
+        anchor="my-button"
+        .open=${e.open}
+        .type=${e.type}
+        .placement=${e.placement}
+        .delay=${e.delay}
+        .offset=${e.offset}
+        .flip=${e.flip}
+        .fallbackPlacements=${(l=e.fallbackPlacements)!=null&&l.length?e.fallbackPlacements:[]}
+        .triggerType=${e.triggerType}
+        style=${c}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      </forge-tooltip>
+    `},component:a,parameters:{layout:"centered",actions:{disable:!0}},argTypes:{...d({tagName:a,exclude:["anchor","anchorElement","target","position","boundary","boundaryElement"],controls:{type:{control:"select",options:["presentation","label","description"]},placement:{control:"select",options:s},flip:{control:"select",options:T},triggerType:{control:"multi-select",options:["hover","longpress","focus"]},fallbackPlacements:{control:"multi-select",options:s}}})},args:{open:!1,type:t.defaults.TYPE,placement:t.defaults.PLACEMENT,delay:t.defaults.DELAY,offset:t.defaults.OFFSET,flip:t.defaults.FLIP,triggerType:t.defaults.TRIGGER_TYPES}},o={};var n,p,i;o.parameters={...o.parameters,docs:{...(n=o.parameters)==null?void 0:n.docs,source:{originalSource:"{}",...(i=(p=o.parameters)==null?void 0:p.docs)==null?void 0:i.source}}};const b=["Demo"],I=Object.freeze(Object.defineProperty({__proto__:null,Demo:o,__namedExportsOrder:b,default:g},Symbol.toStringTag,{value:"Module"}));export{o as D,I as T};
