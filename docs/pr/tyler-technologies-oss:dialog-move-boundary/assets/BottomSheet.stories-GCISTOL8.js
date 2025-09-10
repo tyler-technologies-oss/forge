00d173590a4e9b6fa5b951b58e962d0203a223a8
DRiFTy17 <DRiFTy17@users.noreply.github.com>
2025-09-10 19:04:44 +0000

    Deploying to gh-pages from @ tyler-technologies-oss/forge@0f7753f4c0130a4955ecddabba7232e3c1cd6527 🚀

---
+++ docs/pr/tyler-technologies-oss:dialog-move-boundary/assets/BottomSheet.stories-GCISTOL8.js
@@ -0,0 +1,26 @@
+import{E as c,x as d}from"./iframe-BBIpcTp1.js";import{g as f,a as u}from"./utils-DdTQ0mv3.js";import{s as g}from"./decorators-BfeqqAiL.js";import{o as b}from"./style-map-BOfes_n7.js";import{n as h,e as y}from"./ref-DatYGz8K.js";import"./feature-detection-uS6p5jc8.js";import"./bottom-sheet-DnPc6oFc.js";import"./dialog-5FLDrlCd.js";import"./backdrop-BZvWLwDX.js";import"./button-r2EMLpWm.js";import"./focus-indicator-IWpzSXYP.js";import"./index-CiLSBptl.js";import"./state-layer-BFwsAUDA.js";import"./toolbar-Byb6kcao.js";const s="forge-bottom-sheet",S={title:"Components/Bottom Sheet",render:e=>{const r=u(e),l=r?b(r):c,t=y();function m(){t.value.open=!t.value.open}function p(){t.value.open=!1}return d`
+      <forge-button variant="raised" @click=${m}>Show Bottom Sheet</forge-button>
+      <forge-bottom-sheet
+        ${h(t)}
+        aria-labelledby="title"
+        aria-describedby="message"
+        .open=${e.open}
+        .mode=${e.mode}
+        .persistent=${e.persistent}
+        .fullscreen=${e.fullscreen}
+        aria-labelledby="title"
+        aria-describedby="message"
+        style=${l}>
+        <div class="content">
+          <h2 id="title">Bottom Sheet Title</h2>
+          <p id="message">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nec urna et felis.</p>
+        </div>
+        <forge-toolbar>
+          <forge-button slot="end" @click=${p}>Close</forge-button>
+        </forge-toolbar>
+      </forge-bottom-sheet>
+    `},component:s,decorators:[g(`
+    .content {
+      padding: var(--forge-spacing-medium);
+    }
+  `)],argTypes:{...f({tagName:s,controls:{mode:{control:"select",options:["modal","inline-modal","nonmodal"]}}})},args:{open:!1,mode:"modal",persistent:!1,fullscreen:!1}},o={};var n,a,i;o.parameters={...o.parameters,docs:{...(n=o.parameters)==null?void 0:n.docs,source:{originalSource:"{}",...(i=(a=o.parameters)==null?void 0:a.docs)==null?void 0:i.source}}};const v=["Demo"],z=Object.freeze(Object.defineProperty({__proto__:null,Demo:o,__namedExportsOrder:v,default:S},Symbol.toStringTag,{value:"Module"}));export{z as B,o as D};
---
+++ docs/pr/tyler-technologies-oss:dialog-move-boundary/assets/Calendar-BYAA9HJ1.js
@@ -0,0 +1,14 @@
+import{j as e,M as r,T as s,C as a}from"./blocks-B0mVbcMk.js";import{useMDXComponents as i}from"./index-DhkgItXa.js";import{C as l}from"./CustomArgTypes-C4tsofsZ.js";import{C as m,D as d}from"./Calendar.stories-3fWIfS6y.js";import"./iframe-BBIpcTp1.js";import"./_commonjsHelpers-CqkleIqs.js";import"./utils-DdTQ0mv3.js";import"./feature-detection-uS6p5jc8.js";import"./calendar-Buw9wM2z.js";import"./constants-wOq9K3uV.js";import"./base-adapter-Mla2Q9YN.js";import"./icon-B8CdcxqJ.js";import"./index-CiLSBptl.js";import"./tooltip-CcxiN_AO.js";import"./utils-CRxrUqQD.js";import"./overlay-D-bkGTD9.js";import"./with-longpress-listener-CGXzI-TM.js";import"./dismissible-stack-BdWcv7_4.js";import"./with-default-aria-CUUWGrPB.js";import"./a11y-utils-fzPGYZJ6.js";import"./focus-indicator-IWpzSXYP.js";import"./state-layer-BFwsAUDA.js";import"./event-utils-C1SDeUaq.js";import"./button-r2EMLpWm.js";import"./base-button-adapter-D0zqnDMQ.js";import"./with-label-aware-D31hYnqk.js";import"./icon-button-DkluvO-9.js";function o(n){const t={a:"a",h2:"h2",li:"li",p:"p",ul:"ul",...i(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(r,{of:m}),`
+`,e.jsx(s,{}),`
+`,e.jsx(t.p,{children:`The calendar component is used for selecting dates or displaying data related to dates in a small calendar format. It is commonly used in date pickers, date range pickers,
+as well as standalone.`}),`
+`,e.jsxs(t.p,{children:[`This is not a full-featured calendar component and it's not intended to be. The goal of this component is to be used in simple scenarios to allow for users to select dates or
+data ranges, or for displaying basic events on important dates. For more complex scenarios, consider using a library such as `,e.jsx(t.a,{href:"https://fullcalendar.io/",rel:"nofollow",children:"FullCalendar"}),"."]}),`
+`,e.jsx(a,{of:d}),`
+`,e.jsx(t.h2,{id:"api",children:"API"}),`
+`,e.jsx(l,{}),`
+`,e.jsx(t.h2,{id:"accessibility",children:"Accessibility"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsx(t.li,{children:"Ensure that the days can be navigated by keyboard."}),`
+`,e.jsx(t.li,{children:"Ensure that each day is properly announced via screen reader, including its selected state."}),`
+`]})]})}function q(n={}){const{wrapper:t}={...i(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o(n)}export{q as default};
