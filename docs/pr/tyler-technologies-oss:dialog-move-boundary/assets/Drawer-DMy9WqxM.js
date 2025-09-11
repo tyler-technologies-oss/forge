0d3a18c5edc0e6496d022e34b6ec06c875e9dc35
DRiFTy17 <DRiFTy17@users.noreply.github.com>
2025-09-11 12:18:24 +0000

    Deploying to gh-pages from @ tyler-technologies-oss/forge@502095e47c546c60bf59b956312c295b5f7e7ee7 🚀

---
+++ docs/pr/tyler-technologies-oss:dialog-move-boundary/assets/Drawer-DMy9WqxM.js
@@ -0,0 +1,18 @@
+import{j as o,M as s,T as p,C as i}from"./blocks-D0yBO_jN.js";import{useMDXComponents as n}from"./index-BdO0fnZl.js";import{C as m}from"./CustomArgTypes-CT0geJaP.js";import{C as a}from"./CssOnlyInformation-gUqGSPm-.js";import{D as c,a as d,C as l}from"./Drawer.stories-Fyh5z9dA.js";import"./iframe-Dk6FLRDb.js";import"./_commonjsHelpers-CqkleIqs.js";import"./utils-QLgq-F6h.js";import"./icon-B8CdcxqJ.js";import"./constants-wOq9K3uV.js";import"./feature-detection-uS6p5jc8.js";import"./base-adapter-Mla2Q9YN.js";import"./index-CiLSBptl.js";import"./ref-BqNYG9I-.js";import"./directive-CJw_OlP2.js";import"./drawer-BllaMlBA.js";import"./base-drawer-DcDVrIqr.js";import"./event-utils-zQ4FLDwK.js";import"./utils-f75LITG3.js";import"./list-BoYqkH2U.js";import"./state-layer-CDmGOVud.js";import"./focus-indicator-13Sfphtk.js";import"./with-default-aria-CUUWGrPB.js";import"./a11y-utils-fzPGYZJ6.js";import"./toolbar-Byb6kcao.js";import"./scaffold-CGyusmPL.js";import"./card-BmuWhvgj.js";import"./base-lit-element-CrUUnKFH.js";import"./app-bar-profile-button-DqvvmGNO.js";import"./icon-button-CvGVrMJL.js";import"./base-button-adapter-C_PsGoIF.js";import"./with-label-aware-D31hYnqk.js";import"./tooltip-BZNSu0mZ.js";import"./overlay-Dg-i6Kxe.js";import"./with-longpress-listener-CGXzI-TM.js";import"./dismissible-stack-CrNbqK-n.js";import"./badge-CH-ibTj3.js";import"./menu-COBLhr1H.js";import"./list-dropdown-aware-core-D4TDs8eC.js";import"./list-dropdown-dkmdW-J-.js";import"./event-utils-C1SDeUaq.js";import"./linear-progress-2PahUgVv.js";import"./popover-DCwSavHj.js";import"./skeleton-C4EH8VF8.js";import"./a11y-BxM9_46k.js";import"./base-component-delegate-CN_SSLUw.js";import"./avatar-USdFneRN.js";import"./state-BaWhT1Bd.js";import"./style-map-DxxfczF-.js";import"./class-map-CFV1iACR.js";import"./avatar-constants-jPVIytDz.js";import"./button-BApUEgZW.js";function e(t){const r={code:"code",h2:"h2",li:"li",p:"p",strong:"strong",ul:"ul",...n(),...t.components};return o.jsxs(o.Fragment,{children:[o.jsx(s,{of:c}),`
+`,o.jsx(p,{}),`
+`,o.jsx(r.p,{children:"Drawers are used to display content that is not a primary focus of the page. They can be used for navigation, settings, or other secondary content."}),`
+`,o.jsx(r.p,{children:"There are two types of drawers:"}),`
+`,o.jsxs(r.ul,{children:[`
+`,o.jsxs(r.li,{children:[o.jsx(r.strong,{children:"dismissible:"})," can be hidden by the user via a button provided by the developer"]}),`
+`,o.jsxs(r.li,{children:[o.jsx(r.strong,{children:"permanent:"})," always visible"]}),`
+`]}),`
+`,o.jsx(i,{of:d}),`
+`,o.jsx(r.h2,{id:"api",children:"API"}),`
+`,o.jsx(m,{}),`
+`,o.jsx(r.h2,{id:"accessibility",children:"Accessibility"}),`
+`,o.jsxs(r.p,{children:["The drawer component does ",o.jsx(r.strong,{children:"not"})," provide any semantics by default. Developers should provide their own ",o.jsx(r.code,{children:"<aside>"}),` for navigation,
+or various ARIA roles in other scenarios.`]}),`
+`,o.jsx(r.h2,{id:"css-only",children:"CSS-Only"}),`
+`,o.jsx(r.p,{children:"The drawer component is also available as a CSS-only component without the need for JavaScript."}),`
+`,o.jsx(i,{of:l}),`
+`,o.jsx(a,{})]})}function lo(t={}){const{wrapper:r}={...n(),...t.components};return r?o.jsx(r,{...t,children:o.jsx(e,{...t})}):e(t)}export{lo as default};
