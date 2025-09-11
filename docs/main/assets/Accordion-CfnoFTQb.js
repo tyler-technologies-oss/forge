c29adec569cde89412e75c1eee192f08580f1c08
DRiFTy17 <nicholskb17@gmail.com>
2025-09-11 15:06:42 +0000

    Deploying to gh-pages from @ tyler-technologies-oss/forge@999eaebf673b3ae856d54c43e47b408b11fa6d38 🚀

---
+++ docs/main/assets/Accordion-CfnoFTQb.js
@@ -0,0 +1,20 @@
+import{j as e,M as s,T as r,C as a}from"./blocks-DYlyVCuO.js";import{useMDXComponents as i}from"./index-CXqlKZ2V.js";import{C as l}from"./CustomArgTypes--1sc8o-V.js";import{A as c,D as p}from"./Accordion.stories-D-FS9vhD.js";import"./iframe-DP5aPN1v.js";import"./_commonjsHelpers-CqkleIqs.js";import"./utils-BMhZCC5f.js";import"./decorators-CqoV9quD.js";import"./feature-detection-uS6p5jc8.js";import"./accordion-9V_eyO_B.js";import"./constants-wOq9K3uV.js";import"./base-adapter-Mla2Q9YN.js";import"./expansion-panel-CTn5fG9B.js";import"./icon-B8CdcxqJ.js";import"./index-CiLSBptl.js";import"./utils-BtvMddIW.js";import"./divider-DoNAUeHX.js";function t(o){const n={code:"code",h2:"h2",li:"li",p:"p",ul:"ul",...i(),...o.components};return e.jsxs(e.Fragment,{children:[e.jsx(s,{of:c}),`
+`,e.jsx(r,{}),`
+`,e.jsxs(n.p,{children:["This component does not provide any visual appearance. It is just an orchestrator of child ",e.jsx(n.code,{children:"<forge-expansion-panel>"})," components."]}),`
+`,e.jsxs(n.p,{children:["The ",e.jsx(n.code,{children:"<forge-accordion>"}),` will ensure that only one child expansion panel, at most, is open at any given time. If you need to allow for multiple panels to be opened
+simultaneously, you should opt for using just multiple `,e.jsx(n.code,{children:"<forge-expansion-panel>"})," components together without an accordion wrapper."]}),`
+`,e.jsx(n.p,{children:"See the expansion panel component for information on how to use that component separately."}),`
+`,e.jsx(a,{of:p}),`
+`,e.jsx(n.h2,{id:"api",children:"API"}),`
+`,e.jsx(l,{}),`
+`,e.jsx(n.h2,{id:"accessibility",children:"Accessibility"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:["Verify that you can tab to each panel in the accordion.",`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Ensure that there is a visual cue that the panel is selected."}),`
+`]}),`
+`]}),`
+`,e.jsx(n.li,{children:"Verify that pressing the space bar or enter key while focusing on a panel will toggle it open and closed."}),`
+`,e.jsx(n.li,{children:"If any open panel contains a link, button, or other navigation element, ensure that those tab stops are included while tabbing through the accordion."}),`
+`,e.jsx(n.li,{children:"If any closed panel contains a link, button, or other navigation element, ensure those tab stops are skipped while tabbing through the accordion."}),`
+`]})]})}function T(o={}){const{wrapper:n}={...i(),...o.components};return n?e.jsx(n,{...o,children:e.jsx(t,{...o})}):t(o)}export{T as default};
---
+++ docs/main/assets/Slider-D1I5R6Pv.js
@@ -0,0 +1,15 @@
+import{j as e,M as i,T as a,C as t}from"./blocks-DYlyVCuO.js";import{useMDXComponents as o}from"./index-CXqlKZ2V.js";import{C as l}from"./CustomArgTypes--1sc8o-V.js";import{S as c,D as d,R as m}from"./Slider.stories-DVD9JEjs.js";import"./iframe-DP5aPN1v.js";import"./_commonjsHelpers-CqkleIqs.js";import"./utils-BMhZCC5f.js";import"./decorators-CqoV9quD.js";import"./feature-detection-uS6p5jc8.js";import"./slider-B2_itZtp.js";import"./constants-wOq9K3uV.js";import"./base-adapter-Mla2Q9YN.js";import"./focus-indicator-BeibAi2h.js";import"./utils-BtvMddIW.js";import"./index-CiLSBptl.js";import"./state-layer-C7sW6v-0.js";function n(s){const r={code:"code",h2:"h2",li:"li",p:"p",ul:"ul",...o(),...s.components};return e.jsxs(e.Fragment,{children:[e.jsx(i,{of:c}),`
+`,e.jsx(a,{}),`
+`,e.jsx(r.p,{children:"Sliders are used to allow users to select a value from a range of values."}),`
+`,e.jsx(t,{of:d}),`
+`,e.jsx(r.h2,{id:"range",children:"Range"}),`
+`,e.jsxs(r.p,{children:["The ",e.jsx(r.code,{children:"range"})," property allows for a start and end value to be set."]}),`
+`,e.jsx(t,{of:m}),`
+`,e.jsx(r.h2,{id:"api",children:"API"}),`
+`,e.jsx(l,{}),`
+`,e.jsx(r.h2,{id:"accessibility",children:"Accessibility"}),`
+`,e.jsxs(r.ul,{children:[`
+`,e.jsx(r.li,{children:"Ensure that all of the controls that are accessible by a mouse are also accessible by keyboard."}),`
+`,e.jsx(r.li,{children:"Ensure the slider controls are reachable by the tab key."}),`
+`,e.jsx(r.li,{children:"Ensure each slider control can be updated by pressing the left-arrow and right-arrow keys."}),`
+`]})]})}function S(s={}){const{wrapper:r}={...o(),...s.components};return r?e.jsx(r,{...s,children:e.jsx(n,{...s})}):n(s)}export{S as default};
