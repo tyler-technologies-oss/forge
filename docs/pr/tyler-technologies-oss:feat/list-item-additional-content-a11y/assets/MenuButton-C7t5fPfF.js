ae32fe87359ce99a7450410b0fd635197b6fba22
DRiFTy17 <DRiFTy17@users.noreply.github.com>
2025-09-18 14:59:45 +0000

    Deploying to gh-pages from @ tyler-technologies-oss/forge@0ca95c5510d99b17440f582f9e736b06fc838d40 🚀

---
+++ docs/pr/tyler-technologies-oss:feat/list-item-additional-content-a11y/assets/MenuButton-C7t5fPfF.js
@@ -0,0 +1,17 @@
+import{j as t,M as n,T as s,C as p}from"./blocks-Baka_L3a.js";import{useMDXComponents as r}from"./index-ZxMoIkYY.js";import{C as m}from"./CustomArgTypes-CZvTMphG.js";import{M as c,D as a}from"./MenuButton.stories-dYw0mEYv.js";import"./iframe-DFSP4Zee.js";import"./_commonjsHelpers-CqkleIqs.js";import"./utils-dHcGLBTV.js";import"./feature-detection-uS6p5jc8.js";import"./app-bar-profile-button-D6AcDina.js";import"./constants-wOq9K3uV.js";import"./base-adapter-Mla2Q9YN.js";import"./utils-BtvMddIW.js";import"./state-layer-C7sW6v-0.js";import"./focus-indicator-BeibAi2h.js";import"./index-CiLSBptl.js";import"./icon-B8CdcxqJ.js";import"./icon-button-C-UNXlAt.js";import"./base-button-adapter-DUGDd8bj.js";import"./with-label-aware-D31hYnqk.js";import"./with-default-aria-CUUWGrPB.js";import"./a11y-utils-fzPGYZJ6.js";import"./tooltip-Chj_0PH7.js";import"./overlay-CewVvJzX.js";import"./with-longpress-listener-CGXzI-TM.js";import"./dismissible-stack-D-1uOlnz.js";import"./badge-Bc4guVN3.js";import"./base-lit-element-KE2IWGvB.js";import"./menu-CgaJinUS.js";import"./list-CaA17a8j.js";import"./event-utils-zQ4FLDwK.js";import"./list-dropdown-aware-core-xDqSDXyy.js";import"./list-dropdown-BSC3euq9.js";import"./event-utils-C1SDeUaq.js";import"./linear-progress-2PahUgVv.js";import"./popover-CjB4Fwlr.js";import"./skeleton-C4EH8VF8.js";import"./a11y-BxM9_46k.js";import"./base-component-delegate-CN_SSLUw.js";import"./avatar-D6xbgObL.js";import"./state-5dsHz1dt.js";import"./style-map-HDcfxDtV.js";import"./directive-CJw_OlP2.js";import"./class-map-Cce_pN0U.js";import"./avatar-constants-jPVIytDz.js";import"./button-DEhPRUdY.js";import"./toolbar-Byb6kcao.js";import"./expansion-panel-CTn5fG9B.js";import"./divider-DoNAUeHX.js";function e(i){const o={blockquote:"blockquote",code:"code",h2:"h2",li:"li",p:"p",ul:"ul",...r(),...i.components};return t.jsxs(t.Fragment,{children:[t.jsx(n,{of:c}),`
+`,t.jsx(s,{}),`
+`,t.jsxs(o.p,{children:["For convenience Forge provides the ",t.jsx(o.code,{children:"<forge-app-bar-menu-button>"})," component. This component uses an icon-button with a predefined icon. The power of this component is that it will ensure a consistent experience for users across all applications that consume it."]}),`
+`,t.jsxs(o.blockquote,{children:[`
+`,t.jsxs(o.p,{children:["It is expected that you place this component in the ",t.jsx(o.code,{children:"start"})," slot of the app-bar."]}),`
+`]}),`
+`,t.jsx(o.h2,{id:"default",children:"Default"}),`
+`,t.jsx(p,{of:a}),`
+`,t.jsxs(o.blockquote,{children:[`
+`,t.jsxs(o.p,{children:["You will listen for click events on the ",t.jsx(o.code,{children:"<forge-app-bar-menu-button>"})," element as if it were a native ",t.jsx(o.code,{children:"<button>"}),". You can then react to that event to toggle the navigation drawer visibility."]}),`
+`]}),`
+`,t.jsx(o.h2,{id:"api",children:"API"}),`
+`,t.jsx(m,{}),`
+`,t.jsx(o.h2,{id:"accessibility",children:"Accessibility"}),`
+`,t.jsxs(o.ul,{children:[`
+`,t.jsx(o.li,{children:"The component will handle adding necessary ARIA attributes for you, as well as ensure the proper color contrast ratio is used."}),`
+`]})]})}function nt(i={}){const{wrapper:o}={...r(),...i.components};return o?t.jsx(o,{...i,children:t.jsx(e,{...i})}):e(i)}export{nt as default};
