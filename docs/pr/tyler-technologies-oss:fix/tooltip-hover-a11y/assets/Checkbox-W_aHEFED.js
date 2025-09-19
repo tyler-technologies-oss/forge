d478a0838206fbecb246a4eaf95f04b4f9a03378
DRiFTy17 <DRiFTy17@users.noreply.github.com>
2025-09-19 15:51:37 +0000

    Deploying to gh-pages from @ tyler-technologies-oss/forge@540ab0a80cccc94e57286314c5d00d05de05015e 🚀

---
+++ docs/pr/tyler-technologies-oss:fix/tooltip-hover-a11y/assets/Checkbox-W_aHEFED.js
@@ -0,0 +1,23 @@
+import{j as e,M as c,T as r,C as s}from"./blocks-Cjq-vBL4.js";import{useMDXComponents as o}from"./index-BRZRUyAF.js";import{C as a}from"./CustomArgTypes-DjCh7jZ6.js";import{C as h}from"./CssOnlyInformation-BLWzk_a_.js";import{C as l,D as m,a as d}from"./Checkbox.stories-BdsBpGVt.js";import"./iframe-BwpD6VNE.js";import"./_commonjsHelpers-CqkleIqs.js";import"./utils-ByWQ95He.js";import"./class-map-CQWp9g2p.js";import"./directive-CJw_OlP2.js";import"./style-map-cqis5eGt.js";import"./feature-detection-uS6p5jc8.js";import"./checkbox-B1PtNwCH.js";import"./constants-wOq9K3uV.js";import"./base-adapter-Mla2Q9YN.js";import"./index-CiLSBptl.js";import"./with-form-associated-BeE6NBc1.js";import"./with-default-aria-CUUWGrPB.js";import"./a11y-utils-fzPGYZJ6.js";import"./with-label-aware-D31hYnqk.js";import"./focus-indicator-BeibAi2h.js";import"./utils-BtvMddIW.js";import"./state-layer-C7sW6v-0.js";function i(n){const t={h2:"h2",li:"li",p:"p",ul:"ul",...o(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(c,{of:l}),`
+`,e.jsx(r,{}),`
+`,e.jsx(t.p,{children:`Checkboxes are used when a user can select one or more options from a list. They are commonly used in forms and settings pages, but can be used in many other contexts
+to allow users to toggle between a checked and unchecked state.`}),`
+`,e.jsx(t.p,{children:"Checkboxes can also have an indeterminate state, which is useful when the state of the checkbox is dependent on the state of its children or is not yet known."}),`
+`,e.jsx(s,{of:m}),`
+`,e.jsx(t.h2,{id:"api",children:"API"}),`
+`,e.jsx(a,{}),`
+`,e.jsx(t.h2,{id:"accessibility",children:"Accessibility"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsxs(t.li,{children:["Verify that you can tab to each checkbox.",`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsx(t.li,{children:"Ensure there is a distinct visual cue that a checkbox is checked or unchecked."}),`
+`,e.jsx(t.li,{children:"Ensure that there is a distinct visual cue that a checkbox is focused."}),`
+`,e.jsx(t.li,{children:"Ensure that there is a distinct visual cue when a checkbox is disabled."}),`
+`]}),`
+`]}),`
+`,e.jsx(t.li,{children:"Verify that pressing the space bar key, while focused on a checkbox, will toggle it checked and unchecked in the same manner as it would be with a mouse click."}),`
+`]}),`
+`,e.jsx(t.h2,{id:"css-only",children:"CSS-Only"}),`
+`,e.jsx(t.p,{children:"The checkbox component is also available as a CSS-only component without the need for JavaScript."}),`
+`,e.jsx(s,{of:d}),`
+`,e.jsx(h,{})]})}function F(n={}){const{wrapper:t}={...o(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(i,{...n})}):i(n)}export{F as default};
---
+++ docs/pr/tyler-technologies-oss:fix/tooltip-hover-a11y/assets/Popover-BkhqxPnb.js
@@ -0,0 +1,36 @@
+import{j as e,M as s,T as a,C as i}from"./blocks-Cjq-vBL4.js";import{useMDXComponents as r}from"./index-BRZRUyAF.js";import{C as p}from"./CustomArgTypes-DjCh7jZ6.js";import{P as h,D as l,N as d,a as c}from"./Popover.stories-CxNGc956.js";import"./iframe-BwpD6VNE.js";import"./_commonjsHelpers-CqkleIqs.js";import"./utils-ByWQ95He.js";import"./style-map-cqis5eGt.js";import"./directive-CJw_OlP2.js";import"./ref-T-7d9-vm.js";import"./feature-detection-uS6p5jc8.js";import"./toast-bjL5K8Dp.js";import"./constants-wOq9K3uV.js";import"./base-adapter-Mla2Q9YN.js";import"./icon-B8CdcxqJ.js";import"./index-CiLSBptl.js";import"./button-DEhPRUdY.js";import"./focus-indicator-BeibAi2h.js";import"./utils-BtvMddIW.js";import"./state-layer-C7sW6v-0.js";import"./base-button-adapter-DUGDd8bj.js";import"./with-label-aware-D31hYnqk.js";import"./with-default-aria-CUUWGrPB.js";import"./a11y-utils-fzPGYZJ6.js";import"./dialog-DvvDkIKd.js";import"./backdrop-BZvWLwDX.js";import"./dismissible-stack-D-1uOlnz.js";import"./icon-button-C-UNXlAt.js";import"./overlay-CewVvJzX.js";import"./decorators-D6bdEsvH.js";import"./popover-CjB4Fwlr.js";import"./with-longpress-listener-CGXzI-TM.js";import"./scaffold-CGyusmPL.js";import"./toolbar-Byb6kcao.js";import"./text-field-CIHsEan0.js";import"./base-field-7hlMWjmp.js";import"./label-v2fBLVwj.js";import"./button-toggle-group-DWRuBNed.js";import"./with-form-associated-BeE6NBc1.js";import"./checkbox-B1PtNwCH.js";import"./switch-c2I0wwHc.js";import"./tooltip-C0vNXME6.js";import"./base-drawer-BSbu5Knl.js";import"./event-utils-zQ4FLDwK.js";import"./drawer-BX8jbqp4.js";import"./modal-drawer-DepTplO4.js";import"./mini-drawer-DjSpDxYe.js";import"./list-CkPu5vu3.js";function n(o){const t={code:"code",h2:"h2",li:"li",p:"p",strong:"strong",ul:"ul",...r(),...o.components};return e.jsxs(e.Fragment,{children:[e.jsx(s,{of:h}),`
+`,e.jsx(a,{}),`
+`,e.jsx(t.p,{children:`Popovers are used to display content on top of other content. They are used to show additional information related to the content that is currently
+displayed on the screen, and is typically anchored to a specific element or area on the screen that triggered it to open.`}),`
+`,e.jsx(i,{of:l}),`
+`,e.jsx(t.h2,{id:"semantics",children:"Semantics"}),`
+`,e.jsx(t.p,{children:`Popovers do not have any semantic meaning by default. This means that it's up to the developer to ensure that the content inside the popover is
+accessible if it needs to be. This can be done by adding the appropriate ARIA attributes to the popover itself, or the content within it.`}),`
+`,e.jsx(t.h2,{id:"popovers-vs-dialogs",children:"Popovers vs Dialogs"}),`
+`,e.jsx(t.p,{children:`There is a lot of overlap between popovers and dialogs, and it can be difficult to know when to use one over the other. Here are some guidelines to help
+you decide:`}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsxs(t.li,{children:[e.jsx(t.strong,{children:"Popovers"}),` are used to display additional information related to the content that is currently displayed on the screen. They are typically anchored to
+a specific element or area on the screen. Popovers are transient and non-modal, meaning that they do not block the user from interacting with the rest
+of the page.`]}),`
+`,e.jsxs(t.li,{children:[e.jsx(t.strong,{children:"Dialogs"}),` are used to display content that requires the user's immediate attention. They are typically modal (but can be non-modal), meaning that they
+block the user from interacting with the rest of the page until they are dismissed. Dialogs are typically used for things like confirmation messages,
+alerts, and forms.`]}),`
+`]}),`
+`,e.jsx(t.p,{children:`There are some cases where you may need to blur these lines a bit (and you can use either component), especially if the design of your application calls
+for it. Just be sure to consider the user experience and accessibility implications.`}),`
+`,e.jsx(t.h2,{id:"non-modal-popoverdialog",children:"Non-modal Popover/Dialog"}),`
+`,e.jsxs(t.p,{children:[`If you need to create a popover that behaves like a dialog (i.e. it is non-modal and does not block the user from interacting with the rest of the page),
+you can use the `,e.jsx(t.code,{children:"<forge-popover>"})," and add the proper ARIA attributes to make it behave like a dialog."]}),`
+`,e.jsx(i,{of:d}),`
+`,e.jsxs(t.p,{children:["In this example, the popover is presented with the ",e.jsx(t.code,{children:'role="dialog"'})," and ",e.jsx(t.code,{children:'aria-modal="false"'}),` attributes. This tells screen readers that the popover is a
+dialog, but it is not modal. This specific example will gracefully handle user entry into a form, by ensuring that the user does not lose valuable data
+by accidentally closing the popover.`]}),`
+`,e.jsx(t.h2,{id:"distinct-popovers",children:"Distinct Popovers"}),`
+`,e.jsxs(t.p,{children:["In some cases, typically when using the ",e.jsx(t.code,{children:'"hover"'}),` trigger type, you may want to ensure that the popover is distinct from other popovers on the page meaning
+that it can be the only popover within a specific group that is open at a time. This can be achieved by using the `,e.jsx(t.code,{children:"distinct"}),` attribute on
+the `,e.jsx(t.code,{children:"<forge-popover>"})," element. While you can use the ",e.jsx(t.code,{children:"distinct"}),` attribute on any popover, this will cause it to use a "default" group. It is most commonly
+used with a specific name or group to scope the distinct behavior to a specific set of popovers.`]}),`
+`,e.jsx(i,{of:c}),`
+`,e.jsx(t.h2,{id:"api",children:"API"}),`
+`,e.jsx(p,{})]})}function ae(o={}){const{wrapper:t}={...r(),...o.components};return t?e.jsx(t,{...o,children:e.jsx(n,{...o})}):n(o)}export{ae as default};
