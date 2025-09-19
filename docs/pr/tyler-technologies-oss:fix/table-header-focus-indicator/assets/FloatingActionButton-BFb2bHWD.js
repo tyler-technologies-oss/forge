8464a07aeed2d137bcc63ee2bbff99189184f928
DRiFTy17 <DRiFTy17@users.noreply.github.com>
2025-09-19 16:22:34 +0000

    Deploying to gh-pages from @ tyler-technologies-oss/forge@ecba2ac5a1adbacea9a318be7bc90c00b44361b1 🚀

---
+++ docs/pr/tyler-technologies-oss:fix/table-header-focus-indicator/assets/FloatingActionButton-BFb2bHWD.js
@@ -0,0 +1,33 @@
+import{j as n,M as s,T as a,C as i}from"./blocks-BvdAFB72.js";import{useMDXComponents as r}from"./index-DK8ePJIJ.js";import{C as c}from"./CustomArgTypes-DMPRNWsM.js";import{C as l}from"./CssOnlyInformation-Y45c2SzS.js";import{F as d,D as h,E as p,W as m,C as x}from"./FloatingActionButton.stories-CR5zxaje.js";import"./iframe-JvnhaxnU.js";import"./_commonjsHelpers-CqkleIqs.js";import"./utils-UeWn3HvG.js";import"./style-map-CgHzuYWO.js";import"./directive-CJw_OlP2.js";import"./class-map-BaaKyclT.js";import"./icon-B8CdcxqJ.js";import"./constants-wOq9K3uV.js";import"./feature-detection-uS6p5jc8.js";import"./base-adapter-Mla2Q9YN.js";import"./index-CiLSBptl.js";import"./floating-action-button-BZKxYF82.js";import"./base-button-adapter-DUGDd8bj.js";import"./with-label-aware-D31hYnqk.js";import"./with-default-aria-CUUWGrPB.js";import"./a11y-utils-fzPGYZJ6.js";import"./utils-BtvMddIW.js";import"./focus-indicator-BeibAi2h.js";import"./state-layer-C7sW6v-0.js";import"./button-DEhPRUdY.js";function o(t){const e={code:"code",h2:"h2",li:"li",p:"p",pre:"pre",ul:"ul",...r(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(s,{of:d}),`
+`,n.jsx(a,{}),`
+`,n.jsx(e.p,{children:"Use floating action buttons to represent the primary action on a screen within an application. It's recommended to only use one floating action button per screen."}),`
+`,n.jsx(i,{of:h}),`
+`,n.jsx(e.h2,{id:"positioning",children:"Positioning"}),`
+`,n.jsx(e.p,{children:"Typically you will position floating action buttons manually on the screen, for example to apply a fixed position in the bottom-right you could use this CSS:"}),`
+`,n.jsx(e.pre,{children:n.jsx(e.code,{className:"language-css",children:`.bottom-right {
+  position: fixed;
+  bottom: var(--forge-spacing-medium);
+  right: var(--forge-spacing-medium);
+}
+`})}),`
+`,n.jsx(e.h2,{id:"extended",children:"Extended"}),`
+`,n.jsx(e.p,{children:"Extended floating action buttons are larger and have a text label."}),`
+`,n.jsx(i,{of:p}),`
+`,n.jsx(e.h2,{id:"with-anchor",children:"With Anchor"}),`
+`,n.jsxs(e.p,{children:["You can nest an ",n.jsx(e.code,{children:"<a>"})," element inside the floating action button to create a link."]}),`
+`,n.jsx(i,{of:m}),`
+`,n.jsx(e.h2,{id:"api",children:"API"}),`
+`,n.jsx(c,{}),`
+`,n.jsx(e.h2,{id:"accessibility",children:"Accessibility"}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsxs(e.li,{children:["Buttons containing only icons should be given a meaningful label via ",n.jsx(e.code,{children:"aria-label"})," or ",n.jsx(e.code,{children:"aria-labelledby"}),"."]}),`
+`,n.jsxs(e.li,{children:["Avoid using capitalized text because screen readers will read the text character-by-character. Instead use ",n.jsx(e.code,{children:"text-transform: uppercase"}),"."]}),`
+`,n.jsx(e.li,{children:"Ensure the FAB can be reached by keyboard navigation."}),`
+`,n.jsx(e.li,{children:"Ensure that there is a distinct visual cue when the FAB is in focus."}),`
+`,n.jsx(e.li,{children:"Verify that there is sufficient contrast between the foreground text and background to meet WCAG requirements."}),`
+`,n.jsx(e.li,{children:"Ensure that buttons placed above other content on the page have proper contrast ratio."}),`
+`]}),`
+`,n.jsx(e.h2,{id:"css-only",children:"CSS-Only"}),`
+`,n.jsx(e.p,{children:"The floating action button component is also available as a CSS-only component without the need for JavaScript."}),`
+`,n.jsx(i,{of:x}),`
+`,n.jsx(l,{})]})}function z(t={}){const{wrapper:e}={...r(),...t.components};return e?n.jsx(e,{...t,children:n.jsx(o,{...t})}):o(t)}export{z as default};
---
+++ docs/pr/tyler-technologies-oss:fix/table-header-focus-indicator/assets/TimePicker-D_1Jrx_G.js
@@ -0,0 +1,30 @@
+import{j as e,M as r,T as s,C as l}from"./blocks-BvdAFB72.js";import{useMDXComponents as n}from"./index-DK8ePJIJ.js";import{C as c}from"./CustomArgTypes-DMPRNWsM.js";import{T as d,D as a}from"./TimePicker.stories-JY1H8z3O.js";import"./iframe-JvnhaxnU.js";import"./_commonjsHelpers-CqkleIqs.js";import"./utils-UeWn3HvG.js";import"./feature-detection-uS6p5jc8.js";import"./time-picker-CXkhYVwz.js";import"./constants-wOq9K3uV.js";import"./base-adapter-Mla2Q9YN.js";import"./icon-B8CdcxqJ.js";import"./index-CiLSBptl.js";import"./utils-BtvMddIW.js";import"./text-field-CrQVM6fF.js";import"./base-field-7hlMWjmp.js";import"./focus-indicator-BeibAi2h.js";import"./label-v2fBLVwj.js";import"./button-DEhPRUdY.js";import"./state-layer-C7sW6v-0.js";import"./base-button-adapter-DUGDd8bj.js";import"./with-label-aware-D31hYnqk.js";import"./with-default-aria-CUUWGrPB.js";import"./a11y-utils-fzPGYZJ6.js";import"./button-toggle-group-DWRuBNed.js";import"./with-form-associated-BeE6NBc1.js";import"./checkbox-B1PtNwCH.js";import"./icon-button-C-UNXlAt.js";import"./switch-c2I0wwHc.js";import"./tooltip-BqLKJMIV.js";import"./overlay-CewVvJzX.js";import"./with-longpress-listener-CGXzI-TM.js";import"./dismissible-stack-D-1uOlnz.js";import"./list-dropdown-DIklKypy.js";import"./event-utils-C1SDeUaq.js";import"./linear-progress-2PahUgVv.js";import"./list-CkPu5vu3.js";import"./event-utils-zQ4FLDwK.js";import"./popover-CjB4Fwlr.js";import"./skeleton-C4EH8VF8.js";import"./a11y-BxM9_46k.js";import"./dialog-DvvDkIKd.js";import"./backdrop-BZvWLwDX.js";function o(i){const t={blockquote:"blockquote",code:"code",h2:"h2",li:"li",p:"p",strong:"strong",ul:"ul",...n(),...i.components};return e.jsxs(e.Fragment,{children:[e.jsx(r,{of:d}),`
+`,e.jsx(s,{}),`
+`,e.jsx(t.p,{children:"The time picker component can be used to allow the user to enter a time manually, or to choose a time from the configurable dropdown list of suggestions. The built-in input mask is enabled by default, and will force users to enter a time in either 12 hour (default) or 24 hour formats."}),`
+`,e.jsxs(t.p,{children:["This component is composable and only requires that an ",e.jsx(t.code,{children:"<input>"})," element be provided as one of its children. It's common that the component wraps a text-field component to provide the Forge look-and-feel, but it can technically attach itself to any instance of a child ",e.jsx(t.code,{children:"<input>"})," element."]}),`
+`,e.jsxs(t.blockquote,{children:[`
+`,e.jsxs(t.p,{children:[e.jsx(t.strong,{children:"Important"}),": all communication with this component through its APIs, such as getting/setting value or listen for change events ",e.jsx(t.strong,{children:"must"}),' be in 24 hour time format. Ex. "15:30".']}),`
+`,e.jsx(t.p,{children:"This is to ensure that a compatible format is used in all locales, as well as to provide a uniform way to interact with the component."}),`
+`]}),`
+`,e.jsx(t.h2,{id:"example",children:"Example"}),`
+`,e.jsxs(t.p,{children:["Values should ",e.jsx(t.strong,{children:"always"})," be set through the ",e.jsx(t.code,{children:"<forge-time-picker>"})," element, ",e.jsx(t.strong,{children:"not"})," the ",e.jsx(t.code,{children:"<input>"})," element."]}),`
+`,e.jsxs(t.p,{children:["You can still use attributes such as ",e.jsx(t.code,{children:"placeholder"})," on the ",e.jsx(t.code,{children:"<input>"})," but things like ",e.jsx(t.code,{children:"value"}),", ",e.jsx(t.code,{children:"disabled"}),", ",e.jsx(t.code,{children:"min"}),", and ",e.jsx(t.code,{children:"max"})," should be set on the ",e.jsx(t.code,{children:"<forge-time-picker>"})," element itself."]}),`
+`,e.jsxs(t.p,{children:[e.jsx(t.strong,{children:"Note"}),": When used in a form where you want the date to be required, you must set the ",e.jsx(t.code,{children:"required"})," property on the embedded ",e.jsx(t.code,{children:"<forge-text-field>"})," element, rather than the ",e.jsx(t.code,{children:"<forge-time-picker>"})," element. This also applies to the invalid property."]}),`
+`,e.jsx(l,{of:a}),`
+`,e.jsx(t.h2,{id:"api",children:"API"}),`
+`,e.jsx(c,{}),`
+`,e.jsx(t.h2,{id:"accessibility",children:"Accessibility"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsx(t.li,{children:"When using a screen reader, ensure keyboard navigation in the dropdown list is announced."}),`
+`,e.jsxs(t.li,{children:["Be sure that you add the proper ",e.jsx(t.code,{children:"aria-label"})," to the ",e.jsx(t.code,{children:"<input>"})," element if not using a ",e.jsx(t.code,{children:"<label>"})," element with a ",e.jsx(t.code,{children:"for"})," attribute."]}),`
+`,e.jsxs(t.li,{children:["The time-picker component will add the following ARIA attributes to the ",e.jsx(t.code,{children:"<input>"})," element for you:",`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsx(t.li,{children:e.jsx(t.code,{children:"aria-live"})}),`
+`,e.jsx(t.li,{children:e.jsx(t.code,{children:"aria-atomic"})}),`
+`,e.jsx(t.li,{children:e.jsx(t.code,{children:"aria-haspopup"})}),`
+`,e.jsx(t.li,{children:e.jsx(t.code,{children:"aria-expanded"})}),`
+`,e.jsx(t.li,{children:e.jsx(t.code,{children:"aria-owns"})}),`
+`,e.jsx(t.li,{children:e.jsx(t.code,{children:"aria-disabled"})}),`
+`]}),`
+`]}),`
+`]})]})}function ee(i={}){const{wrapper:t}={...n(),...i.components};return t?e.jsx(t,{...i,children:e.jsx(o,{...i})}):o(i)}export{ee as default};
