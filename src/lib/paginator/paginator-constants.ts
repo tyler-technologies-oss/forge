d158ee56b2d16cb38eb4d0df26622981fcbf363c
Nichols, Kieran <kieran.nichols@tylertech.com>
2025-10-30 12:26:09 -0400

    feat(paginator): add interaction methods for convenience

--- src/lib/paginator/paginator-constants.ts
+++ src/lib/paginator/paginator-constants.ts
@@ -77,3 +77,13 @@ export interface IPaginatorRangeState {
 }
 
 export type PaginatorRangeLabelBuilder = ((state: IPaginatorRangeState) => string) | undefined | null;
+
+export interface IPaginatorPageInfo {
+  pageIndex: number;
+  pageSize: number;
+  total: number;
+  offset: number;
+  pageStart: number;
+  pageEnd: number;
+  totalPages: number;
+}
