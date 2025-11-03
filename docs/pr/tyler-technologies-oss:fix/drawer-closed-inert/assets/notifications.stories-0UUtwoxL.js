02a9afbc93bc9be2c1462b2d984d8ef67ce9d128
DRiFTy17 <DRiFTy17@users.noreply.github.com>
2025-11-03 22:00:00 +0000

    Deploying to gh-pages from @ tyler-technologies-oss/forge@5ff8c621e22c62988a7cd3433ac276f08f4c6617 🚀

---
+++ docs/pr/tyler-technologies-oss:fix/drawer-closed-inert/assets/notifications.stories-0UUtwoxL.js
@@ -0,0 +1,14 @@
+import{x as e}from"./iframe-9ecIVFyi.js";import{g as a,c as n,s as p}from"./utils-BfYLGoHh.js";import"./service-adapter-CffG5Lhq.js";import"./app-bar-profile-button-B4XjnHpf.js";import"./state-layer-gAgMwMHF.js";import"./focus-indicator-whDi61ik.js";import"./icon-kuXwuZAY.js";import"./index-5CPwzmQS.js";import"./menu-D1-DWff5.js";import"./linear-progress-r0Hzg69v.js";import"./list-CpdPnrIG.js";import"./popover-BWVazmya.js";import"./overlay-B5pGv-rV.js";import"./skeleton-BSiuL_ME.js";import"./avatar-DxEPP2Mh.js";import"./icon-button-BoMj15KR.js";const o="forge-app-bar-notification-button",i={title:"Components/App Bar/Notifications",render:r=>n(o,r),component:o,parameters:{actions:{disable:!0}},argTypes:{...a({tagName:o})},args:{}},t={...p,render:()=>e`
+      <forge-app-bar title-text="Menu Button">
+        <forge-app-bar-notification-button slot="end" show-badge dot> </forge-app-bar-notification-button>
+      </forge-app-bar>
+    `};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  ...standaloneStoryParams,
+  render: () => {
+    return html\`
+      <forge-app-bar title-text="Menu Button">
+        <forge-app-bar-notification-button slot="end" show-badge dot> </forge-app-bar-notification-button>
+      </forge-app-bar>
+    \`;
+  }
+}`,...t.parameters?.docs?.source}}};const s=["Demo"],E=Object.freeze(Object.defineProperty({__proto__:null,Demo:t,__namedExportsOrder:s,default:i},Symbol.toStringTag,{value:"Module"}));export{t as D,E as N};
