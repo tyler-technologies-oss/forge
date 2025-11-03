f2dbe49a1f2af93c81a276e145c9d4ff8ccc6f0b
DRiFTy17 <DRiFTy17@users.noreply.github.com>
2025-11-03 22:16:43 +0000

    Deploying to gh-pages from @ tyler-technologies-oss/forge@06d3b4f79b8700a3ae64d22bd8f55541439ca891 🚀

---
+++ docs/pr/tyler-technologies-oss:fix/state-layer-animation/assets/ColorPicker.stories-BCUB8A1M.js
@@ -0,0 +1,7 @@
+import{x as t}from"./iframe-BhvJVctQ.js";import{g as c}from"./utils-CZJYXYdK.js";import"./service-adapter-CffG5Lhq.js";import"./color-picker-BtsbfVCb.js";const{action:a}=__STORYBOOK_MODULE_ACTIONS__,r="forge-color-picker",n=a("forge-color-picker-change"),l={title:"Components/Color Picker",render:o=>t`
+      <forge-color-picker
+        .value=${o.value}
+        .allowOpacity=${o.allowOpacity}
+        .opacity=${o.opacity}
+        @forge-color-picker-change=${n}></forge-color-picker>
+    `,component:r,argTypes:{...c({tagName:r,exclude:["rgba","hsva"],controls:{opacity:{control:"number",min:0,max:1,step:.01}}})},args:{value:"#000000",opacity:1,allowOpacity:!1,debounceChangeEvent:!1}},e={};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:"{}",...e.parameters?.docs?.source}}};const p=["Demo"],u=Object.freeze(Object.defineProperty({__proto__:null,Demo:e,__namedExportsOrder:p,default:l},Symbol.toStringTag,{value:"Module"}));export{u as C,e as D};
