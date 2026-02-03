2042fd22340b88f9c18c5ce613066219a9f31b2e
Nichols, Kieran <kieran.nichols@tylertech.com>
2026-02-03 15:09:43 -0500

    chore: initial reorg to subdirectory

---
+++ packages/forge/.storybook/manager.ts
@@ -0,0 +1,17 @@
+import { addons } from 'storybook/manager-api';
+
+const banner = document.createElement('div');
+banner.classList.add('forge-docs-banner');
+banner.innerHTML =
+  '<p>You are currently viewing the latest v3.x docs. To view the legacy v2.x docs, click <a href="https://forge.tylerdev.io/version-2" target="_blank" rel="noreferrer noopener">here</a>.</p>';
+document.body.prepend(banner);
+
+addons.setConfig({
+  sidebar: {
+    showRoots: true,
+    collapsedRoots: ['Getting Started', 'Frameworks', 'Components', 'Tokens', 'About'],
+    filters: {
+      hidden: item => !item.tags?.includes('hidden')
+    }
+  }
+});
---
+++ packages/forge/src/dev/pages/icon-button/icon-button.ts
@@ -0,0 +1,47 @@
+import '$src/shared';
+import '@tylertech/forge/icon-button';
+import '@tylertech/forge/deprecated/icon-button';
+import '@tylertech/forge/label';
+import { toggleAttribute } from '@tylertech/forge-core';
+import { IconRegistry } from '@tylertech/forge/icon';
+import type { IIconButtonComponent } from '@tylertech/forge/icon-button';
+import type { ISelectComponent } from '@tylertech/forge/select';
+import type { ISwitchComponent } from '@tylertech/forge/switch';
+import { tylIconCheck, tylIconClose, tylIconFavorite, tylIconFileCopy, tylIconOpenInNew, tylIconSettings, tylIconNotifications } from '@tylertech/tyler-icons';
+import '@tylertech/forge/icon-button/forge-icon-button.scss';
+import './icon-button.scss';
+
+IconRegistry.define([tylIconFavorite, tylIconCheck, tylIconClose, tylIconOpenInNew, tylIconSettings, tylIconFileCopy, tylIconNotifications]);
+
+const allIconButtons = Array.from(document.querySelectorAll<IIconButtonComponent>('.content forge-icon-button'));
+allIconButtons.forEach(btn => btn.addEventListener('click', evt => console.log('click', evt)));
+
+const disabledToggle = document.querySelector('#opt-disabled') as ISwitchComponent;
+disabledToggle.addEventListener('forge-switch-change', ({ detail: selected }) => {
+  allIconButtons.forEach(btn => (btn.disabled = selected));
+});
+
+const denseToggle = document.querySelector('#opt-dense') as ISwitchComponent;
+denseToggle.addEventListener('forge-switch-change', ({ detail: selected }) => {
+  allIconButtons.forEach(btn => (btn.dense = selected));
+});
+
+const popoverIconToggle = document.querySelector('#opt-popover-icon') as ISwitchComponent;
+popoverIconToggle.addEventListener('forge-switch-change', ({ detail: selected }) => {
+  allIconButtons.forEach(btn => (btn.popoverIcon = selected));
+});
+
+const densitySelect = document.querySelector('#opt-density') as ISelectComponent;
+densitySelect.addEventListener('change', ({ detail }) => {
+  allIconButtons.forEach(btn => (btn.density = detail));
+});
+
+const shapeSelect = document.querySelector('#opt-shape') as ISelectComponent;
+shapeSelect.addEventListener('change', ({ detail }) => {
+  allIconButtons.forEach(btn => (btn.shape = detail));
+});
+
+const themeSelect = document.querySelector('#opt-theme') as ISelectComponent;
+themeSelect.addEventListener('change', ({ detail }) => {
+  allIconButtons.forEach(btn => toggleAttribute(btn, !!detail, 'theme', detail));
+});
