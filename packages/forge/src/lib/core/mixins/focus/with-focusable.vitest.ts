a8d88f5a9a6e42b1c70fd7398d48d3da4d8e3ab9
Nichols, Kieran <kieran.nichols@tylertech.com>
2026-02-11 16:13:51 -0500

    test(with-focusable): convert to vitest

--- packages/forge/src/lib/core/mixins/focus/with-focusable.vitest.ts
+++ packages/forge/src/lib/core/mixins/focus/with-focusable.vitest.ts
@@ -7,8 +7,9 @@
  * The original source code can be found at: [GitHub](https://github.com/material-components/material-web/blob/main/labs/behaviors/focusable_test.ts)
  */
 
-import { expect } from '@esm-bundle/chai';
-import { fixture, html } from '@open-wc/testing';
+import { describe, it, expect } from 'vitest';
+import { render } from 'vitest-browser-lit';
+import { html } from 'lit';
 import { customElement } from '@tylertech/forge-core';
 
 import { isFocusable } from '../../../constants.js';
@@ -23,55 +24,56 @@ describe('WithFocusable', () => {
     }
   }
 
-  async function setupTest(): Promise<TestFocusable> {
-    return await fixture<TestFocusable>(html`<test-focusable></test-focusable>`);
+  function setupTest(): TestFocusable {
+    const screen = render(html`<test-focusable></test-focusable>`);
+    return screen.container.querySelector('test-focusable') as TestFocusable;
   }
 
   it('should set isFocusable to true by default', async () => {
-    const element = await setupTest();
+    const element = setupTest();
 
-    expect(element[isFocusable]).to.be.true;
+    expect(element[isFocusable]).toBe(true);
   });
 
   it('should set tabindex="0" when isFocusable is true', async () => {
-    const element = await setupTest();
+    const element = setupTest();
 
-    expect(element.tabIndex).to.equal(0);
+    expect(element.tabIndex).toBe(0);
   });
 
   it('should set tabindex="-1" when isFocusable is false', async () => {
-    const element = await setupTest();
+    const element = setupTest();
 
     element[isFocusable] = false;
 
-    expect(element.tabIndex).to.equal(-1);
+    expect(element.tabIndex).toBe(-1);
   });
 
   it('should not override user-set tabindex="0" when isFocusable is false', async () => {
-    const element = await setupTest();
+    const element = setupTest();
 
     element[isFocusable] = false;
     element.tabIndex = 0;
 
-    expect(element[isFocusable]).to.be.false;
-    expect(element.tabIndex).to.equal(0);
+    expect(element[isFocusable]).toBe(false);
+    expect(element.tabIndex).toBe(0);
   });
 
   it('should not override user-set tabindex="-1" when isFocusable is true', async () => {
-    const element = await setupTest();
+    const element = setupTest();
 
     element.tabIndex = -1;
 
-    expect(element[isFocusable]).to.be.true;
-    expect(element.tabIndex).to.equal(-1);
+    expect(element[isFocusable]).toBe(true);
+    expect(element.tabIndex).toBe(-1);
   });
 
   it('should restore default tabindex when user-set tabindex attribute is removed', async () => {
-    const element = await setupTest();
+    const element = setupTest();
 
     element.tabIndex = -1;
     element.removeAttribute('tabindex');
 
-    expect(element.tabIndex).to.equal(0);
+    expect(element.tabIndex).toBe(0);
   });
 });
