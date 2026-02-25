0b9dc60a098c8adbc9f5c0210254e5ff30ddb7d8
Sam Richardson <sam.richardson@tylertech.com>
2026-02-25 16:18:12 -0500

    feat(icon): migrate to lit

--- packages/forge/src/lib/icon/index.ts
+++ packages/forge/src/lib/icon/index.ts
@@ -1,9 +1,7 @@
 import { defineCustomElement } from '@tylertech/forge-core';
 import { IconComponent } from './icon.js';
 
-export * from './icon-adapter.js';
 export * from './icon-constants.js';
-export * from './icon-core.js';
 export * from './icon-registry.js';
 export * from './icon-utils.js';
 export * from './icon.js';
