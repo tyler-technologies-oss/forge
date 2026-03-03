4aac30cd0bd0cfa4de30736f3c09b5372d52bdb3
Sam Richardson <sam.richardson@tylertech.com>
2026-03-03 14:32:20 -0500

    chore(accordion): deprecate define function

--- packages/forge/src/lib/accordion/index.ts
+++ packages/forge/src/lib/accordion/index.ts
@@ -5,6 +5,7 @@ import { AccordionComponent } from './accordion.js';
 export * from './accordion-constants.js';
 export * from './accordion.js';
 
+/** @deprecated Definition functions are deprecated and replaced with side effect imports (`import '@tylertech/forge/accordion'`). */
 export function defineAccordionComponent(): void {
   defineCustomElement(AccordionComponent);
 }
