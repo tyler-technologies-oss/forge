7050cf67c603fb2ebb3f0b96e6f80a6e930ccc30
Sam Richardson <sam.richardson@tylertech.com>
2026-03-03 17:27:22 -0500

    chore(expansion-panel): deprecate define function

--- packages/forge/src/lib/expansion-panel/index.ts
+++ packages/forge/src/lib/expansion-panel/index.ts
@@ -5,6 +5,7 @@ import { ExpansionPanelComponent } from './expansion-panel.js';
 export * from './expansion-panel-constants.js';
 export * from './expansion-panel.js';
 
+/** @deprecated Definition functions are deprecated and replaced with side effect imports (`import '@tylertech/forge/expansion-panel'`). */
 export function defineExpansionPanelComponent(): void {
   defineCustomElement(ExpansionPanelComponent);
 }
