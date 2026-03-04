b98a834f67e131c5c1449236bc598ab5b8a5687d
Sam Richardson <sam.richardson@tylertech.com>
2026-03-03 22:43:47 -0500

    chore(app-bar): deprecate define function

--- packages/forge/src/lib/app-bar/app-bar/index.ts
+++ packages/forge/src/lib/app-bar/app-bar/index.ts
@@ -4,6 +4,7 @@ import { AppBarComponent } from './app-bar.js';
 export * from './app-bar-constants.js';
 export * from './app-bar.js';
 
+/** @deprecated Definition functions are deprecated and replaced with side effect imports (`import '@tylertech/forge/app-bar'`). */
 export function defineAppBarComponent(): void {
   defineCustomElement(AppBarComponent);
 }
