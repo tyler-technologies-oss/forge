70d16119dc428d751a0795d52c946b5814de7eff
Nichols, Kieran <kieran.nichols@tylertech.com>
2026-05-22 10:39:15 -0400

    fix(data-table): fix header cell content alignment & overflow handling

--- packages/forge/src/lib/data-table/data-table.ts
+++ packages/forge/src/lib/data-table/data-table.ts
@@ -448,7 +448,7 @@ export class DataTableElement<TData extends Row = unknown> extends LitElement {
                       : canBeDragged
                         ? (evt: MouseEvent) => this.#onHeaderClick(evt, header.column)
                         : null}>
-                    ${header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
+                    <span class="cell-text">${header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}</span>
                     ${this.sortable && header.column.getIsSorted()
                       ? svg`<svg class="sort-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M20 12l-1.41-1.41L13 16.17V4h-2v12.17l-5.58-5.59L4 12l8 8 8-8z"/></svg>`
                       : null}
