c8fd20013d3784a2f49c8f256b38b8f1665f48c0
geraltofrivendell <aml.hancock@gmail.com>
2026-05-29 15:37:32 -0400

    refactor(toolbar): use class-based styling instead of reflected attribute

--- packages/forge/src/lib/toolbar/toolbar.ts
+++ packages/forge/src/lib/toolbar/toolbar.ts
@@ -59,7 +59,7 @@ export const TOOLBAR_TAG_NAME: keyof HTMLElementTagNameMap = 'forge-toolbar';
  * @slot after-end - The content to place after the end slot.
  *
  * @cssclass forge-toolbar - Apply to the root element _(required)_.
- * @cssclass forge-toolbar--inverted - Inverts the toolbar so the divider is at the top.
+ * @cssclass inverted - Inverts the toolbar so the divider is at the top.
  */
 @customElement(TOOLBAR_TAG_NAME)
 export class ToolbarComponent extends BaseLitElement {
@@ -76,7 +76,7 @@ export class ToolbarComponent extends BaseLitElement {
   public render(): TemplateResult {
     const classes = {
       'forge-toolbar': true,
-      'forge-toolbar--inverted': this.inverted
+      inverted: this.inverted
     };
     return html`<div class=${classMap(classes)} part="root">
       <div class="section" part="before-section-start">
