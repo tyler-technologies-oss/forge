fcfed0e1b17f0832836bdbadff3a1dfc4e2fd607
Sam Richardson <sam.richardson@tylertech.com>
2026-07-07 12:40:30 -0400

    feat(timeline): add layout slots

--- packages/forge/src/lib/timeline/timeline-item/timeline-item.ts
+++ packages/forge/src/lib/timeline/timeline-item/timeline-item.ts
@@ -5,8 +5,12 @@ import { classMap } from 'lit/directives/class-map.js';
 import { Theme } from '../../constants.js';
 import { BaseLitElement } from '../../core/base/base-lit-element.js';
 import { setDefaultAria } from '../../core/utils/a11y-utils.js';
+import { hideWhenEmpty } from '@tylertech/forge/core/utils/lit-utils.js';
 
 import styles from './timeline-item.scss';
+
+export type TimelineSidebarPosition = 'auto' | 'start' | 'end' | 'both' | 'none';
+export type TimelineDetailVariant = 'default' | 'card';
 
 export const TIMELINE_ITEM_TAG_NAME: keyof HTMLElementTagNameMap = 'forge-timeline-item';
 
@@ -17,24 +21,34 @@ export const TIMELINE_ITEM_TAG_NAME: keyof HTMLElementTagNameMap = 'forge-timeli
  *
  * @slot - The default slot for the content.
  * @slot marker - The marker to display (typically an icon or badge).
+ * @slot start - The content to display at the start of the summary.
+ * @slot center - The content to display in the center of the summary.
+ * @slot end - The content to display at the end of the summary.
+ * @slot detail - The content to display under the summary.
  *
  * @cssproperty --forge-timeline-item-marker-size - The size of the slotted icon marker.
- * @cssproperty --forge-timeline-item-marker-padding - Space around the marker that is filled with the background color.
+ * @cssproperty --forge-timeline-item-marker-padding - The space around the marker that is filled with the background color.
  * @cssproperty --forge-timeline-item-marker-color - The color of the marker.
  * @cssproperty --forge-timeline-item-marker-background - The background color of the marker.
  * @cssproperty --forge-timeline-item-marker-shape - The corner radius of the marker.
  * @cssproperty --forge-timeline-item-default-marker-size - The size of the default marker displayed when no slotted marker is provided.
  * @cssproperty --forge-timeline-item-default-marker-shape - The corner radius of the default marker displayed when no slotted marker is provided.
- * @cssproperty --forge-timeline-item-default-marker-background - The background color of the default marker.
- * @cssproperty --forge-timeline-item-gap - The spacing between the marker and content.
- * @cssproperty --forge-timeline-item-line-gap - The spacing between the marker and the line.
+ * @cssproperty --forge-timeline-item-sidebar-margin - The spacing between the marker and content.
  * @cssproperty --forge-timeline-item-line-color - The color of the line drawn between timeline items.
  * @cssproperty --forge-timeline-item-line-width - The width of the line drawn between timeline items.
+ * @cssproperty --forge-timeline-item-line-inset - The distance from the left edge of the timeline item to the line drawn between timeline items.
+ * @cssproperty --forge-timeline-item-summary-gap - The spacing between the start, center, and end slots.
+ * @cssproperty --forge-timeline-item-detail-margin - The spacing between the summary and detail sections.
+ * @cssproperty --forge-timeline-item-detail-card-padding - The padding of the detail section when the detail-variant is set to "card".
  *
  * @csspart root - The root element.
  * @csspart marker - The marker element.
  * @csspart default-marker - The default marker element.
- * @csspart content - The content element.
+ * @csspart summary - The element displayed inline with the marker.
+ * @csspart start - The element displayed in the start slot.
+ * @csspart center - The element displayed in the default slot.
+ * @csspart end - The element displayed in the end slot.
+ * @csspart detail - The element displayed in the detail slot.
  */
 @customElement(TIMELINE_ITEM_TAG_NAME)
 export class TimelineItemComponent extends BaseLitElement {
@@ -51,6 +65,22 @@ export class TimelineItemComponent extends BaseLitElement {
   @property()
   public theme: Theme | '' = '';
 
+  /**
+   * Which parts of the sidebar line should appear.
+   * @default 'auto'
+   * @attribute
+   */
+  @property()
+  public sidebar: TimelineSidebarPosition = 'auto';
+
+  /**
+   * Whether the detail section displays in a card or not.
+   * @default 'default'
+   * @attribute detail-variant
+   */
+  @property({ attribute: 'detail-variant' })
+  public detailVariant: TimelineDetailVariant = 'default';
+
   #internals: ElementInternals;
 
   constructor() {
@@ -66,21 +96,28 @@ export class TimelineItemComponent extends BaseLitElement {
   }
 
   public render(): TemplateResult {
-    const classes = {
-      'forge-timeline-item': true,
-      [this.theme]: !!this.theme
-    };
     return html`
-      <div part="root" class=${classMap(classes)}>
-        <div class="marker-wrapper">
-          <div part="marker" class="marker">
+      <div part="root" class=${classMap({ 'forge-timeline-item': true, [this.theme]: !!this.theme })}>
+        <div class="sidebar">
+          <div part="marker" class=${classMap({ marker: true, [`sidebar-${this.sidebar}`]: !!this.sidebar })}>
             <slot name="marker">
               <div part="default-marker" class="default-marker"></div>
             </slot>
           </div>
         </div>
-        <div part="content" class="content">
-          <slot></slot>
+        <div part="summary" class="summary">
+          <div class="start" ${hideWhenEmpty()}>
+            <slot name="start"></slot>
+          </div>
+          <div class="center">
+            <slot></slot>
+          </div>
+          <div class="end" ${hideWhenEmpty()}>
+            <slot name="end"></slot>
+          </div>
+        </div>
+        <div part="detail" class=${classMap({ detail: true, card: this.detailVariant === 'card' })} ${hideWhenEmpty()}>
+          <slot name="detail"></slot>
         </div>
       </div>
     `;
