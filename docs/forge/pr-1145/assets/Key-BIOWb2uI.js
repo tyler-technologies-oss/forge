e53e79495595328d30d32009c84e13b1f3267c05
nickonometry <nickonometry@users.noreply.github.com>
2026-07-10 13:18:05 +0000

    Deploying to gh-pages from @ tyler-technologies-oss/forge@bdf321803fe77d94a4d1c000b63b3403ae8d5fc2 🚀

---
+++ docs/forge/pr-1145/assets/Key-BIOWb2uI.js
@@ -0,0 +1,10 @@
+import{u as s,j as t,M as n,T as a,C as r}from"./blocks-Dtp4SZC5.js";import{C as m}from"./CustomArgTypes-Ex77rGw-.js";import{K as c,D as p}from"./Key.stories-BguY7Uf8.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-DkPiitrC.js";import"./utils-GdTrqNrR.js";import"./service-adapter-8tADcN_b.js";import"./key-item-BEhcEP45.js";import"./base-lit-element-BiLQyNYl.js";import"./directive-jorct-Oe.js";import"./a11y-utils-Cc0M_rsz.js";import"./dom-utils-D0uG6d5z.js";import"./utils-CjYv_z18.js";import"./feature-detection-DaAsmZBy.js";import"./state-CqYgVodW.js";import"./query-assigned-nodes-D8SsSM9e.js";import"./base-DVmwUFg0.js";import"./class-map-BG0OcsFG.js";function o(e){const i={h2:"h2",p:"p",...s(),...e.components};return t.jsxs(t.Fragment,{children:[t.jsx(n,{of:c}),`
+`,t.jsx(a,{}),`
+`,t.jsx(i.p,{children:"Keys are used to label data in a chart or visualization."}),`
+`,t.jsx(r,{of:p}),`
+`,t.jsx(i.h2,{id:"api",children:"API"}),`
+`,t.jsx(m,{}),`
+`,t.jsx(i.h2,{id:"accessibility",children:"Accessibility"}),`
+`,t.jsx(i.p,{children:`Ensure that the icon colors of the key items are easily distinguishable and match the color of the
+associated visualization.`}),`
+`,t.jsx(i.p,{children:"The value slot may be used to create an accessible text alternative for data in the visualization."})]})}function E(e={}){const{wrapper:i}={...s(),...e.components};return i?t.jsx(i,{...e,children:t.jsx(o,{...e})}):o(e)}export{E as default};
---
+++ docs/forge/pr-1145/assets/profile-CrZo4WGp.js
@@ -0,0 +1,56 @@
+import{u as r,j as t,M as m,T as p,C as i}from"./blocks-Dtp4SZC5.js";import{C as s}from"./CustomArgTypes-Ex77rGw-.js";import{P as l,D as a,W as c}from"./profile.stories-D2rdX0QI.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-DkPiitrC.js";import"./utils-GdTrqNrR.js";import"./service-adapter-8tADcN_b.js";import"./accordion-Bft1KYTZ.js";import"./dom-utils-D0uG6d5z.js";import"./utils-CjYv_z18.js";import"./base-lit-element-BiLQyNYl.js";import"./directive-jorct-Oe.js";import"./expansion-panel-Bwu0lI6t.js";import"./class-map-BG0OcsFG.js";import"./state-CqYgVodW.js";import"./query-assigned-elements-43hYArgI.js";import"./base-DVmwUFg0.js";import"./utils-DU-9AqTO.js";import"./key-action-CxixFCpG.js";import"./index-5CPwzmQS.js";import"./open-icon-_TxO7zX7.js";import"./tyler-icons-AW4xHEvO.js";import"./constants-mjcpBxJK.js";import"./feature-detection-DaAsmZBy.js";import"./app-bar-profile-button-BccSJnQl.js";import"./a11y-utils-Cc0M_rsz.js";import"./base-component-CjLGUkBU.js";import"./base-adapter-CWZvffxF.js";import"./focus-indicator-CoVWMvtA.js";import"./icon-button-BPC8xvtB.js";import"./base-button-core-B9LSBvAC.js";import"./with-label-aware-BTfOA73Y.js";import"./with-default-aria-B469Hs3U.js";import"./state-layer-CKPcsXao.js";import"./icon-button-constants-CcGPrq8W.js";import"./tooltip-CE7u4Ary.js";import"./overlay-CX_m1mvq.js";import"./with-longpress-listener-D3f6tu0y.js";import"./dismissible-stack-xq-0Rg1q.js";import"./badge-DLSg4EEH.js";import"./menu-CSDXcoFN.js";import"./list-U-EsoDH6.js";import"./event-utils-zQ4FLDwK.js";import"./list-dropdown-aware-core-BhWn6x8A.js";import"./list-dropdown-CvQNxga8.js";import"./event-utils-C1SDeUaq.js";import"./linear-progress-dFUODLVX.js";import"./popover-CZRIaKbl.js";import"./skeleton-BxrGynUp.js";import"./a11y-BxM9_46k.js";import"./base-component-delegate-BR0lUG2N.js";import"./avatar-B6chPF29.js";import"./style-map-BMcvpGeO.js";import"./avatar-constants-C8ceiFxq.js";import"./button-CzVHO11F.js";import"./button-constants-BCAqtVHo.js";import"./toolbar-m2auwteb.js";import"./autocomplete-D_xLVBzb.js";import"./divider-BqEH_Pqm.js";import"./text-field-BgvmEmwH.js";import"./base-field-SLgFjVwO.js";import"./label-CCJpr-qV.js";import"./button-toggle-group-constants-CeBNz6Zr.js";import"./checkbox-constants-C4OOV8dY.js";import"./switch-constants-BYeSq8KE.js";import"./chip-set-UrZ-3ZSP.js";import"./backdrop--ezx6yHr.js";import"./banner-COBkP9Oe.js";import"./bottom-sheet-D4YyTlPZ.js";import"./dialog-D3n85AUX.js";import"./button-area-DmOqPTQk.js";import"./ref-BoXFnMnu.js";import"./button-toggle-group-ByOzG7_a.js";import"./with-form-associated-F3QSJ-zZ.js";import"./calendar-3sIvgK9v.js";import"./card-fNg0rjd_.js";import"./checkbox-DmoEtC-x.js";import"./circular-progress-BvJErdQG.js";import"./color-picker-DnuCPSJ3.js";import"./date-picker-eBlUkm1Z.js";import"./base-date-picker-core-B57s0LgB.js";import"./date-range-picker-BicCm06k.js";import"./base-drawer-CR9sKDRH.js";import"./drawer-BJC8mhbC.js";import"./modal-drawer-C_HzRa2f.js";import"./mini-drawer-CJFzgWnt.js";import"./file-picker-BwlwlhwB.js";import"./floating-action-button-DdXHD1Wz.js";import"./inline-message-1YYbEfHN.js";import"./key-item-BEhcEP45.js";import"./query-assigned-nodes-D8SsSM9e.js";import"./keyboard-shortcut-CoyZVPSf.js";import"./label-value-CMJEsLJf.js";import"./meter-group-DfKvxYjV.js";import"./page-state-BwPC_Hd9.js";import"./paginator-DKTNh-Gp.js";import"./live-announcer-DuLqNKxe.js";import"./select-DHGD13-1.js";import"./scaffold-BAVRvYZ-.js";import"./radio-group-DfVygGGc.js";import"./secret-BwHjKTBO.js";import"./select-dropdown-C6zsKS6F.js";import"./skip-link-gpUxmPut.js";import"./slider-WpAP6ogM.js";import"./split-view-CqigWcxZ.js";import"./stack-BRmnsrL_.js";import"./stepper-DmTnkdIe.js";import"./switch-BexSob7j.js";import"./table-BpH87K7N.js";import"./tab-bar-BGJjqQ49.js";import"./time-picker-Cl678r--.js";import"./toast-Bzi_dBJJ.js";import"./tree-item-DPTfFrQ9.js";import"./view-switcher-B4ao6iwI.js";import"./deprecated-icon-button-l0JzMd6q.js";import"./split-button-3f16CVNe.js";function n(o){const e={a:"a",blockquote:"blockquote",code:"code",h2:"h2",li:"li",p:"p",pre:"pre",strong:"strong",ul:"ul",...r(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(m,{of:l}),`
+`,t.jsx(p,{}),`
+`,t.jsxs(e.p,{children:["For convenience Forge provides the ",t.jsx(e.code,{children:"<forge-app-bar-profile-button>"})," component. This component uses an icon-button (with a predefined avatar), and is responsible for displaying profile information within a popup. The power of this component is that it will ensure a consistent experience for users across all applications that consume it."]}),`
+`,t.jsxs(e.p,{children:["If necessary, you can use the ",t.jsx(e.code,{children:"profileCardBuilder"})," API to provide custom content within the profile card popup."]}),`
+`,t.jsxs(e.blockquote,{children:[`
+`,t.jsx(e.p,{children:"It is expected that you place this component in the end slot of the app-bar for consistency and familiarity for users."}),`
+`]}),`
+`,t.jsx(e.h2,{id:"deprecation-notice",children:"Deprecation notice"}),`
+`,t.jsxs(e.p,{children:[t.jsxs(e.strong,{children:["🚨 This component is deprecated and has been replaced by the ",t.jsx(e.a,{href:"https://tyler-technologies-oss.github.io/forge-extended/v1/?path=/docs/components-user-profile--docs",rel:"nofollow",children:"user profile"})," component in the Tyler Forge™ Extended library."]}),` This
+component will remain in the core library for the foreseeable future, but no new features will be added and it may be removed in a future major release. We recommend that you migrate to the new user profile component when possible.`]}),`
+`,t.jsx(e.h2,{id:"common",children:"Common"}),`
+`,t.jsx(e.p,{children:"A common usage of the profile button will require you to provide some basic information about the user so that it can properly display the data in a familiar location for users. This information needs to be available prior to the user opening the popup."}),`
+`,t.jsx(i,{of:a}),`
+`,t.jsx(e.h2,{id:"custom-content",children:"Custom content"}),`
+`,t.jsx(e.p,{children:`The profile card allows for custom content to be placed within its template. This is provided in the form of a "builder" callback that you can use to generate DOM
+that the component will render for you. You will need to use JavaScript to accomplish this.`}),`
+`,t.jsx(i,{of:c}),`
+`,t.jsx(e.p,{children:`The following snippet demonstrates basic usage in vanilla JavaScript, but if you're using this within a framework you can use the same concept, or if you prefer to avoid
+using vanilla DOM APIs within framework code you can always create a dynamic framework component and return the element instance from the builder callback.`}),`
+`,t.jsx(e.pre,{children:t.jsx(e.code,{className:"language-typescript",children:`profileButton.profileCardBuilder = () => {
+  const listElement = document.createElement('forge-list');
+  listElement.addEventListener('forge-list-item-select', ({ detail }) => {
+    console.log('[profile-card] Selected custom item:', detail.value);
+  });
+  listElement.style.setProperty('--forge-list-padding', '0');
+  listElement.appendChild(document.createElement('forge-divider'));
+  listElement.appendChild(buildListItemElement('My Reports', 'assignment', 'reports'));
+  listElement.appendChild(buildListItemElement('My Workflow', 'work_outline', 'workflow'));
+  listElement.appendChild(buildListItemElement('My Alerts', 'warning', 'alerts'));
+  listElement.appendChild(buildListItemElement('My Preferences', 'settings', 'preferences'));
+  return listElement;
+};
+
+function buildListItemElement(text: string, icon: string, value: string): HTMLElement {
+  const listItemElement = document.createElement('forge-list-item');
+  listItemElement.value = value;
+
+  const iconElement = document.createElement('forge-icon');
+  iconElement.slot = 'leading';
+  iconElement.name = icon;
+  listItemElement.appendChild(iconElement);
+
+  const buttonElement = document.createElement('button');
+  buttonElement.type = 'button';
+  buttonElement.innerText = text;
+  listItemElement.appendChild(buttonElement);
+
+  return listItemElement;
+}
+`})}),`
+`,t.jsx(e.h2,{id:"api",children:"API"}),`
+`,t.jsx(s,{}),`
+`,t.jsx(e.h2,{id:"accessibility",children:"Accessibility"}),`
+`,t.jsxs(e.ul,{children:[`
+`,t.jsx(e.li,{children:"The component will handle adding necessary ARIA attributes for you, as well as ensure the proper color contrast ratio is used."}),`
+`]})]})}function je(o={}){const{wrapper:e}={...r(),...o.components};return e?t.jsx(e,{...o,children:t.jsx(n,{...o})}):n(o)}export{je as default};
