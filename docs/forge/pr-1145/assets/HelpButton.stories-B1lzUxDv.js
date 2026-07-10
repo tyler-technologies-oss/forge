52f96607c0cca75f1db720276106c88965827f6d
nickonometry <nickonometry@users.noreply.github.com>
2026-07-10 13:11:28 +0000

    Deploying to gh-pages from @ tyler-technologies-oss/forge@a55c40b4e979f4acaa01ee52211209f298d0baa3 🚀

---
+++ docs/forge/pr-1145/assets/HelpButton.stories-B1lzUxDv.js
@@ -0,0 +1,11 @@
+import{b as r}from"./iframe-CIdAnCtw.js";import{g as p,c as a}from"./utils-GdTrqNrR.js";import"./service-adapter-8tADcN_b.js";import"./app-bar-profile-button-CbPs9GgD.js";import"./tyler-icons-BkjMgz0l.js";import"./menu-CdZ0C2AZ.js";import"./linear-progress-dFUODLVX.js";import"./list-DwYKm2Vi.js";import"./popover-CZRIaKbl.js";import"./overlay-CX_m1mvq.js";import"./key-action-CxixFCpG.js";import"./index-5CPwzmQS.js";import"./skeleton-BxrGynUp.js";import"./avatar-C3Blvn97.js";import"./icon-button-CBIsTSys.js";import"./focus-indicator-BCun1rI-.js";import"./state-layer-CKPcsXao.js";import"./expansion-panel-z_6VAAL6.js";import"./open-icon-KoBSCZBv.js";import"./divider-C-zcfD_v.js";const o="forge-app-bar-help-button",n=[{label:"Help",value:"help"}],m={title:"Components/App Bar/Help Button",render:t=>a(o,t),component:o,parameters:{actions:{disable:!0}},argTypes:{...p({tagName:o,exclude:["options","icon"]})},args:{}},e={render:()=>r`
+    <forge-app-bar title-text="Help Icon">
+      <forge-app-bar-help-button slot="end" .options=${n}> </forge-app-bar-help-button>
+    </forge-app-bar>
+  `};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  render: () => html\`
+    <forge-app-bar title-text="Help Icon">
+      <forge-app-bar-help-button slot="end" .options=\${options}> </forge-app-bar-help-button>
+    </forge-app-bar>
+  \`
+}`,...e.parameters?.docs?.source}}};const s=["Demo"],v=Object.freeze(Object.defineProperty({__proto__:null,Demo:e,__namedExportsOrder:s,default:m},Symbol.toStringTag,{value:"Module"}));export{e as D,v as H};
